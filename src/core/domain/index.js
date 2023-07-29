7abc4d201d4c698d863f4604fcd6a2e96fa63f5f
jiffy <pmtyjiffy@itcom888.com>
2023-07-29 11:05:14 +0800

    添加文件夹 和index.js 初始化 http

---
+++ src/core/domain/index.js
@@ -0,0 +1,6 @@
+/**
+ * 获取最新的http domain
+*/
+export function getHttpDomains() {
+  return [];
+}
---
+++ src/core/ws/ws-log.js
@@ -0,0 +1,10 @@
+class WSLOG {
+  sendMsg(msg) {
+    console.log(msg);
+  }
+  /**
+   * 刷新最新ws url?>
+  */
+  refresh() {}
+}
+export default new WSLOG();
