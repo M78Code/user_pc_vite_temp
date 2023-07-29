239481eaf2477e1b422612b9e12ad41d1d92d5e9
jinnian <jinnian@123.cc>
2023-07-29 18:55:53 +0800

    命令优化

--- job/export-merchant-config.js
+++ job/export-merchant-config.js
@@ -1,3 +1,23 @@
 /**
  * 合并输出商户配置
  */
+
+
+
+console.log('export-merchant-config----------合并输出商户配置-');
+
+
+console.log('process.argv----------------------0---' );
+console.log(process.argv);
+
+
+console.log('process.argv----------------------1---' );
+ 
+
+console.log('MERCHANT-CONFIG-VERSION  :  ', process.env['MERCHANT-CONFIG-VERSION']);
+// console.log('MERCHANT-CONFIG-VERSION  2:  ', process.env);
+
+
+
+
+console.log('process.argv----------------------3---' );
