72cb2b3fe257e4ec3166067c192450a73ec0aa94
jinnian <jinnian@123.cc>
2023-07-30 13:24:44 +0800

    添加 quasar 组件 配置作为参照

--- job/remove-dist.js
+++ job/remove-dist.js
@@ -1,12 +1,12 @@
 /**
  * 删除 服务器上可能的历史版本输出缓存，确保不出错
  */
-const fs = require("fs");
-fs.stat("./dist/spa", (err, stats) => {
+import * as fs from "node:fs";
+fs.stat("./dist/self-use-version", (err, stats) => {
   console.log("stats-----", stats);
 
   if (stats) {
-    fs.rm("./dist/spa", { recursive: true, force: true }, (err) => {
+    fs.rm("./dist/self-use-version", { recursive: true, force: true }, (err) => {
       console.log(err);
     });
   }
