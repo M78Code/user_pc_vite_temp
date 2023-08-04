e29d2af27d94addc47ed5101465a6cf5c3328528
jiffy <pmtyjiffy@itcom888.com>
2023-08-04 11:31:44 +0800

    修改 delf-use root

--- entries/self-use-version/self-use-version.vite.config.js
+++ entries/self-use-version/self-use-version.vite.config.js
@@ -1,53 +1,45 @@
 // FILE: vite.config.js
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+import path from "path";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-import path from "path"
+console.log("-------------__dirname---------", __dirname);
 
+import FINAL_MERCHANT_CONFIG from "../../job/output/merchant/config.json";
 
-console.log("-------------__dirname---------",__dirname);
+let { project } = FINAL_MERCHANT_CONFIG;
 
-
- import FINAL_MERCHANT_CONFIG from "../../job/output/merchant/config.json"  
-
-
-
- let {project} = FINAL_MERCHANT_CONFIG
-
- if(!project){
-
-  console.error('目标项目必须设定 ----------');
-  console.error('进程结束');
-  
- }else{
-  console.log("-------------project---------",project);
- }
+if (!project) {
+  console.error("目标项目必须设定 ----------");
+  console.error("进程结束");
+} else {
+  console.log("-------------project---------", project);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
-      template: { transformAssetUrls }
+      template: { transformAssetUrls },
     }),
 
     quasar({
-       sassVariables: `app/project/${project}/src/css/quasar-variables.scss`
-    })
+      sassVariables: `app/project/${project}/src/css/quasar-variables.scss`,
+    }),
   ],
+  root: path.resolve(__dirname,`../../project/${project}/`),
   build: {
- 
     outDir: "dist/self-use-version",
- 
     rollupOptions: {
       // external: ["vue"],
-      input:{
-        // index: path.resolve(__dirname,'/entries/self-use-version/index.html')
-        // index: `../../project/${project}/index.html`
-          index: path.resolve(__dirname, `../../project/${project}/index.html`)
-          // index: path.resolve(__dirname, "../../project/yazhou-pc/index.html")
-        //  index:  "../../project/yazhou-pc/index.html"
-      },
+      // input: {
+      //   // index: path.resolve(__dirname,'/entries/self-use-version/index.html')
+      //   // index: `../../project/${project}/index.html`
+      //   index: path.resolve(__dirname, `../../project/${project}/index.html`),
+      //   // index: path.resolve(__dirname, "../../project/yazhou-pc/index.html")
+      //   //  index:  "../../project/yazhou-pc/index.html"
+      // },
       output: {
         // Provide global variables to use in the UMD build
         // Add external deps here
@@ -67,11 +59,10 @@ export default defineConfig({
       project_path: `../${project}`,
     },
   },
-  server:{
-    port:28300,
-    open:    `../../project/${project}/index.html` ,
+  server: {
+    port: 28300,
+    // open: `../../project/${project}/index.html`,
     // open:    "../../project/yazhou-pc/index.html" ,
-    hmr:true
-  }
-
-})
+    hmr: true,
+  },
+});
--- project/yazhou-pc/App.vue
+++ project/yazhou-pc/App.vue
@@ -1,15 +1,25 @@
+<!--
+ * @Author: jiffy
+ * @Date: 2023-08-01 18:37:50
+ * @LastEditTime: 2023-08-04 10:41:02
+ * @LastEditors: jiffy
+ * @Description: 说明
+ *  
+-->
 <template>
-  <div>
-
-     <h1>客户端模块化+SDK版本</h1>
-
+  <div id="ty-app">
+    <h1>客户端模块化+SDK版本</h1>
+    <img src="images/tes.png" alt="" />
   </div>
 </template>
 
 <script setup>
+import { onMounted } from "vue";
 
+onMounted(() => {
+  document.querySelector("#loading-root-ele").style.display = "none";
+});
 </script>
 
 <style lang="scss" scoped>
-
 </style>
