b1b46e9403d72f995f31fe0da8b35375bc55909d
tyrone <pmtytyrone@itcom888.com>
2023-08-05 13:34:59 +0800

    feat: bet&userinfo store

--- project/yazhou-h5/src/store-redux-vuex/index.js
+++ project/yazhou-h5/src/store-redux-vuex/index.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import menuReducer from './module/menu'
+import betInfoReducer from './module/betInfo'
+import userInfoReducer from './module/userinfo'
   const store = configureStore( {
     reducer:{
+      betInfoReducer,
+      userInfoReducer,
       menuReducer
     }
   })
