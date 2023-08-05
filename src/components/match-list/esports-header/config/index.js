6b81b169c44a510f7ee42a163c17b11a6cc88a30
lockie <pmtylockie@itcom888.com>
2023-08-05 14:40:47 +0800

    新增组件

--- src/components/match-list/esports-header/config/index.js
+++ src/components/match-list/esports-header/config/index.js
@@ -1,9 +1,9 @@
 // 当前 系列组件 对外注册 的组件名字
-export  const  component_symbol = 'MatchMedia'
+export  const  component_symbol = 'EsportsHeader'
 //公用的 需要注册的一些对接参数
 export  const need_register_props = {
   match: {
-    type: Object,
+    load_data_state: String,
     default: {  },
   },
   NewMenu: {
