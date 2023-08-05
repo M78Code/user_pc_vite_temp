911da2fed955bb086f02be74a23b88859f9d11aa
cooper <cooper@123.com>
2023-08-05 17:44:21 +0800

    滚动组件

--- project/yazhou-pc/src/pages/match-details/match-details.vue
+++ project/yazhou-pc/src/pages/match-details/match-details.vue
@@ -5,12 +5,24 @@
 -->
 
 <template>
-    <div class="">
-    
-    </div>
-    
-  </template>
-  
-  <script setup>
+  <div class="details relative-position">
+    <!-- 加载中，无数据等显示模板 -->
+    <load-data
+      v-show="load_detail_statu != 'data'"
+      :class="[
+        'details_data_load',
+        { details_loading: load_detail_statu == 'loading' },
+      ]"
+      :state="load_detail_statu"
+      :style="{ 'margin-top': headerHeight + 'px' }"
+      :is_detail="true"
+    />
 
-  </script>
+    <div class="wrap-handicap v-scroll-area-bar">
+  </div>
+</div>
+</template>
+
+<script setup>
+import loadData from "src/components/load-data/load_data.vue";
+</script>
