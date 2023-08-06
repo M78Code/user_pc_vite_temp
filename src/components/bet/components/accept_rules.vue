b9ee8356e416abea719335d34efc2a2a159064f4
lampson <pmtylampson@itcom888.com>
2023-08-06 10:53:04 +0800

    投注相关

---
+++ src/components/bet/components/accept_rules.vue
@@ -0,0 +1,83 @@
+<!--
+ * @Description: 自动接受更好赔率规则弹框
+-->
+<template>
+  <!-- 自动接受更好赔率规则 -->
+  <div class="accept-rules fullscreen" >
+    <div class="fixed-center content-box">
+      <header>自动接受更好赔率</header>
+
+      <footer @click="change_show">我知道了</footer>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref ,reactive,provide,onMounted,watch,computed} from 'vue'
+import EMITTER from  "src/global/mitt.js"
+
+// 点击键盘
+const change_show = () => {
+  EMITTER.emit("change_accept", false)
+}
+
+
+
+</script>
+
+<style lang="scss" scoped>
+.accept-rules {
+  text-align: center;
+  z-index: 800000 !important;
+  white-space: pre-wrap;
+  background: rgba(0, 0, 0, 0.9);
+}
+.content-box {
+  border-radius: 0.16rem;
+  width: 30rem;
+  padding: 0.16rem 0;
+  &.hengping-content-box {
+    height: 3rem;
+    overflow: auto;
+    * {
+      // color: #999 !important;
+    }
+    p {
+      :first-child {
+        color: #fff !important;
+      }
+    }
+  }
+}
+.content-box > p {
+  margin: 0.12rem 0.2rem;
+  font-size: 0.14rem;
+  text-align: left;
+}
+.content-box > header {
+  font-size: 0.16rem;
+  letter-spacing: 0;
+  margin: 0 0.2rem;
+  font-weight: 600;
+  color: salmon;
+}
+.content-box > footer {
+  font-size: 0.16rem;
+  margin-bottom: -0.06rem;
+  padding: 0.13rem 0.2rem 0;
+  color: yellow;
+}
+
+p > span:not(.chu) {
+  text-align: center;
+  display: inline-block;
+  width: 0.16rem;
+  height: 0.16rem;
+  line-height: 0.18rem;
+  border-radius: 8px 0 8px 0;
+  margin-right: 0.08rem;
+}
+.chu {
+  font-weight: bold;
+}
+</style>
---
+++ src/core/bet/betting.js
@@ -0,0 +1,73 @@
+/*
+ * @Description: 押注动作相关的方法
+ */
+
+const hide_bet_series_but = () =>{
+    let res = false;
+    // 单关时,获取投注列表数据
+    if(!get_is_mix && _.get(get_bet_list,'get_bet_list.length')){
+      // 遍历投注列表数据,检测是否红猫赛事
+      for (let i = 0; i < get_bet_list.length; i++) {
+        // 获取投注项id
+        let id = _.get(this,`get_bet_list[${i}]`);
+        // 获取投注项的数据源
+        let cds = _.get(this,`get_bet_obj[${id}].bs.cds`);
+        if(cds == "RC"){
+          // C301赛事时,隐藏串关按钮
+          res = true;
+          break;
+        }
+      }
+    }
+    return res;
+}
+
+ /**
+* @description 校验串关投注项数量是否小于最小串关数量
+*  @param {*} value 不为空表示是删除某个投注项，undefined表示校验所有
+*  @returns
+*/
+const vilidata_mix_count = (value) =>{
+    const bet_length  = get_bet_list.length
+    if(value && bet_length <= 2){
+        return true
+    }
+    const min_num = _.get(get_user, 'configVO.minSeriesNum', 2)
+    if((bet_length - (value ? 1 : 0)) < min_num){
+        set_toast({ 'txt': this.$root.$t('bet.match_min', [min_num]) });
+        return false
+    }else{
+        return true
+    }
+}
+
+/**
+     *@description 更新提示信息
+    * @param {*} value 需要更新的内容
+    * @returns
+*/
+const tips_msg_update = (value) =>{
+    set_update_tips(value||'')
+}   
+
+/**
+ *@description 是否存在PA操盘的赛事
+*@return {Boolean} flag
+*/
+const is_exist_pa_operate = ()=> {
+    let flag = false;
+    _.forIn(get_bet_obj, function(item) {
+    if (item.bs.mprmc == 'PA') {
+        flag = true
+    }
+    });
+    return flag
+}
+
+
+export {
+    hide_bet_series_but,
+    vilidata_mix_count,
+    tips_msg_update,
+    is_exist_pa_operate,
+}
