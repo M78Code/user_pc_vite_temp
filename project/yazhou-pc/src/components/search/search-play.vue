f3856a3552b5185d1afa3249836509ea404cc84f
suho <pmtysuho@itcom888.com>
2023-08-06 14:17:48 +0800

    pc搜索模块代码迁移

---
+++ project/yazhou-pc/src/components/search/search-play.vue
@@ -0,0 +1,18 @@
+<!--  @Description: 搜索玩法 -->
+
+<template>
+    <div class="play-wrap" @click.stop>
+
+    </div>
+</template>
+  
+<script setup>
+
+</script>
+  
+<style lang="scss" scoped>
+.play-wrap {
+    color: #fff;
+    font-size: 30px;
+}
+</style>
