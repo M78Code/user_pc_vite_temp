1b537267a2d08be83768945edca3c569700beda0
jiffy <pmtyjiffy@itcom888.com>
2023-08-07 14:14:19 +0800

    proload

---
+++ src/core/server-img/sprite-img/module/mock.js
@@ -0,0 +1,7 @@
+const server_resource = {
+  "img-bg-menu-live": {
+    day: "public\server-resource\spirit\y0_sport_icon36.png",
+    dark: "public\server-resource\spirit\y0_sport_icon36.png",
+  },
+};
+export {server_resource};
