ec9dde0d4fc6208ab06a2ab3dc69ff4f883c8e3d
gamer <pmtygamer@itcom888.com>
2023-08-08 11:00:27 +0800

    h5详情页底部组件

---
+++ project/yazhou-h5/src/pages/details/category-loading.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: Cronus
+ * @Date: 2020-07-14 17:46:47
+ * @Description: 赛事组件，用于赛事列表展示赛事信息
+-->
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  import {mapMutations} from "vuex";
+
+  export default {
+    name: "category_loading",
+    mounted(){
+      this.set_is_close_video(Math.random());
+      this.$router.push({name:'category', params: {mid: this.$route.params.mid}});
+    },
+    methods:{
+      ...mapMutations(['set_is_close_video']),
+    }
+  }
+</script>
+
+
+
+
