f0b16fed22c3f4a8e34dbc4f1ebe7b3f3222e8d1
suho <pmtysuho@itcom888.com>
2023-08-12 16:45:46 +0800

    pc 体育规则路由调试

---
+++ project/yazhou-pc/src/pages/rule/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <rule-wapper></rule-wapper>
+</template>
+
+<script setup>
+import { RuleWapper } from 'src/components/rule'
+
+</script>
--- project/yazhou-pc/src/pages/test/index.vue
+++ project/yazhou-pc/src/pages/test/index.vue
@@ -1,3 +1,15 @@
 <template>
   <div>测试页面</div>
+  <q-btn @click="on_test">体育规则</q-btn>
 </template>
+
+<script setup>
+
+const on_test = () => {
+  window.open(
+    'http://localhost:28300/project/yazhou-pc/index.html#/rule',
+    '_blank',
+    `height=${650}, width=${1000}, top=${100}, left=${100}, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, status=no,fullscreen=no`
+  );
+}
+</script>
