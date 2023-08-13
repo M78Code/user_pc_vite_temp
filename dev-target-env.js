c93de4a256cd2dcd73699cb7bdecd1ae36ca972f
jiffy <pmtyjiffy@itcom888.com>
2023-08-13 15:18:13 +0800

    打包环境合并 mmerchant-config修改

--- dev-target-env.js
+++ dev-target-env.js
@@ -21,16 +21,48 @@ DEV_TARGET_ENV = "idc_sandbox";
 // IDC 微型测试环境
 // DEV_TARGET_ENV = 'idc_ylcs'
 
+//运维 打包对应 环境
+const ENV_MAP = {
+  online: "idc_online",
+  shiwan: "idc_sandbox",
+};
+
 // --------------------------------
+//测试参数 应该是从 process.argv 来的
+const TEST_ARGV =
+  "project_4-0b1d5ec0284611eea47c132df8d8e15d-1691897964355-online";
+//命令行参数 配置的   商户版本号    ，一般是 本地测试 打包指定版本用 ，也可以支持 打包流程
+let argv_version = (process.argv[2] || "").trim();
+//env 变量  配置的   商户版本号    , 一般是运维那边 配置打包使用的
+let env_version = (process.env["MERCHANT-CONFIG-VERSION"] || "").trim();
+//最终计算出的 用于执行脚本的  商户版本号
+let ARGV = (env_version || argv_version || TEST_ARGV).trim();
 
-let DEV_TARGET_MERCHANT_VERSION =''
-  // 代码内 配置的   商户版本号       ，一般是  本地开发 或者  本地测试 打包指定版本用
-// 指定打包/运行 某个版本 ： 亚洲 H5   
-//  DEV_TARGET_MERCHANT_VERSION =   "yazhou-h5"
+//分割字符串先 第一个项目  版本号 时间戳 环境 1全量/2差量
+const [PROJECT, MERCHANT_CONFIG_VERSION, TIMESTAMP, ENV] = ARGV.split("-");
+console.log(process);
 
-// 指定打包/运行 某个版本 ： 亚洲 PC
- DEV_TARGET_MERCHANT_VERSION =  "yazhou-pc"
+//获取命令行的 打包环境
+if (ENV_MAP[ENV]) {
+  DEV_TARGET_ENV = ENV_MAP[ENV];
+}
+//数字对应的项目
+const project_map = {
+  1: "-", //亚洲版 H5（旧版）
+  2: "-", //亚洲版 PC（旧版）
+  3: "yazhou-h5", //亚洲版 H5（新版)
+  4: "yazhou-pc", //亚洲版 PC（新版)
+};
+//提取项目名称对应是数字
+const [_, PROJECT_NUM] = PROJECT.split("_");
+// 代码内 配置的   商户版本号 ，一般是  本地开发 或者  本地测试 打包指定版本用
+let PROJECT_NAME = project_map[PROJECT_NUM];
 
+//本地环境
+if (process.title.includes("npm run dev")) {
+  //使用 npm run dev yazhou-h5 / yazhou-pc来启动项目
+  PROJECT_NAME = process.argv[2] || "yazhou-pc";
+}
 // --------------------------------
 // 所有  目标环境标识
 let ALL_ENV_ARR = [
@@ -43,7 +75,21 @@ let ALL_ENV_ARR = [
   "idc_ylcs",
   "idc_online",
 ];
-
-
-
-export { DEV_TARGET_ENV, ALL_ENV_ARR ,DEV_TARGET_MERCHANT_VERSION };
+function get_date_format() {
+  const _date = new Date(Number(TIMESTAMP));
+  const _y = _date.getFullYear();
+  const _m = _date.getMonth() + 1;
+  const _d = _date.getDate();
+  return `${_y}${_m > 9 ? _m : "0" + _m}${_d}`;
+}
+const FILE_PATH = `https://api-doc-server-new.sportxxxw1box.com/public/upload/json/${get_date_format()}/${ARGV}-1.json`;
+export {
+  DEV_TARGET_ENV,
+  ALL_ENV_ARR,
+  MERCHANT_CONFIG_VERSION,
+  TIMESTAMP,
+  ENV,
+  FILE_PATH,
+  PROJECT_NAME,
+  PROJECT_NUM,
+};
--- job/export-server-resource.js
+++ job/export-server-resource.js
@@ -10,27 +10,14 @@ import {
 import fs from "node:fs";
 
 // 代码内 配置的   商户版本号       ，一般是  本地测试 打包指定版本用
-import { DEV_TARGET_MERCHANT_VERSION } from "../dev-target-env.js";
-console.log("export-merchant-config----------server-resource ----");
+import {  FILE_PATH } from "../dev-target-env.js";
+console.log("export-server-resource----------server-resource ----");
 console.log("process.argv----------------------0---");
 console.log(process.argv);
 console.log("process.argv----------------------1---");
-console.log(
-  "MERCHANT-CONFIG-VERSION  :  ",
-  process.env["MERCHANT-CONFIG-VERSION"]
-);
 // console.log('MERCHANT-CONFIG-VERSION  2:  ', process.env);
 console.log("process.argv----------------------3---");
 
-//命令行参数 配置的   商户版本号    ，一般是 本地测试 打包指定版本用 ，也可以支持 打包流程
-let argv_version = (process.argv[2] || "").trim();
-//env 变量  配置的   商户版本号    , 一般是运维那边 配置打包使用的
-let env_version = (process.env["MERCHANT-CONFIG-VERSION"] || "").trim();
-//最终计算出的 用于执行脚本的  商户版本号
-let MERCHANT_CONFIG_VERSION =
-  env_version || argv_version || DEV_TARGET_MERCHANT_VERSION;
-
-console.log("MERCHANT_CONFIG_VERSION", MERCHANT_CONFIG_VERSION);
 // 商户配置 输出目录
 let write_folder = "./job/output/merchant";
 let file_path = `${write_folder}/server-resource.json`;
@@ -51,11 +38,6 @@ const download_file_to_local = async (srcs) => {
   try {
     //确保配置 输出目录存在
     ensure_write_folder_exist(img_folder);
-    // let add_obj = {
-    //   MERCHANT_CONFIG_VERSION,
-    //   project: MERCHANT_CONFIG_VERSION,
-    //   write_file_date: Date.now(),
-    // };
     const img_url_theme_map = {};
     Object.entries(srcs).map(([key, themes]) => {
       img_url_theme_map[key] = {};
@@ -80,13 +62,14 @@ const download_file_to_local = async (srcs) => {
 const get_config_info = async () => {
   const username = "TY-yazhou-lan";
   // API 对外文档 的 单个 版本的详情 获取地址
-  let url = `https://api-doc-server-new.sportxxxw1box.com/openapi/serverSource/findAllDefaultConfigKey?version=${MERCHANT_CONFIG_VERSION}&username=${username}`;
   try {
-    let res = await axios.get(url);
+    console.log(FILE_PATH);
+    let res = await axios.get(FILE_PATH);
+    console.log(res.data);
     let { data } = res;
     if (data) {
       //此处1 应该是配置的与后台相对应
-      download_file_to_local(data.data.assets["1"]);
+      download_file_to_local(data.assets);
     }
   } catch (error) {
     console.log("获取 服务器上 当前商户的 版本配置 出错");
