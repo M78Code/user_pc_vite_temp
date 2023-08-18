10cce6e8fbfa7f5eacd24848f94cc1732edbd879
tyrone <pmtytyrone@itcom888.com>
2023-08-18 16:32:14 +0900

    feat: layout

---
+++ project/yazhou-h5/src/layouts/layout-header.vue
@@ -0,0 +1,31 @@
+<!-- 页面头部容器-->
+<template>
+    <div class="yb-h5-layout-header" >
+      <!-- 一级菜单 -->
+     <ul>
+        <li>滚球</li>
+        <li>早盘</li>
+        <li>电竞</li>
+     </ul>
+    </div>
+  </template>
+  
+  <script setup>
+  import { onMounted, ref, reactive } from 'vue'
+  import store from "src/store-redux/index.js";
+  import menu_obj1 from "src/core/meu-h5/menu-data-class.js";
+  onMounted(()=>{
+    console.error(menu_obj1,"menudata")
+  })
+  </script>
+ <style lang="scss" scoped>
+ .yb-h5-layout-header {
+    height:50px ;
+    ul{
+        display: flex;
+        li{
+            flex: 1;
+        }
+    }
+ }
+ </style>
--- project/yazhou-pc/src/layouts/main-layout1.vue
+++ project/yazhou-pc/src/layouts/main-layout1.vue
@@ -23,7 +23,7 @@
   </div>
 </template>
 <script setup>
-import { ref, computed, onBeforeUnmount } from "vue";
+import { ref, computed, onBeforfeUnmount } from "vue";
 import store from "src/store-redux/index.js";
 import "./main-layout.js"; //初始化数据
 import { debounce } from "lodash";
