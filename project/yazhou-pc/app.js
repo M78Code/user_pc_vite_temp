a8f4e2c69760bf7bbd4496156778580a3ebfc585
lane <jstylane@itcom888.com>
2023-08-18 13:57:33 +0800

    1

--- project/yazhou-pc/app.js
+++ project/yazhou-pc/app.js
@@ -3,6 +3,9 @@ import { http, AllDomain } from "src/core/http/";
 import { GetUrlParams } from "src/core/utils/";
 import { get_user_info } from "src/store-redux/module/user-info.js";
 import store from "src/store-redux/index.js";
+
+import { api_match } from "src/api/index.js";
+
 import STANDARD_KEY from "src/core/standard-key";
 import { ss } from "src/core/utils/web-storage";
 import { loadLanguageAsync } from "./src/boot/i18n";
