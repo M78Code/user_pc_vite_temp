8f854045da8eb68ffc0668e817dacec79b7b21bf
suho <pmtysuho@itcom888.com>
2023-08-19 10:43:23 +0800

    layout-header 展示

--- project/yazhou-pc/src/router/routes.js
+++ project/yazhou-pc/src/router/routes.js
@@ -4,47 +4,12 @@ const routes = [
   {
     path: "/",
     name: "main_layout",
-    redirect: {
-      name: "home",
-    },
+    // redirect: {
+    //   name: "home",
+    // },
     component: () => import(`../layouts/main-layout1.vue`),
-    children: [
-      //       // 首页
-      {
-        path: "/home",
-        name: "home",
-        component: () => import(`../pages/test/index.vue`),
-        // component: () => import(`../pages/match_list/match_list.vue`),
-        meta: {
-          keepAlive: true, // 需要缓存
-        },
-      },
-      //       // 搜索
-      //       {
-      //         path: "/search/:keyword/:time?",
-      //         name: "search",
-      //         component: () => import(`../pages/test/index.vue`),
-      //           meta: {
-      //             keepAlive: true // 需要缓存
-      //           }
-      //       },
-      {
-        path: "/details/:mid/:tid/:csid",
-        name: "details",
-        component: () =>
-          import(`../pages/match-details/details_main.vue`),
-      },
-      //       {
-      //         path: "/virtual_details/:mid/:tid/:csid",
-      //         name: "virtual_details",
-      //         component: () => import(`../pages/test/index.vue`),
-      //       },
-      //       {
-      //         path: "/video/:mid/:tid/:csid/:play_type/:video_size",
-      //         name: "video",
-      //         component: () => import(`../pages/test/index.vue`),
-      //       },
-    ],
+    // component: () => import(`../pages/test/index.vue`),
+
   },
   // //赛事分析
   // {
