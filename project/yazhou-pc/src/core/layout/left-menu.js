ffeba64aa7821470ffa983107e31f39b6f8ebc3e
jiffy <pmtyjiffy@itcom888.com>
2023-08-19 11:45:38 +0800

    左侧 代码注释

--- project/yazhou-pc/src/core/layout/left-menu.js
+++ project/yazhou-pc/src/core/layout/left-menu.js
@@ -1,11 +1,14 @@
 import store from "src/store-redux/index.js";
-import { onBeforeUnmount, ref } from "vue";
+import { ref } from "vue";
 import { useMittOn, MITT_TYPES } from "src/core/mitt";
 const { layoutReducer } = store.getState();
 //是否为mini状态
+/**
+ * main_menu_toggle 左侧列表显示形式 default: 'normal' -- normal：展开 mini：收起
+ * is_mini_menu 获取菜单收起状态 default: false
+ */
 const is_mini_menu = ref(layoutReducer.left_menu_is_mini);
-const main_menu_toggle = ref(layoutReducer.left_menu_is_mini);
-
+const main_menu_toggle = ref(layoutReducer.left_menu_status);
 //监听宽度变化/单击事件
 const list_emit = [
   useMittOn(MITT_TYPES.EMIT_LAYOUT_MENU_TOGGLE, (v) => {
