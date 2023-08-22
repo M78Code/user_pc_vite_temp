1b8860d3637a6362b28613c6f727141f8dbb2174
suho <pmtysuho@itcom888.com>
2023-08-22 16:19:47 +0800

    处理颜色变量

--- project/yazhou-pc/src/css/component/search.js
+++ project/yazhou-pc/src/css/component/search.js
@@ -5,6 +5,7 @@ export default {
     'search-border-color2': '#f6f7fa',
     'search-border-color3': '#ebecf4',
     'search-border-color4': '#bfcedd',
+    'search-border-color5': '#999',
     /** 背景颜色 */
     'search-bg-color1': '#f6f7fa',
     "search-bg-color2": "rgba(20,23,29,0.15)",
