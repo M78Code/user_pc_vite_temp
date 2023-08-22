5fd2e990ee07909b85ce3b991e94e34a517e20e2
pmtymalick <pmtymalick>
2023-08-22 17:30:58 +0800

    注单历史组件主题色变量-2

--- project/yazhou-h5/src/css/component/cathectic.js
+++ project/yazhou-h5/src/css/component/cathectic.js
@@ -2,77 +2,82 @@ export default {
 /**
  * -----字体颜色-----
  **/
-// 注单历史页面默认字体颜色/对阵信息，玩法赔率(var(--q-color-fs-color-3))
-"cathectic-fs-defalut-color-1": "#414655",
-// 对阵信息，玩法赔率(var(--q-color-fs-color-147))
-"cathectic-body-main-fs-defalut-color": "#98a3c2",
-// 对阵信息，玩法赔率 body-main(var(--q-color-com-fs-color-22))
-"cathectic-text-red-fs-color": "#e02020",
-// 对阵信息，玩法赔率 body-main/联赛名和比赛时间body-top(var(--q-color-fs-color-7))
-"cathectic-fs-color": "#414655",
-// 对阵信息，玩法赔率 body-main/联赛名和比赛时间body-top pre-wrapper(var(--q-color-fs-color-152))
-"cathectic-fs-defalut-color-2": "#ffb001",
-// 联赛名和比赛时间body-top pre-cancle-button(var(--q-color-fs-color-36))
-"cathectic-pre-cancle-button-fs-color": "#ffb001",
-// 提前结算的按钮、滑块和提前结算详情early-settle/btn3 span(var(--q-color-fs-color-13))
-"cathectic-early-settle-comom-fs-color-1": "#999999",
-// 提前结算的按钮、滑块和提前结算详情early-settle(var(--q-color-fs-color-12))
-"cathectic-early-settle-fs-color": "#999999",
-// 提前结算的按钮、滑块和提前结算详情early-settle  btn-l(var(--q-color-fs-color-122))
-"cathectic-btn-l-fs-color": "#999999",
-// 提前结算的按钮、滑块和提前结算详情early-settle  detail-wrap span(var(--q-color-fs-color-1))
-"cathectic-detail-wrap-fs-color": "#333333",
-// 提前结算的按钮、滑块和提前结算详情early-settle  tips-right/slider-content/order-num(var(--q-color-fs-color-50))
-"cathectic-early-settle-commom-fs-color-2": "#ffb001",
-// 提前结算的按钮、滑块和提前结算详情early-settle  btn-ll(var(--q-color-fs-color-109))
-"cathectic-btn-ll-fs-color": "#FF7000",
-// 提前结算的按钮、滑块和提前结算详情early-settle  btn-ll span(var(--q-color-fs-color-3))
-"cathectic-btn-ll-fs-color": "#414655",
-// 提前结算的按钮、滑块和提前结算详情early-settle  btn2 span(var(--q-color-com-fs-color-8))
-"cathectic-btn2-fs-color": "#ffffff",
+// var(--q-color-fs-color-3)
+"cathectic-text-color-1": "#414655",
+// var(--q-color-com-fs-color-22)
+"cathectic-text-color-2": "#e02020",
+// var(--q-color-fs-color-7)
+"cathectic-text-color-3": "#414655",
+// var(--q-color-fs-color-152)
+"cathectic-text-color-4": "#ffb001",
+// var(--q-color-fs-color-147);
+"cathectic-text-color-5": "#999999",
+// var(--q-color-fs-color-36)
+"cathectic-text-color-6": "#ffffff",
+// var(--q-color-fs-color-13)
+"cathectic-text-color-7": "#999999",
+// var(--q-color-fs-color-122)
+"cathectic-text-color-7": "unset",
+// var(--q-color-fs-color-1)
+"cathectic-text-color-7": "#333333",
+// var(--q-color-fs-color-50)
+"cathectic-text-color-7": "#ffb001",
+// var(--q-color-fs-color-109)
+"cathectic-text-color-7": "#FF7000",
+// var(--q-color-com-fs-color-8)
+"cathectic-text-color-7": "#ffffff",
+// var(--q-color-fs-color-112)
+"cathectic-text-color-7": "#66a754",
+// var(--q-color-com-fs-color-21)
+"cathectic-text-color-7": "#99b8dd",
+// var(--q-color-fs-color-6)
+"cathectic-text-color-7": "rgba(0, 0, 0, 0.25)",
+// var(--q-color-fs-color-113)
+"cathectic-text-color-7": "#e02020",
+// var(--q-color-fs-color-8)
+"cathectic-text-color-7": "#99a3b1",
+// var(--q-color-fs-color-148)
+"cathectic-text-color-7": "#414655",
 
-
-// 已结算+未结算+预约 字体选中颜色
-"cathectic-fs-active-color": "#4987FB",
 /**
  * -----背景色-----
  **/
-// 已结算+未结算+预约
-// 背景色
-"cathectic-page-bg-color": "#ffffff",
-// 选中时下划线背景色
-"cathectic-underline-bg-color": "#ffb001",
-// 对阵信息，玩法赔率 body-main2(var(--q-color-page-bg-color-50))
-"cathectic- body-main-before-page-bg-color": "#ffb001",
-// 联赛名和比赛时间body-top(var(--q-color-page-bg-color-13))
-"cathectic-body-top-before-page-bg-color": "#ffb001",
-// 提前结算的按钮、滑块和提前结算详情early-settle p(var(--q-color-page-bg-color-1))
-"cathectic-detail-wrap-page-bg-color": "#f5f5f5",
-// 提前结算的按钮、滑块和提前结算详情early-settle slider-content(var(--q-color-page-bg-color-9))
-"cathectic-slider-content-page-bg-color": "#F9FBFF",
-// 提前结算的按钮、滑块和提前结算详情early-settle slider-content(var(--q-color-page-bg-color-50))
-"cathectic-slider-content-selection-page-bg-color": "#ffb001",
-// 提前结算的按钮、滑块和提前结算详情early-settle bt3(var(--q-color-page-bg-color-31))
-"cathectic-bt3-page-bg-color": "#dfe0e8",
+// var(--q-color-page-bg-color-50)
+"cathectic-page-bg-color-1": "#ffb001",
+// var(--q-color-page-bg-color-13)
+"cathectic-page-bg-color-2": "#ffb001",
+// var(--q-color-page-bg-color-31)
+"cathectic-page-bg-color-3": "#dfe0e8",
+// var(--q-color-page-bg-color-9)
+"cathectic-page-bg-color-4": "#F9FBFF",
+// var(--q-color-page-bg-color-1)
+"cathectic-page-bg-color-5": "#f5f5f5",
+// var(--q-color-page-bg-color-114)
+"cathectic-page-bg-color-6": "#ffffff",
+
 
 /**
  * -----边框色-----
  **/
-// 已结算+未结算+预约 选中时下划线边框色
-"cathectic-underline-border-color": "#ffb001",
-// (已结算+未结算+预约) 下边框透明色
-"cathectic-border-bottom-color-1": "transparent",
-// 提前结算的按钮、滑块和提前结算详情early-settle (var(--q-color-border-color-55))
-"cathectic-btn-l-border-color": "#ffb001",
+// var(--q-color-border-color-55)
+"cathectic-border-color-1": "#ffb001",
+// var(--q-color-border-color-11)
+"cathectic-border-color-2": "#f5f7fa",
+// var(--q-color-border-color-58)
+
 
 /**
  * -----渐变色-----
  **/
-// 对阵信息，玩法赔率 body-main3(var(--q-color-linear-gradient-bg-16))
-"cathectic-before-linear-gradient-bg-color": "repeating-linear-gradient(180deg, #ffb001 0, #ffb001 6px, #ffffff 6px, #ffffff 8px)",
-// 提前结算的按钮、滑块和提前结算详情 early-settle (var(--q-color-linear-gradient-bg-1))
-"cathectic-btn-l-linear-gradient-bg-color": "linear-gradient(180deg, rgba(255, 176, 1, 0.2) 0%,rgba(255, 176, 1, 0.02) 99%)",
-// 提前结算的按钮、滑块和提前结算详情 early-settle btn2(var(--q-color-linear-gradient-bg-2))
-"cathectic-btn2-linear-gradient-bg-color": "linear-gradient(270deg, #ffb001 0%, #ff7000 100%)",
+// var(--q-color-linear-gradient-bg-16);
+"cathectic-linear-gradient-bg-color-1": "repeating-linear-gradient(180deg, #ffb001 0, #ffb001 6px, #ffffff 6px, #ffffff 8px)",
+// var(--q-color-linear-gradient-bg-1)
+"cathectic-linear-gradient-bg-color-2": "linear-gradient(180deg, rgba(255, 176, 1, 0.2) 0%,rgba(255, 176, 1, 0.02) 99%)",
+// var(--q-color-linear-gradient-bg-2)
+"cathectic-linear-gradient-bg-color-3": "linear-gradient(270deg, #ffb001 0%, #ff7000 100%)",
+// var(--q-color-linear-gradient-bg-9)
+"cathectic-linear-gradient-bg-color-4": "linear-gradient(180deg, rgba(255,176,1,0.20) 0%, rgba(255,176,1,0.02) 99%)",
+
+
 
 }
