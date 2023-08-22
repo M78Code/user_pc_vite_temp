c5a0d01cc148faf0bc011edf51d32dc358db9905
pmtymalick <pmtymalick>
2023-08-22 16:18:05 +0800

    注单历史页面主题是变量

---
+++ project/yazhou-h5/src/css/component/cathectic.js
@@ -0,0 +1,78 @@
+export default {
+/**
+ * -----字体颜色-----
+ **/
+// 注单历史页面默认字体颜色/对阵信息，玩法赔率(var(--q-color-fs-color-3))
+"cathectic-fs-defalut-color-1": "#414655",
+// 对阵信息，玩法赔率(var(--q-color-fs-color-147))
+"cathectic-body-main-fs-defalut-color": "#98a3c2",
+// 对阵信息，玩法赔率 body-main(var(--q-color-com-fs-color-22))
+"cathectic-text-red-fs-color": "#e02020",
+// 对阵信息，玩法赔率 body-main/联赛名和比赛时间body-top(var(--q-color-fs-color-7))
+"cathectic-fs-color": "#414655",
+// 对阵信息，玩法赔率 body-main/联赛名和比赛时间body-top pre-wrapper(var(--q-color-fs-color-152))
+"cathectic-fs-defalut-color-2": "#ffb001",
+// 联赛名和比赛时间body-top pre-cancle-button(var(--q-color-fs-color-36))
+"cathectic-pre-cancle-button-fs-color": "#ffb001",
+// 提前结算的按钮、滑块和提前结算详情early-settle/btn3 span(var(--q-color-fs-color-13))
+"cathectic-early-settle-comom-fs-color-1": "#999999",
+// 提前结算的按钮、滑块和提前结算详情early-settle(var(--q-color-fs-color-12))
+"cathectic-early-settle-fs-color": "#999999",
+// 提前结算的按钮、滑块和提前结算详情early-settle  btn-l(var(--q-color-fs-color-122))
+"cathectic-btn-l-fs-color": "#999999",
+// 提前结算的按钮、滑块和提前结算详情early-settle  detail-wrap span(var(--q-color-fs-color-1))
+"cathectic-detail-wrap-fs-color": "#333333",
+// 提前结算的按钮、滑块和提前结算详情early-settle  tips-right/slider-content/order-num(var(--q-color-fs-color-50))
+"cathectic-early-settle-commom-fs-color-2": "#ffb001",
+// 提前结算的按钮、滑块和提前结算详情early-settle  btn-ll(var(--q-color-fs-color-109))
+"cathectic-btn-ll-fs-color": "#FF7000",
+// 提前结算的按钮、滑块和提前结算详情early-settle  btn-ll span(var(--q-color-fs-color-3))
+"cathectic-btn-ll-fs-color": "#414655",
+// 提前结算的按钮、滑块和提前结算详情early-settle  btn2 span(var(--q-color-com-fs-color-8))
+"cathectic-btn2-fs-color": "#ffffff",
+
+
+// 已结算+未结算+预约 字体选中颜色
+"cathectic-fs-active-color": "#4987FB",
+/**
+ * -----背景色-----
+ **/
+// 已结算+未结算+预约
+// 背景色
+"cathectic-page-bg-color": "#ffffff",
+// 选中时下划线背景色
+"cathectic-underline-bg-color": "#ffb001",
+// 对阵信息，玩法赔率 body-main2(var(--q-color-page-bg-color-50))
+"cathectic- body-main-before-page-bg-color": "#ffb001",
+// 联赛名和比赛时间body-top(var(--q-color-page-bg-color-13))
+"cathectic-body-top-before-page-bg-color": "#ffb001",
+// 提前结算的按钮、滑块和提前结算详情early-settle p(var(--q-color-page-bg-color-1))
+"cathectic-detail-wrap-page-bg-color": "#f5f5f5",
+// 提前结算的按钮、滑块和提前结算详情early-settle slider-content(var(--q-color-page-bg-color-9))
+"cathectic-slider-content-page-bg-color": "#F9FBFF",
+// 提前结算的按钮、滑块和提前结算详情early-settle slider-content(var(--q-color-page-bg-color-50))
+"cathectic-slider-content-selection-page-bg-color": "#ffb001",
+// 提前结算的按钮、滑块和提前结算详情early-settle bt3(var(--q-color-page-bg-color-31))
+"cathectic-bt3-page-bg-color": "#dfe0e8",
+
+/**
+ * -----边框色-----
+ **/
+// 已结算+未结算+预约 选中时下划线边框色
+"cathectic-underline-border-color": "#ffb001",
+// (已结算+未结算+预约) 下边框透明色
+"cathectic-border-bottom-color-1": "transparent",
+// 提前结算的按钮、滑块和提前结算详情early-settle (var(--q-color-border-color-55))
+"cathectic-btn-l-border-color": "#ffb001",
+
+/**
+ * -----渐变色-----
+ **/
+// 对阵信息，玩法赔率 body-main3(var(--q-color-linear-gradient-bg-16))
+"cathectic-before-linear-gradient-bg-color": "repeating-linear-gradient(180deg, #ffb001 0, #ffb001 6px, #ffffff 6px, #ffffff 8px)",
+// 提前结算的按钮、滑块和提前结算详情 early-settle (var(--q-color-linear-gradient-bg-1))
+"cathectic-btn-l-linear-gradient-bg-color": "linear-gradient(180deg, rgba(255, 176, 1, 0.2) 0%,rgba(255, 176, 1, 0.02) 99%)",
+// 提前结算的按钮、滑块和提前结算详情 early-settle btn2(var(--q-color-linear-gradient-bg-2))
+"cathectic-btn2-linear-gradient-bg-color": "linear-gradient(270deg, #ffb001 0%, #ff7000 100%)",
+
+}
