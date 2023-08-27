295fb914b8ebb6c6e9fa8e818b36fc48985ced65
jinnian <jinnian@123.cc>
2023-08-27 22:57:05 +0800

    优化

---
+++ src/core/match-constant/config/csid.js
@@ -0,0 +1,52 @@
+ 
+/**
+ * 球种id枚举
+ *
+ */
+export const CSID_CONST = {
+ 
+  CSID_001:{csid:"1",mark:"足球"},
+  CSID_002:{csid:"2",mark:"篮球"},
+  CSID_003:{csid:"3",mark:"棒球"},
+  CSID_004:{csid:"4",mark:"冰球"},
+  CSID_005:{csid:"5",mark:"网球"},
+  CSID_006:{csid:"6",mark:"美式足球"},
+  CSID_007:{csid:"7",mark:"斯诺克"},
+  CSID_008:{csid:"8",mark:"乒乓球"},
+  CSID_009:{csid:"9",mark:"排球"},
+  CSID_010:{csid:"10",mark:"羽毛球"},
+  CSID_011:{csid:"11",mark:"手球"},
+  CSID_012:{csid:"12",mark:"拳击"},
+  CSID_013:{csid:"13",mark:"沙滩排球"},
+  CSID_014:{csid:"14",mark:"联合式橄榄球"},
+  CSID_015:{csid:"15",mark:"曲棍球"},
+  CSID_016:{csid:"16",mark:"水球"},
+  CSID_017:{csid:"17",mark:"田径"},
+  CSID_018:{csid:"18",mark:"政治娱乐"},
+  CSID_019:{csid:"19",mark:"游泳"},
+  CSID_020:{csid:"20",mark:"体操"},
+  CSID_021:{csid:"21",mark:"跳水"},
+  CSID_023:{csid:"23",mark:"举重"},
+  CSID_024:{csid:"24",mark:"射箭"},
+  CSID_025:{csid:"25",mark:"击剑"},
+  CSID_026:{csid:"26",mark:"冰壶"},
+  CSID_027:{csid:"27",mark:"跆拳道"},
+  CSID_028:{csid:"28",mark:"高尔夫"},
+  CSID_029:{csid:"29",mark:"自行车"},
+  CSID_030:{csid:"30",mark:"赛马"},
+  CSID_031:{csid:"31",mark:"帆船"},
+  CSID_032:{csid:"32",mark:"划船"},
+  CSID_033:{csid:"33",mark:"赛车运动"},
+  CSID_034:{csid:"34",mark:"柔道"},
+  CSID_035:{csid:"35",mark:"空手道"},
+  CSID_036:{csid:"36",mark:"摔跤"},
+  CSID_037:{csid:"37",mark:"板球"},
+  CSID_038:{csid:"38",mark:"飞镖"},
+  CSID_039:{csid:"39",mark:"沙滩足球"},
+  CSID_040:{csid:"40",mark:"其他"},
+  CSID_100:{csid:"100",mark:"英雄联盟"},
+  CSID_101:{csid:"101",mark:"Dota2"},
+  CSID_102:{csid:"102",mark:"CS.GO"},
+  CSID_103:{csid:"103",mark:"王者荣耀"},
+ 
+  };
