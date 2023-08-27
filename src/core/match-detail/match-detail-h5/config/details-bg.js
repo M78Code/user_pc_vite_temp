8f0c16feb9fe62796e1725ac7a48bf5b83353a6c
pmtynati <jstynati@itcom888.com>
2023-08-27 23:55:42 +0800

    代码优化

--- src/core/match-detail/match-detail-h5/config/details-bg.js
+++ src/core/match-detail/match-detail-h5/config/details-bg.js
@@ -1,139 +1,139 @@
   const detail_csid_config= {
-    CSID_001: {
+    CSID_1: {
       csid: '1',
       mark: '足球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/football.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_002: {
+    CSID_2: {
       csid: '2',
       mark: '篮球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/basketball.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_003: {
+    CSID_3: {
       csid: '3',
       mark: '棒球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/baseball.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_004: {
+    CSID_4: {
       csid: '4',
       mark: '冰球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/ice_hockey.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_005: {
+    CSID_5: {
       csid: '5',
       mark: '网球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/tennis.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_006: {
+    CSID_6: {
       csid: '6',
       mark: '美式足球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/american_football.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_007: {
+    CSID_7: {
       csid: '7',
       mark: '斯诺克',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/snooker_pool.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_008: {
+    CSID_8: {
       csid: '8',
       mark: '乒乓球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/ping_pong.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_009: {
+    CSID_9: {
       csid: '9',
       mark: '排球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/volleyball.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_010: {
+    CSID_10: {
       csid: '10',
       mark: '羽毛球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/badminton.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_011: {
+    CSID_11: {
       csid: '11',
       mark: '手球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/handball.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_012: {
+    CSID_12: {
       csid: '12',
       mark: '拳击',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/boxing.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_013: {
+    CSID_13: {
       csid: '13',
       mark: '沙滩排球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/beach_volleyball.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_014: {
+    CSID_14: {
       csid: '14',
       mark: '联合式橄榄球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/rugby.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_015: {
+    CSID_15: {
       csid: '15',
       mark: '曲棍球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/hockey.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_016: {
+    CSID_16: {
       csid: '16',
       mark: '水球',
       detail: {
         B: 'background: url("../../public/yazhou-h5/image/jpg/details/water_polo.jpg" ) no-repeat center center/100%  content-box content-box;'
       }
     },
-    CSID_017: { csid: '17', mark: '田径' },
-    CSID_018: { csid: '18', mark: '政治娱乐' },
-    CSID_019: { csid: '19', mark: '游泳' },
-    CSID_020: { csid: '20', mark: '体操' },
-    CSID_021: { csid: '21', mark: '跳水' },
-    CSID_023: { csid: '23', mark: '举重' },
-    CSID_024: { csid: '24', mark: '射箭' },
-    CSID_025: { csid: '25', mark: '击剑' },
-    CSID_026: { csid: '26', mark: '冰壶' },
-    CSID_027: { csid: '27', mark: '跆拳道' },
-    CSID_028: { csid: '28', mark: '高尔夫' },
-    CSID_029: { csid: '29', mark: '自行车' },
-    CSID_030: { csid: '30', mark: '赛马' },
-    CSID_031: { csid: '31', mark: '帆船' },
-    CSID_032: { csid: '32', mark: '划船' },
-    CSID_033: { csid: '33', mark: '赛车运动' },
-    CSID_034: { csid: '34', mark: '柔道' },
-    CSID_035: { csid: '35', mark: '空手道' },
-    CSID_036: { csid: '36', mark: '摔跤' },
-    CSID_037: { csid: '37', mark: '板球' },
-    CSID_038: { csid: '38', mark: '飞镖' },
-    CSID_039: { csid: '39', mark: '沙滩足球' },
-    CSID_040: { csid: '40', mark: '其他' },
+    CSID_17: { csid: '17', mark: '田径' },
+    CSID_18: { csid: '18', mark: '政治娱乐' },
+    CSID_19: { csid: '19', mark: '游泳' },
+    CSID_20: { csid: '20', mark: '体操' },
+    CSID_21: { csid: '21', mark: '跳水' },
+    CSID_23: { csid: '23', mark: '举重' },
+    CSID_24: { csid: '24', mark: '射箭' },
+    CSID_25: { csid: '25', mark: '击剑' },
+    CSID_26: { csid: '26', mark: '冰壶' },
+    CSID_27: { csid: '27', mark: '跆拳道' },
+    CSID_28: { csid: '28', mark: '高尔夫' },
+    CSID_29: { csid: '29', mark: '自行车' },
+    CSID_30: { csid: '30', mark: '赛马' },
+    CSID_31: { csid: '31', mark: '帆船' },
+    CSID_32: { csid: '32', mark: '划船' },
+    CSID_33: { csid: '33', mark: '赛车运动' },
+    CSID_34: { csid: '34', mark: '柔道' },
+    CSID_35: { csid: '35', mark: '空手道' },
+    CSID_36: { csid: '36', mark: '摔跤' },
+    CSID_37: { csid: '37', mark: '板球' },
+    CSID_38: { csid: '38', mark: '飞镖' },
+    CSID_39: { csid: '39', mark: '沙滩足球' },
+    CSID_40: { csid: '40', mark: '其他' },
     CSID_100: {
       csid: '100',
       mark: '英雄联盟',
