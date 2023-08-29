ab31c2b94a7a1c39622b4eefb8356a8eb0575c43
lockie <pmtylockie@itcom888.com>
2023-08-29 15:13:50 +0800

    1

--- src/components/match-list/composables/match-list-gloab.js
+++ src/components/match-list/composables/match-list-gloab.js
@@ -5,6 +5,7 @@ import { useMittEmit, MITT_TYPES } from "src/core/mitt/index.js";
 import { send_zhuge_event } from 'src/core/http/zhuge-tag.js'
 import NewMenu from "src/core/menu-pc/menu-data-class.js";
 import details from "src/core/match-list-pc/details-class/details.js";
+import UserCtr from 'src/core/user-config/user-ctr.js'
 const latest_match_params_pre = ref("");
 const default_select_all = ref(true);
 const route = useRoute()
@@ -99,7 +100,7 @@ const mx_autoset_active_match = (params = { mid: 0 }) => {
 		tid,
 		sort: this.vx_match_sort,
 		keyword: this.vx_related_keyword.substr(5),
-		cuid: this.vx_get_uid,
+		cuid: UserCtr.uid,
 		mid: remove_mid,
 	};
 	// 如果是聚合冠军页面
@@ -165,112 +166,6 @@ const mx_autoset_active_match = (params = { mid: 0 }) => {
 		});
 	}
 };
-//显示token失效弹窗
-const show_fail_alert = () => {
-	let ret = false;
-	let callbackUrl = this.vx_get_user.callbackUrl;
-	if ( UserCtr.is_invalid) {
-		//是否失效
-		// if ((!callbackUrl) && (callbackUrl != undefined)) {
-		//   // 弹出提示消息、登录层
-		//   window.vue.useMittEmit(
-		//     window.vue.MITT_TYPES.EMIT_SHOW_TOAST_CMD,
-		//     window.vue.i18n.t("login.login_timeout")
-		//   );
-		// } else {
-		// 登录失效直接展示 alert
-		useMittEmit(MITT_TYPES.EMIT_SHOW_ALERT_CMD, {
-			text: i18n.t("login.login_timeout"),
-			callback: () => {
-				location.href = callbackUrl;
-				// 清除旧的登录信息
-				this.vx_clear_user();
-			},
-		});
-		// }
-		ret = true;
-	}
-	return ret;
-};
-// 获取比赛阶段是否需要查询接口
-const get_phase_result = (csid, mmp) => {
-	let check_result = false;
-	if (csid == 2) {
-		// 篮球
-		if (mmp > 0 && mmp < 3) {
-			// 上下半场
-			check_result = true;
-		} else if (mmp > 12 && mmp < 17) {
-			// 第一节~第四节
-			check_result = true;
-		} else if (mmp == 40) {
-			// 加时赛
-			check_result = true;
-		} else if (mmp == 303) {
-			// 第三节休息
-			check_result = true;
-		}
-	} else if (csid == 3) {
-		// 棒球
-		if (mmp > 400 && mmp < 421) {
-			// 第一局上,第一局下~加时上,加时下
-			check_result = true;
-		}
-	} else if (csid == 4) {
-		// 冰球
-		if (mmp > 0 && mmp < 4) {
-			// 第一节~第三节
-			check_result = true;
-		} else if (mmp == 40) {
-			// 加时赛
-			check_result = true;
-		}
-	} else if (csid == 5) {
-		// 网球
-		if (mmp > 7 && mmp < 13) {
-			// 第一盘~第五盘
-			check_result = true;
-		}
-	} else if (csid == 6) {
-		// 美式足球
-		if (mmp > 12 && mmp < 17) {
-			// 第一节~ 第四节 加时赛
-			check_result = true;
-		} else if (mmp == 40) {
-			// 加时赛
-			check_result = true;
-		}
-	} else if (csid == 7) {
-		// 斯洛克
-		if (mmp == 21) {
-			// 进行中
-			check_result = true;
-		}
-	} else if (csid == 8) {
-		// 乒乓球
-		if ((mmp > 7 && mmp < 13) || (mmp > 440 && mmp < 443)) {
-			// 第一局~第七局
-			check_result = true;
-		}
-	} else if (csid == 9) {
-		// 排球
-		if ((mmp > 7 && mmp < 13) || mmp == 17 || (mmp > 440 && mmp < 443)) {
-			// 第一局~第七局
-			check_result = true;
-		} else if (mmp == 17) {
-			// 第五局
-			check_result = true;
-		}
-	} else if (csid == 10) {
-		// 羽毛球
-		if (mmp > 7 && mmp < 13) {
-			// 第一局~第五局
-			check_result = true;
-		}
-	}
-	return check_result;
-};
-
 
 const gloab_composable_fn = () => {
   return {
