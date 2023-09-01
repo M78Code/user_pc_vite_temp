e9dc1a02f0e50dd559664369c37c46e4981536aa
lampson <pmtylampson@itcom888.com>
2023-09-01 11:43:06 +0800

    路径修改

--- project/yazhou-h5/src/pages/home/hot/components/sports-balls-tab.vue
+++ project/yazhou-h5/src/pages/home/hot/components/sports-balls-tab.vue
@@ -53,10 +53,9 @@
 
 <script setup>
 // import match_main from "src/project/pages/match-list/match_main";   // 赛事列表页用于展示滚球、今日、早盘、串关、冠军等赛事
-// import {mapGetters} from "vuex";
 // import {api_home} from "src/project/api";
-// import hot_list from "src/project/components/skeleton/home_hot/hot_list";   // 热门榜单 骨架屏
-// import hot_schedule from "src/project/components/skeleton/home_hot/hot_schedule";   // 热门赛程 骨架屏
+import hotList from "src/components/skeleton/home-hot/hot_list.vue";   // 热门榜单 骨架屏
+import hotSchedule from "src/components/skeleton/home-hot/hot_schedule.vue"     // 热门赛程 骨架屏
 import no_data from "src/components/common/no-data.vue";    // 无网络展示组件
 import public_form from "./public_form.vue";    // 首页热门足球和 篮球的 公共榜单表格
 import { useMittEmit, useMittOn, MITT_TYPES } from "src/core/mitt/index.js"
