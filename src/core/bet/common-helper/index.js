ac7abce91a4f82ccc27ed9eb8ef88c43b511379e
lowen <pmtylowen@itcom888.com>
2023-09-02 17:04:06 +0800

    1

--- src/core/bet/common-helper/index.js
+++ src/core/bet/common-helper/index.js
@@ -1,16 +1,16 @@
 
 
 import * as CommonSportHelper from  "./module/common-sport.js"
-// import * as CommonHelper from  "./module/common.js"
-// import * as VirtualSportHelper from  "./module/virtual-sport.js"
+import * as CommonHelper from  "./module/common.js"
+import * as VirtualSportHelper from  "./module/virtual-sport.js"
 
 
 
 
 const BetCommonHelper={
     ...CommonSportHelper,
-    // ...CommonHelper,
-    // ...VirtualSportHelper,
+    ...CommonHelper,
+    ...VirtualSportHelper,
 }
 
 export default BetCommonHelper
