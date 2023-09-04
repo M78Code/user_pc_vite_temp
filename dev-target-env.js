3dfeaf77c6fda1e1b50b3f48f5d33824684865ca
lane <jstylane@itcom888.com>
2023-09-04 10:29:49 +0800

    启动配置修改

--- dev-target-env.js
+++ dev-target-env.js
@@ -9,11 +9,11 @@ let DEV_TARGET_ENV = "local_test";
 // 局域网  压力测试环境
 // DEV_TARGET_ENV = "local_ylcs";
 // 局域网  测试环境
-// DEV_TARGET_ENV = "local_test";
+DEV_TARGET_ENV = "local_test";
 // IDC  预发布
 // DEV_TARGET_ENV = 'idc_pre'
 // IDC 试玩环境
-DEV_TARGET_ENV = "idc_sandbox";
+// DEV_TARGET_ENV = "idc_sandbox";
 // IDC  隔离预发布
 // DEV_TARGET_ENV = 'idc_lspre'
 // IDC  生产环境
@@ -28,15 +28,16 @@ DEV_TARGET_ENV = "idc_sandbox";
 
  
 //本地开发 目标项目
-// let DEV_PROJECT_NAME = "yazhou-pc";
-let DEV_PROJECT_NAME = "yazhou-h5";
+let DEV_PROJECT_NAME = "yazhou-pc";
+// let DEV_PROJECT_NAME = "yazhou-h5";
 
 
 
 
 // 这里不一般配置为空 
+let  DEV_TARGET_VERSION = ''
 //代码内配置死的    构建 zip 版本参数    ，一般是 本地测试 打包指定版本用 ，也可以支持 打包流程 
-let  DEV_TARGET_VERSION ="project_4-36304ea0499e11ee8848ada2b8a1d739-1693720827442-shiwan";
+// DEV_TARGET_VERSION ="project_4-36304ea0499e11ee8848ada2b8a1d739-1693720827442-shiwan";
 
 // 打包 亚洲版 PC（新版)   试玩环境  
 // DEV_TARGET_VERSION ="project_4-0b1d5ec0284611eea47c132df8d8e15d-1691927205605-shiwan";
