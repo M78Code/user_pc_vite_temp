42474e8d56e86caea6a6e48cd3a8b42b480d17d4
lane <jstylane@itcom888.com>
2023-09-04 16:18:10 +0800

    投注-3

--- src/components/bet/bet-box-pc-1/index.vue
+++ src/components/bet/bet-box-pc-1/index.vue
@@ -21,7 +21,7 @@
       </div>
     </div>
     <!-- 正常入口的单关 -->
-    <!-- <bet-single v-show="BetData.is_bet_single" @set_scroll_this="set_scroll_this" /> -->
+    <bet-single v-show="BetData.is_bet_single" @set_scroll_this="set_scroll_this" />
     <!-- 正常入口的串关 -->
     <!-- <bet-mix v-show="!BetData.is_bet_single" class="full-height" @set_scroll_this="set_scroll_this" /> -->
   </div>
