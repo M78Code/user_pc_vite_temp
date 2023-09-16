1f89d82acf6f32687b7e95bd8cc0ea2636890076
jiffy <pmtyjiffy@itcom888.com>
2023-09-16 19:40:08 +0800

    css脚本修改

--- job/export-css-config.js
+++ job/export-css-config.js
@@ -23,11 +23,11 @@ console.log("process.argv----------------------3---");
 
 // 商户配置 输出目录
 let write_folder = "./job/output/css/";
-let file_path = write_folder + `${PROJECT_NAME}.json`;
+let file_path = write_folder + `index.json`;
 
 //本地scss目录
-// let scss_folder = `./project/${PROJECT_NAME}/src/css/variables/`;
-let scss_folder = `./project/${PROJECT_NAME}/src/css/`;
+let scss_folder = `./project/${PROJECT_NAME}/src/css/variables/`;
+// let scss_folder = `./project/${PROJECT_NAME}/src/css/`;
 
 //确保配置 输出目录存在
 ensure_write_folder_exist(write_folder);
@@ -59,57 +59,44 @@ const getAllFile = function (dir) {
 const get_css_config = async (css_params = {}) => {
   try {
     let merchant_css_config = {};
-    for (const key in css_params) {
-      merchant_css_config[key] = Object.keys(css_params[key]);
-    }
-    const obj = await diff_css_local();
-
+    const obj = await diff_css_local(css_params);
     write_file(
       file_path,
-      JSON.stringify(merchant_css_config)
-    );
-    write_file(
-      write_folder + "index.json",
       JSON.stringify(obj)
     );
   } catch (error) {
-    console.log("css文件错误",error);
+    console.log("css文件错误", error);
   }
 };
 /**对比文档如果和服务器的配置不一样就结束进程*/
-const diff_css_local = async () => {
+const diff_css_local = async (css_params) => {
   const all_global_scss = getAllFile(scss_folder + "global");
   const all_component_scss = getAllFile(scss_folder + "component");
   const obj = {
     global: {},
     component: {},
   };
-  const globals = all_global_scss.map((file_path) => {
+  function red_path_keys(file_path, key) {
     return import("../" + file_path.replace(/\\/g, "/")).then((res) => {
       const file_name = file_path.split(/[\\/]/).pop().replace(".js", "");
-
-      if (res.default) obj.global[file_name] = Object.keys(res.default);
+      if (res.default) obj[key][file_name] = Object.keys(res.default).reduce((sum, cur) => {
+        sum[cur] = css_params[cur] || { day: '', night: '' }
+        console.log(cur, sum[cur])
+        return sum;
+      }, {});
       // if (!lodash.hasIn(merchant_css_config.global, Object.keys(res.default))) {
       //   process.emit(1);
       // }
     });
-  });
-  const components = all_component_scss.map((file_path) => {
-    return import("../" + file_path.replace(/\\/g, "/")).then((res) => {
-      const file_name = file_path.split(/[\\/]/).pop().replace(".js", "");
-      if (res.default) {
-        obj.component[file_name] = Object.keys(res.default);
-      }
-    });
-    // if (
-    //   !lodash.hasIn(merchant_css_config[file_name], Object.keys(res.default))
-    // ) {
-    //   process.emit(1);
-    // }
-  });
+  }
+  //globals
+  const globals = all_global_scss.map(v => red_path_keys(v, "global"));
+  //component
+  const components = all_component_scss.map(v => red_path_keys(v, "component"));
   await Promise.all(globals.concat(components));
   return obj;
 };
+
 
 // 获取 服务器上 当前商户的 版本配置
 get_css_config(final_merchant_config.css);
