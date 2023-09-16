d422012a5beca9f64bce0300524f2105a032b341
nico <pmtynico@itam8888>
2023-09-16 19:22:05 +0800

    2

---
+++ project/yazhou-h5/src/css/server-img/other-img/index.js
@@ -0,0 +1,39 @@
+import server_resource from "app/job/output/assets/index.js";
+import { get } from "lodash";
+const { CURRENT_ENV } = window.BUILDIN_CONFIG;
+import UserCtr from "src/core/user-config/user-ctr.js";
+
+const modules = import.meta.globEager("./module/*.js");
+let all_other_image = {};
+Object.keys(modules).forEach((key) => {
+  // const module_name = key.replace("./module/", "").replace(".js", "");
+  all_other_image = Object.assign(
+    all_other_image,
+    modules[key].default
+  );
+});
+
+/**
+ * 拿图片地址 和位置
+ * @param {*} param0 
+ * @returns 
+ */
+function compute_css({ key, theme }) {
+  //先从商户配置拿 再从本地拿 
+  let config = server_resource[key] || all_other_image[key] || {};
+  //从打包的 环境拿 图片地址
+  let url = get(
+    server_resource,
+    `${config[CURRENT_ENV] || config["default"]}.${UserCtr.theme}`
+  );
+  if (!url) {
+    //从本地拿
+    url = get(config, UserCtr.theme);
+  }
+  return {
+    "background-image": `url(${url})`,
+    "background-repeat": 'no-repeat'
+  };
+}
+
+export default compute_css;
