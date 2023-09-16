47020006881e6ddaa227dbb980f07c205e463bf9
jiffy <pmtyjiffy@itcom888.com>
2023-09-16 20:52:27 +0800

    css脚本

--- src/core/css-var/index.js
+++ src/core/css-var/index.js
@@ -12,18 +12,31 @@ const PROJECT_NAME = BUILDIN_CONFIG.TARGET_PROJECT_NAME;
 // 处理  演示代码 
 // 全局的   
 // const modules = import.meta.globEager("./module/*.js");
-const global_modules = import(`../../../project/${PROJECT_NAME}/src/css/variables/global/*js`);
-const component_modules = import(`../../../project/${PROJECT_NAME}/src/css/variables/component/*js`);
+// const global_modules =import.meta.glob([`../../../project/${PROJECT_NAME}/src/css/variables/global/*js`]);
+// const component_modules = import(`../../../project/${PROJECT_NAME}/src/css/variables/component/*js`);
+const IS_PC = PROJECT_NAME.includes("pc");
 
-const conmpute_css_obj = (modules) => {
+
+// 处理  演示代码 
+// 全局的   
+// const modules = import.meta.globEager("./module/*.js");
+const global_modules_h5 = import.meta.glob(`../../../project/yazhou-h5/src/css/variables/global/*js`, { eager: true });
+const component_modules_h5 = import.meta.glob(`../../../project/yazhou-h5/src/css/variables/component/*js`, { eager: true });
+const global_modules_pc = import.meta.glob(`../../../project/yazhou-pc/src/css/variables/global/*js`, { eager: true });
+const component_modules_pc = import.meta.glob(`../../../project/yazhou-pc/src/css/variables/component/*js`, { eager: true });
+
+const global_modules = IS_PC ? global_modules_pc : global_modules_h5
+const component_modules = IS_PC ? component_modules_pc : component_modules_h5
+
+
+const conmpute_css_obj = (_modules) => {
   let css_obj = {}
-  Object.keys(modules).forEach((path) => {
+  Object.keys(_modules).forEach((path) => {
     let arr = path.split("/")
     let key = arr[arr.length - 1]
     key = key.substring(0, key.length - 3)
-    modules[path]().then((module) => {
-      css_obj[key] = module.default;
-    })
+    // var module = await _modules[path]()
+    css_obj[key] = _modules[path].default;
   });
   return css_obj
 }
@@ -39,8 +52,8 @@ const all_css = {
  */
 
 export const compute_css_variables = ({ category, module }) => {
+  console.log(category, module)
   let css_obj = all_css[category][module] || {}
-
   let keys = Object.keys(css_obj)
 
   let final_obj = {}
