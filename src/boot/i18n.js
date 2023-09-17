9a77ca035df811c8b0f52e00c5ab48907cfa56a3
lane <jstylane@itcom888.com>
2023-09-17 18:59:28 +0800

    css

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -47,11 +47,11 @@ function loadLanguageAsync(lang) {
     /* webpackChunkName: "lang-[request]" */ `../i18n/${IS_PC ? 'pc' : 'h5'}/${map_lang[lang]}/index.json`
   ).then((langfile) => {
     LocalStorage.set("lang", lang),
-      console.error(langfile)
+      // console.error(langfile)
     // 动态加载对应的语言包
     // let langFile = langfile.default || langfile;
     // 设置语言信息
-    i18n.global.setLocaleMessage(lang, langFile);
+    i18n.global.setLocaleMessage(lang, langfile);
     // 设置语种
     i18n.locale = lang;
     return lang;
