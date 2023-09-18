3e4193d2c6aaccdf0ecd2fb123b64e35ba0dc6a1
suho <pmtysuho@itcom888.com>
2023-09-18 18:23:19 +0800

    1

--- src/components/common/tab/tab-1/index.vue
+++ src/components/common/tab/tab-1/index.vue
@@ -6,9 +6,9 @@
         v-for="(val, index) in list" :key="index" @click.stop="onclick(index, val)" @mouseenter="tabs_enter(index)"
         @mouseleave="tabs_leave(index)" :id="BUILDIN_CONFIG.DOM_ID_SHOW && `top-menu-ids-${val.id}`">
         <!-- BUILDIN_CONFIG.DOM_ID_SHOW 显示部分dom ID -->
-        <!-- TODO: g_image_preffix -->
         <img v-if="val.img_src"
           v-check-img="{ src: val.img_src, default: `${$g_image_preffix}/image/common/activity_banner/gift_package.png` }" />
+        <!-- <div :class="val.class" :style="compute_css('gift-package')"></div> -->
         {{ val[tab_name_key] }}
         <!-- 早盘||串关 主列表顶部日期后显示赛事数量 -->
         <span v-if="is_list_top_menu" class="match-count">({{ val.count }})</span>
@@ -43,6 +43,7 @@ import store from 'src/store-redux'
 /** utils 工具类 */
 // import { get_refs_info } from 'src/core/bet/common-helper/common-sport.js'
 import BUILDIN_CONFIG from "app/job/output/env/final.js";
+import { compute_css } from 'src/core/server-img/index.js'
 
 const props = defineProps({
   /** tab列表 */
