3c045aed28d147df41e390f90739643fffeda93c
lowen <pmtylowen@itcom888.com>
2023-09-18 21:23:02 +0800

    完成详情css

--- project/yazhou-pc/src/layouts/main-layout1.vue
+++ project/yazhou-pc/src/layouts/main-layout1.vue
@@ -45,7 +45,7 @@ import "./main-layout.js"; //初始化数据
 import { SearchWapper } from 'src/components/search'
 import layoutHeader from "./layout-header.vue";
 import layoutLeft from "./layout-left.vue";
-// import layoutRight from "./layout-right.vue";
+import layoutRight from "./layout-right.vue";
 import toastComponents from "project_path/src/components/toast/toast.vue";
 import alertComponents from "project_path/src/components/toast/alert.vue";
 import confirmComponents from "project_path/src/components/toast/confirm.vue";
