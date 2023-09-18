5b17fc5c1e1308778a31e0a8b028e110a9ca25e3
suho <pmtysuho@itcom888.com>
2023-09-18 10:44:54 +0800

    1

--- project/yazhou-pc/src/layouts/main-layout1.vue
+++ project/yazhou-pc/src/layouts/main-layout1.vue
@@ -10,7 +10,7 @@
     <div style="display: none;"> {{ LayOutMain_pc.layout_version }}</div>
     <div class="flex">
       <!-- 左侧 菜单 -->
-      <div :style="{ height: LayOutMain_pc.layout_content_height , width:LayOutMain_pc.layout_left_width }" class="layout_main_left">
+      <div :style="{ height: LayOutMain_pc.layout_content_height , width:LayOutMain_pc.layout_left_width }">
         <layout-left />
       </div>
       <div :style="{ height: LayOutMain_pc.layout_content_height , width:LayOutMain_pc.layout_content_width }">
@@ -44,7 +44,7 @@ import "./main-layout.js"; //初始化数据
 /**组件*/
 import { SearchWapper } from 'src/components/search'
 import layoutHeader from "./layout-header.vue";
-import layoutLeft from "./layout-left.vue";
+// import layoutLeft from "./layout-left.vue";
 // import layoutRight from "./layout-right.vue";
 import toastComponents from "project_path/src/components/toast/toast.vue";
 import alertComponents from "project_path/src/components/toast/alert.vue";
@@ -66,7 +66,6 @@ const mitt_offs = [
   width: 100%;
   display: flex;
   flex-direction: column;
-  background: var(--q-gb-bg-c-10);
   .video_page {
     position: fixed;
     top: 0;
@@ -76,10 +75,5 @@ const mitt_offs = [
     width: 100% !important;
     height: 100% !important;
   }
-}
-
-.layout_main_left {
-  border: 1px solid var(--q-gb-bd-c-8);
-  border-radius: 0 8px 0 0;
 }
 </style>
