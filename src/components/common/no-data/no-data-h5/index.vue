da5c6bf6bba36c74999e038f3bcfb10bfea1dc94
suho <pmtysuho@itcom888.com>
2023-09-22 16:20:03 +0800

    no-data1

---
+++ src/components/common/no-data/no-data-h5/index.vue
@@ -0,0 +1,217 @@
+<!-- @Description: 网络不给力，空空如也这些信息展示 -->
+<template>
+  <div class="no-data"
+    :style="{ 'min-height': top_height + 'px', paddingTop: is_detail && top_height < 500 ? '.6rem' : '80px' }">
+
+    <template v-if="['暂无,此处逻辑产品暂时说放弃'].includes(which)">
+      <div class="empty-favorite-bg"
+        :style="{ backgroundImage: UserCtr.theme.includes('day') ? `url(${arr.noMatchNew.url})` : `url(${arr.noMatchNew.url2})` }">
+      </div>
+      <p class="title" :style="{ color: UserCtr.theme.includes('day') ? '#666666' : ' #B9B9B9' }">{{
+        arr.noMatchNew.txt[0]
+      }}</p>
+      <p class="title-tint" :style="{ color: UserCtr.theme.includes('day') ? '#999999' : ' #999999' }">{{
+        arr.noMatchNew.txt[1] }}</p>
+      <p>
+        <span class="btn" @click="refresh_data"
+          :style="{ color: UserCtr.theme.includes('y0') ? '#4987FB' : '#FF9124', borderColor: UserCtr.theme.includes('y0') ? '#569FFD' : '#FF9124' }">
+          {{ arr.noMatchNew.txt[2] }}</span>
+      </p>
+    </template>
+
+    <template v-if="['noMatch', 'noWifi', 'noMessage'].includes(which)">
+      <div class="empty-favorite-bg"
+        :style="{ backgroundImage: UserCtr.theme.includes('day') ? `url(${arr.noMatch.url})` : `url(${arr.noMatch.url2})` }">
+      </div>
+      {{arr.noMatch}}
+      <p style="color:#A5A9B3;">{{ which === 'noMessage' ? arr.noMessage.txt || msg : arr.noMatch.txt || msg}}</p>
+    </template>
+
+    <template v-if="which === 'nolive'">
+      <div class="empty-favorite-bg"
+        :style="{ backgroundImage: UserCtr.theme.includes('day') ? `url(${arr.nolive.url})` : `url(${arr.nolive.url2})` }">
+      </div>
+      <p style="color:#A5A9B3;"> {{ arr.nolive.txt }} </p>
+    </template>
+
+    <template v-if="which === 'collect'">
+      <div class="empty-favorite-bg"
+        :style="{ backgroundImage: UserCtr.theme.includes('day') ? `url(${arr.collect.url})` : `url(${arr.collect.url2})` }">
+      </div>
+      <p style="color:#A5A9B3;">{{ arr.collect.txt }}</p>
+    </template>
+  </div>
+</template>
+  
+<script setup>
+import { ref, onMounted, onUnmounted, defineProps, watch } from 'vue'
+import { useMittEmit, MITT_TYPES } from "src/core/mitt"
+import { i18n_t } from "src/core/index.js";
+import { useRoute } from "vue-router"
+import UserCtr from 'src/core/user-config/user-ctr.js'
+
+//-------------------- 对接参数 prop 注册  开始  -------------------- 
+import { useRegistPropsHelper } from "src/composables/regist-props/index.js"
+import { component_symbol, need_register_props } from "../config/index.js"
+
+console.error('nodata++++',props);
+useRegistPropsHelper(component_symbol, need_register_props)
+const props = defineProps({
+  which: {
+    type: String,
+    required: true
+  },
+  height: {
+    required: true
+  },
+  msg: {
+    type: String,
+  },
+  type_name: {
+    type: String,
+  },
+  marginBottom: {
+    type: String,
+  },
+  width: {
+    type: String,
+  },
+  height: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  class: {
+    type: String,
+  }
+})
+// const computed_props = useRegistPropsHelper(component_symbol, defineProps(need_register_props));
+// const tableClass_computed = useComputed.tableClass_computed(props)
+// const title_computed = useComputed.title_computed(props)
+//-------------------- 对接参数 prop 注册  结束  --------------------
+
+const arr_const = {
+  collect: {
+    url: "image/bw3/svg/no_shouc.svg",
+    url2: "image/bw3/svg/no_shouc2.svg",
+    txt: i18n_t('msg.msg_nodata_08'),// '暂无关注的赛事哦',
+  },
+  noWifi: {
+    url: "image/bw3/svg/nowifi.svg",
+    txt: i18n_t('msg.msg_nodata_09'),//'网络不给力',
+  },
+  noMatch: {
+    url: "public/image/bw3/svg/noMatch.svg",
+    url2: "image/bw3/png/noMatch2.png",
+    txt: i18n_t('msg.msg_nodata_02'),//'空空如也~',
+  },
+  noMatchNew: {
+    url: "image/bw3/png/noMatch_new.png",
+    url2: "image/bw3/png/noMatch2_new.png",
+    txt: i18n_t('msg.msg_nodata_02_new'),//'数组 对应 标题 提示文字 刷新',
+  },
+  noMessage: {
+    url: "image/bw3/svg/noMatch.svg",
+    url2: "image/bw3/png/noMatch2.png",
+    txt: i18n_t('msg.msg_nodata_17'),//'暂无消息记录~',
+  },
+  nolive: {
+    url: "image/bw3/svg/no_livedata.svg",
+    url2: "image/bw3/svg/no_livedata2.svg",
+    txt: i18n_t('msg.msg_nodata_14'),//'暂无直播的赛事哦',
+  }
+}
+
+const arr = ref(arr_const)
+const top_height = ref(0)
+const is_detail = ref(false)
+
+const route = useRoute()
+function init() {
+  top_height.value = window.innerHeight - props.height;
+  is_detail.value = route.name === 'category';
+}
+onMounted(init)
+
+// 监听国际化语种变化,一旦变化修正国际化字符串
+watch(
+  () => i18n_t.locale,
+  () => arr.value = arr_const
+)
+
+function refresh_data() {
+  useMittEmit(MITT_TYPES.EMIT_MENU_CHANGE_FOOTER_CMD, {
+    text: "footer-refresh"
+  })
+}
+
+// onBeforeUnmount(() => {
+//   // TODO: 暂不清楚$data用途
+//   for (const key in $data) {
+//     $data[key] = null
+//   }
+// })
+
+</script>
+    
+<style lang="scss" scoped>
+.no-data {
+  border-bottom: 1px solid transparent;
+  min-height: 100%;
+  text-align: center;
+  color: #a5a9b3;
+
+  .empty-favorite-bg {
+    width: 1.8rem;
+    height: 1.61rem;
+    margin: 0 auto;
+    background-size: contain;
+    background-repeat: no-repeat;
+    user-select: none;
+  }
+
+  .title {
+    font-family: PingFangSC-Medium;
+    font-size: 0.14rem;
+    color: #666666;
+    margin-bottom: 0rem;
+  }
+
+  .title-tint {
+    margin-top: 0.03rem;
+    font-size: 0.12rem;
+    color: #999999;
+  }
+
+  .btn {
+    display: inline-block;
+    width: 1.2rem;
+    height: 0.34rem;
+    border: 1px solid transparent;
+    border-radius: 0.17rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 0.14rem;
+    margin: auto;
+  }
+
+
+  p {
+    margin: 0.14rem;
+    font-size: 0.14rem;
+
+    span {
+      display: inline-block;
+      width: 1.4rem;
+      height: 0.4rem;
+      line-height: 0.4rem;
+      border-radius: 0.05rem;
+      color: #ffb001;
+      border: 1px solid #ffb001;
+    }
+  }
+}
+</style>
+  
