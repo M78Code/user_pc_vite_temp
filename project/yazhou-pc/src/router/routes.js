faa38965974a3a3a7e61831caaa6c2b37dd2d136
malick <malick@Aa12>
2023-09-23 18:41:51 +0800

    路由地址

--- project/yazhou-pc/src/router/routes.js
+++ project/yazhou-pc/src/router/routes.js
@@ -66,7 +66,7 @@ const routes = [
   {
     path: "/bet_record",
     name: "bet_record",
-    component: () => import("../pages/bet-record/index.vue"),//project/yazhou-pc/src/pages/bet-record/index.vue
+    component: () => import("project_path/src/pages/bet-record/index.vue"),//project/yazhou-pc/src/pages/bet-record/index.vue
   },
   // 赛果
   {
