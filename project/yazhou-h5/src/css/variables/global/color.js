5ca26e49351ff828d2e65d724809ddb982d71bae
jinnian <jinnian@123.cc>
2023-10-10 21:32:02 +0800

    CSS  提升一层 做 base

--- project/yazhou-h5/src/css/variables/global/color.js
+++ project/yazhou-h5/src/css/variables/global/color.js
@@ -1,19 +1,4 @@
 export default {
-  'gb-t-c-1': '#006aff',
-  'gb-t-c-2': '#69c969',
-  'gb-t-c-3': '#707070',
-  'gb-t-c-4': '#bdc5cf',
-  'gb-t-c-5': '#d8d8d8',
-  'gb-t-c-6': '#e6e6e6',
-  'gb-t-c-7': '#e4eaff',
-  'gb-t-c-8': '#edeef9',
-  'gb-t-c-9': '#fcfcfc',
-  'gb-t-c-10': '#f9efef',
-  'gb-t-c-11': '#ff9124',
-  'gb-t-c-12': '#ffb001',
-  'gb-t-c-13': '#ffe8e8',
-  'gb-t-c-14': '#ffffff',
-  'gb-t-c-17': '#6c7ba8',
-  'gb-t-c-15': 'tranparent',
-  'gb-t-c-16': 'unset'
+ 
+    
 }
