048ee9edb6399da328463243662f32253f882165
lane <jstylane@itcom888.com>
2023-10-13 13:53:18 +0800

    修改路由文件路径

--- project/yazhou-pc/src/router/routes.js
+++ project/yazhou-pc/src/router/routes.js
@@ -39,7 +39,7 @@ const routes = [
       {
         path: "/details/:mid/:tid/:csid",
         name: "details",
-        component: () => import('../pages/match-details/details-main.vue')
+        component: () => import('../pages/match-details/match-details.vue')
       },
       {
         path: "/video/:mid/:tid/:csid/:play_type/:video_size",
