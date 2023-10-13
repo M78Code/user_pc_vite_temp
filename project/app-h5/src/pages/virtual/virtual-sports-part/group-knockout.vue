36aedca187fdc82e5408c25686babe0c7cd03fb2
jiffy <pmtyjiffy@itcom888.com>
2023-10-13 17:11:39 +0800

    新 app-h5添加 页面

---
+++ project/app-h5/src/pages/virtual/virtual-sports-part/group-knockout.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="group_knockout">
+    <div class="row justify-center tabs hairline-border">
+      <div v-for="(item, index) in tab_list" :key="index"
+           :class="[tabIndex == index ? 'is-active' : '']"
+           @click="tab_click(item)"
+      >
+        {{item.label}}
+      </div>
+    </div>
+    <q-carousel
+      v-model="currentContent"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      :swipeable="false"
+      animated
+      :height="height_change"
+    >
+      <q-carousel-slide :name="item.component" v-for="(item,i) in tabList" :key="i">
+        <!-- 顶部切换 下边的内容组件 -->
+        <component :is="item.component" :tid="tid" :current_match = "current_match"/>
+      </q-carousel-slide>
+    </q-carousel>
+  </div>
+</template>
+
+<script>
+// #TODO VUEX 
+// import {mapGetters} from "vuex";
+import group_matches from "project_path/pages/virtual/virtual_sports_part/group_matches.vue";
+import knockout from "project_path/pages/virtual/virtual_sports_part/knockout.vue";
+
+import { reactive, computed, onMounted, onUnmounted, toRefs, watch } from "vue";
+export default defineComponent({
+  name: "group_knockout",
+  props:{
+    tid: {
+      type: Number|String,
+      default: null,
+      require: true
+    },
+    current_match:Object,
+  },
+  components: {
+    group_matches: group_matches,
+    knockout: knockout,
+  },
+  
+  setup(props, evnet) {
+    const component_data = reactive({
+      tabIndex: 0,
+      tab_list: [
+        { index: 0, label: i18n_t('virtual_sports.group_matches'), value: 'group_matches' ,disable: false},
+        { index: 1, label: i18n_t('virtual_sports.knockout'), value: 'knockout', disable: false }
+      ],
+      tabList: [
+        {
+          name: i18n_t('virtual_sports.group_matches'),
+          component: 'group_matches'
+        },
+        {
+          name: i18n_t('virtual_sports.knockout'),
+          component: 'knockout'
+        }
+      ],
+      currentContent: 'group_matches',
+      height_change: '100%'
+    });
+    // #TODO VUEX 
+    // computed: {
+    //   ...mapGetters({
+    //     current_batch:'get_current_batch'
+    //   }),
+    // },
+    const current_batch = computed(() => {
+      return ""
+    });
+    watch(
+      () => data.currentContent,
+      () => {
+        component_data.height_change = '400px'
+
+        clearTimeout(component_data.timer)
+        component_data.timer = setTimeout(() => {
+          component_data.height_change = '100%'
+        },200)
+      }
+    );
+    // 监听小组赛 淘汰赛的变化
+    // #TODO VUEX GETTERS 
+    watch(
+      () => current_batch.value.mmp,
+      (n,o) => {
+        const key_list = ['Q8', 'Q4', 'SEMIFINAL', 'FINAL']
+        if(key_list.includes(component_data.current_batch.value.mmp)){
+          component_data.tabIndex = 1
+          component_data.currentContent = 'knockout'
+          component_data.tab_list[0].disable = true
+          component_data.tab_list[1].disable = false
+        }else{
+          component_data.tabIndex = 0
+          component_data.currentContent = 'group_matches'
+          component_data.tab_list[0].disable = false
+          component_data.tab_list[1].disable = true
+        }
+      }
+    );
+    // 点击菜单切换
+    const tab_click = (tab) => {
+      if(tab.index==1 && tab.disable) return
+      component_data.tabIndex = tab.index
+      component_data.currentContent = tab.value
+    }
+    onUnmounted(() => {
+      clearTimeout(component_data.timer)
+      component_data.timer = null
+
+      // #TODO $data 
+      // for (const key in component_data.$data) {
+      //   component_data.$data[key] = null
+      // }
+    })
+    return {
+      ...toRefs(component_data),
+      tab_click,
+      current_batch
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.group_knockout {
+  .tabs {
+    height: 0.4rem;
+    margin: 0.11rem 0 0.05rem 0;
+    border-radius: 0.08rem;
+    position: relative;
+
+    > div {
+      flex: 1;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 0.14rem;
+
+      &:nth-child(1) {
+        border-radius: 0.08rem 0 0 0.08rem;
+      }
+
+      &:nth-child(2) {
+        border-radius: 0 0.08rem 0.08rem 0;
+      }
+
+      // &.is-active {
+      //   background: var(--q-color-page-bg-color-9);
+      //   color: var(--q-gb-t-c-13);
+
+      // }
+    }
+  }
+  :deep(.q-carousel__slide) {
+    padding: unset;
+  }
+}
+
+
+</style>
+
---
+++ project/app-h5/src/pages/virtual/virtual-sports-part/virtual-sports-match-list.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class="match-list-wrapper" :class="{standard:get_newer_standard_edition == 2}">
+    <template>
+      <div class="title-wrap-standard row justify-end" v-if="get_newer_standard_edition == 2">
+        <div class="odd-title-wrapper row">
+          <div class="odd-t-w-inner row items-center" :class="{status2:standard_odd_status}">
+            <div v-for="(hpl_title, hp_i) of i18n_t('list_title.'+csid+'.title')" :key="hp_i">
+              {{hpl_title}}
+            </div>
+          </div>
+        </div>
+      </div>
+      <v-sports-match-item v-for="(match_item,i) in virtual_match_list" :match_selected_i="selected_match_i"
+        :key="i" :i="i" :match_item="match_item" @switch_match="switch_match_handle"
+        @odd_pan="odd_pan_handle" :other_status="standard_odd_status">
+      </v-sports-match-item>
+    </template>
+  </div>
+</template>
+
+<script>
+// #TODO VUEX
+// import { mapGetters, mapActions } from "vuex";
+import v_s_match_timer from "project_path/pages/virtual/virtual_sports_part/virtual_sports_match_timer.vue";
+import virtual_sports_match_item from "project_path/pages/virtual/virtual_sports_part/virtual_sports_match_item.vue";
+// #TODO MIXINS 
+// import betting from 'project_path/mixins/betting/betting.js';
+import {  PageSourceData  } from "src/core/index.js";
+import SVirtual from "project_path/components/skeleton/virtual_sports/virtual"
+import { reactive, computed, onMounted, onUnmounted, toRefs, watch } from "vue";
+export default defineComponent({
+  name: "virtual_sports_match_list",
+  // #TODO MIXINS 
+  // mixins:[common],
+
+  components:{
+    'v-s-match-timer':v_s_match_timer,
+    'v-sports-match-item':virtual_sports_match_item,
+    SVirtual
+  },
+
+  props:{
+    virtual_match_list:Array,
+    match_list_loaded:Number,
+    v_menu_changed:Number | String,
+    csid:Number | String,
+  },
+  
+  
+  setup(props, evnet) {
+    const component_data = reactive({
+      selected_match_i:0,
+      v_match_hps:[],
+      standard_odd_status:0,
+    })
+    // #TODO VUEX
+    // ...mapActions([
+    //   // 设置玩法项默认选中
+    //   "set_details_item",
+    // ]),
+
+    // #TODO VUEX
+    //   computed:{
+    //   ...mapGetters({
+    //     footer_sub_menu_id:"get_footer_sub_menu_id",
+    //     get_newer_standard_edition:"get_newer_standard_edition",//新手版1    标准版  2
+    //   }),
+    // },
+    const set_details_item = () => {}
+    const footer_sub_menu_id = computed(() => {
+      return ""
+    })
+    const get_newer_standard_edition = computed(() => {
+      return PageSourceData.get_newer_standard_edition()
+    })
+    onMounted(() => {
+      // #TODO emit
+      // useMittOn(MITT_TYPES.EMIT_XU_NI_TY_STANDARD_ODD_STATUS,odd_pan_handle);
+    })
+    onUnmounted(() => {
+      // #TODO emit
+    // useMittOn(MITT_TYPES.EMIT_XU_NI_TY_STANDARD_ODD_STATUS,odd_pan_handle)
+    })
+    /**
+     * 切换赛事
+     * @param {Number} i 赛事下标
+     * @return {Undefined}
+     */
+    const switch_match_handle = (i) => {
+      component_data.selected_match_i = i;
+      // #TODO emit
+      // $emit('switch_match',i);
+    };
+    /**
+     * 赔率滑动状态
+     */
+    const odd_pan_handle = (status) => {
+      component_data.standard_odd_status = status;
+    };
+    watch(
+      () => props.v_menu_changed,
+      () => {
+        component_data.standard_odd_status = 0;
+      }
+    );
+    watch(
+      () => props.virtual_match_list,
+      () => {
+        if(!virtual_match_list || !virtual_match_list.length) return;
+        component_data.v_match_hps = virtual_match_list[0].hps
+        switch_match_handle(component_data.selected_match_i);
+
+        // #TODO emit 
+        // $emit('switch_match',selected_match_i);
+      }
+    );
+    return {
+      ...toRefs(component_data),
+      footer_sub_menu_id,
+      get_newer_standard_edition,
+      switch_match_handle,
+      odd_pan_handle,
+      set_details_item,
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.match-list-wrapper {
+  position: relative;
+  width: 100%;
+  height: auto;
+  padding-top: 0.08rem;
+
+  &.standard {
+    padding-top: 0;
+  }
+
+  .title-wrap-standard {
+    width: 3.61rem;
+    height: 0.24rem;
+    margin: 0 auto;
+
+    .odd-title-wrapper {
+      width: 1.92rem;
+      height: 100%;
+      overflow: hidden;
+    }
+
+    .odd-t-w-inner {
+      width: 2.52rem;
+      height: 100%;
+      float: right;
+      flex-wrap: nowrap;
+      -webkit-transition: transform 0.2s;
+
+      &.status2 {
+        -webkit-transform: translateX(-1.84rem);
+      }
+
+      & > div {
+        width: 0.6rem;
+        text-align: center;
+        font-size: 0.1rem;
+        line-height: 1;
+        margin-right: 0.03rem;
+      }
+    }
+  }
+}
+</style>
