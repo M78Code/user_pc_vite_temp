b1d40cdc090cd2a7eda73626a026d52ea73b4824
lowen <pmtylowen@itcom888.com>
2023-10-13 13:40:32 +0800

    project修改入口组件

--- project/yazhou-pc/src/pages/match-results/match-results.vue
+++ project/yazhou-pc/src/pages/match-results/match-results.vue
@@ -1,11 +1,11 @@
 <!-- 赛果 -->
 <template>
- <div>
- 
- </div>
+  <match-results></match-results>
 </template>
 <script setup>
+import  matchResults from "src/base-pc/pages/match-results/match-results.vue";
 </script>
+
 <style scoped>
  
 </style>
