7378a424ec38b1b13171569feaa24a3f509cc717
jinnian <jinnian@123.cc>
2023-10-22 19:32:55 +0800

    复制 文件

--- project/yazhou-h5/src/css/server-img/sprite-img/module/pc-left-menu-bg-image.js
+++ project/yazhou-h5/src/css/server-img/sprite-img/module/pc-left-menu-bg-image.js
@@ -2,9 +2,13 @@
 // img-bg-menu-live 对应输出的css名称
 // 
 
+import server_resource from "app/job/output/assets/index.json";
+import UserCtr from "src/core/user-config/user-ctr.js";
+
+import { get } from "lodash";
 const { CURRENT_ENV } = window.BUILDIN_CONFIG;
 const config = {
-  default: "pc-left-menu-bg-image",
+  default:"pc-left-menu-bg-image",
   // local_dev: "pc-left-menu-bg-image",
   // local_test: "pc-left-menu-bg-image",
   // local_ylcs: "pc-left-menu-bg-image",
@@ -12,7 +16,7 @@ const config = {
   // idc_sandbox: "pc-left-menu-bg-image",
   // idc_lspre: "pc-left-menu-bg-image",
   // idc_online: "pc-left-menu-bg-image",
-
+ 
 };
 // x y 
 const item = {
@@ -99,11 +103,18 @@ function compute_position(position) {
  * @param {*} param0
  * @returns
  */
-function compute_css(position) {
-
-  return {
+function compute_css({ position, theme  ,path  }) {
+  // 当前主题的 服务端配置
+   let theme_config=   server_resource[theme] ||{}
+    //最终资源键 计算
+    let final_key = ''  
+    final_key =   config[CURRENT_ENV] || config['default']
+   //从打包的 环境拿 图片地址
+   let url =theme_config[final_key] ||'';
+   return  path?{url}:{
+    "background-image": `url(${url})`,
     "background-position": compute_position(position),
   };
 }
 
-export default compute_css;
+export { compute_css };
