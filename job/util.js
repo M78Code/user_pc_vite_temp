418c67f43720a02e00154b95f85772c809654d2c
jinnian <jinnian@123.cc>
2023-10-23 16:39:28 +0800

    动态导入

--- job/util.js
+++ job/util.js
@@ -2,9 +2,7 @@ import { readFile } from "fs/promises";
 export const import_json_data = async (json_data_path) => {
   // const  { default:json_data} = await import( json_data_path, { assert: { type: "json" }, } );
   //  return json_data
-  const json_data = JSON.parse(
-    await readFile(new URL(json_data_path, import.meta.url))
-  );
+  const json_data = JSON.parse( await readFile(new URL(json_data_path, import.meta.url)) );
   return json_data;
 };
 
