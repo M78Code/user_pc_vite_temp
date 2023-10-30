7b250a0267febc4b26959be24782a4204c90577e
jinnian <jinnian@123.cc>
2023-10-30 21:55:20 +0800

    1

---
+++ src/base-h5/components/match-container/main/match-container-main-template3.vue
@@ -0,0 +1,82 @@
+
+<!--
+ * @Description: 冠军赛事组件，用于赛事列表展示赛事信息
+-->
+<template>
+  <div class="champion-wrap" v-if="is_show">
+    <!--体育类别  menuType 1:滚球 2:即将开赛 3:今日 4:早盘 11:串关 -->
+    <div class="sport-title match-indent" v-if="get_sport_show(i)" @click="handle_ball_seed_fold">
+      <span class="score-inner-span"> {{match_of_list.csna}} </span>
+      <div class="collapse-dire">
+        <img class="icon-down-arrow" :class="{ 'collapsed': league_collapsed }" :src='compute_img_url("icon-collapse")' />
+      </div>
+    </div>
+    <div v-if="is_show_league(i)" class="league-container flex items-center justify-between hairline-border" @click="handle_league_fold">
+      <div class="league-wrapper champion flex items-center">
+        <div @click.stop="handle_match_collect" class="collect-img">
+          <!-- 未收藏图标 -->
+          <img v-if="!match_collect_state" :src="compute_img_url('icon-favorite')" alt="">
+          <!-- 收藏图标 -->
+          <img v-if='match_collect_state' :src="compute_img_url('icon-favorite-s')">
+        </div>
+        <div v-if="menu_type === 100 && GlobalAccessConfig.get_collectSwitch()"  class="favorite" :class="[{favorited:match_of_list.tf},theme]"
+          @click.stop="handle_league_fold"></div>
+            <div class="league-title-text row justify-between" :class="{'without-collect': menu_type !== 100 || (menu_type === 100 && !GlobalAccessConfig.get_collectSwitch())}" >
+              {{menu_type == 100 ? match_of_list.onTn : match_of_list.tn}}
+            </div>
+        </div>
+
+      <div class="collapse-dire">
+        <img class="icon-down-arrow" :class="{collapsed:collapsed}" alt="" :src="compute_img_url('icon-collapse')">
+      </div>
+    </div>
+
+    <template v-for="(hp,index) of match_of_list.hps">
+      <div class="hps-wrap hairline-border" v-if="hp.hs != 2 && !collapsed" :key="index">
+       
+        <div class="match-title flex items-center"
+          :class="{'is-favorite':get_show_favorite_list}">
+          <div class="debug-head" style="color:red;position:absolute;right:0;">
+            {{get_key_by_obg(hp)}}
+          </div>
+          <div class="hpn-wrap ellipsis">
+            {{hp.hps}}
+          </div>
+        </div>
+
+        <div v-if="!collapsed && hp.hmed" class="limit-time flex items-center justify-center"
+          :class="{'first-t':index == 0}">
+          <div class="limit-t-i row justify-center items-center">
+            <template v-if="!['zh', 'tw'].includes(lang)">
+              {{(new Date(+hp.hmed)).Format(i18n_t('time7'))}} {{ $t('match_main.cut_off')}}
+            </template>
+            <template v-else>
+              {{(new Date(+hp.hmed)).Format(i18n_t('time7'))}} {{ $t('match_main.cut_off')}}
+            </template>
+          </div>
+        </div>
+
+        <div class="ol-list-wrap flex justify-start" :data-ol="hp.ol.length" v-if="hp.ol">
+
+          <odd-item-champion 
+            v-for="(ol_item,i) of hp.ol"
+            :key="i"
+            :hs="hp.hs"
+            :data-i="i"
+            :ol_item="ol_item"
+            :style="calc_bgcolor(ol_item)"
+            :class="calc_bgcolor(ol_item) && 'active-item'"
+            :csid="match_of_list.csid"
+            @click.stop="item_click(match_of_list,hp,ol_item)">
+          </odd-item-champion>
+        </div>
+      </div>
+    </template>
+
+  </div>
+</template>
+
+<style scoped lang="scss">
+  @import "../styles/match-container-champion.scss";
+</style>
+../../match-list/components/odd-item-champion.vue
--- src/base-h5/components/match-container/mixins/champion.mixin.js
+++ src/base-h5/components/match-container/mixins/champion.mixin.js
@@ -1,89 +1,10 @@
-<!--
- * @Description: 冠军赛事组件，用于赛事列表展示赛事信息
--->
-<template>
-  <div class="champion-wrap-2" v-if="is_show">
-    <!--体育类别  menuType 1:滚球 2:即将开赛 3:今日 4:早盘 11:串关 -->
-    <div class="sport-title match-indent" v-if="get_sport_show(i)" @click="handle_ball_seed_fold">
-      <span class="score-inner-span"> {{match_of_list.csna}} </span>
-      <div class="collapse-dire">
-        <img class="icon-down-arrow" :class="{ 'collapsed': league_collapsed }" :src='compute_img_url("icon-collapse")' />
-      </div>
-    </div>
-    <div v-if="is_show_league(i)" 
-      :class="['league-container flex items-center justify-between right-border', {collapsed: !collapsed}]"
-      @click="handle_league_fold">
-      <div class="league-wrapper champion flex items-center">
-        <!-- <div @click.stop="handle_match_collect" class="collect-img">
-          <img v-if="!match_collect_state" :src="compute_img_url('icon-favorite')" alt="">
-          <img v-if='match_collect_state' :src="compute_img_url('icon-favorite-s')">
-        </div> -->
-        <div v-if="menu_type === 100 && GlobalAccessConfig.get_collectSwitch()"  class="favorite" :class="[{favorited:match_of_list.tf},theme]"
-          @click.stop="handle_league_fold"></div>
-            <span class="league-title-text row justify-between" :class="{'without-collect': menu_type !== 100 || (menu_type === 100 && !GlobalAccessConfig.get_collectSwitch())}" >
-              {{menu_type == 100 ? match_of_list.onTn : match_of_list.tn}}
-            </span>
-        </div>
-
-      <div class="collapse-dire">
-        <!-- <img class="icon-down-arrow" :class="{ 'collapsed': league_collapsed }" :src='compute_img_url("icon-collapse")' /> -->
-        <icon-wapper color="#c9c9c9" name="icon-arrow" size="15px" :class="['icon-wapper', {'collapsed': !collapsed}]" />
-      </div>
-    </div>
-
-    <template v-for="(hp,index) of match_of_list.hps">
-      <div class="hps-wrap hairline-border" v-if="hp.hs != 2 && !collapsed" :key="index">
-       
-        <div class="flex items-center justify-between"
-          :class="{'is-favorite':get_show_favorite_list}">
-          <div class="match-title items-center">
-            <div class="debug-head" style="color:red;position:absolute;right:0;">
-              {{get_key_by_obg(hp)}}
-            </div>
-            <div class="hpn-wrap ellipsis">
-              {{hp.hps}}
-            </div>
-          </div>
-          <div v-if="!collapsed && hp.hmed" class="limit-time">
-            <div class="limit-t-i">
-              <template v-if="!['zh', 'tw'].includes(lang)">
-                {{(new Date(+hp.hmed)).Format(i18n_t('time7'))}} {{ $t('match_main.cut_off')}}
-              </template>
-              <template v-else>
-                {{(new Date(+hp.hmed)).Format(i18n_t('time7'))}} {{ $t('match_main.cut_off')}}
-              </template>
-            </div>
-          </div>
-        </div>
-
-        
-
-        <div class="ol-list-wrap flex justify-start" :data-ol="hp.ol.length" v-if="hp.ol">
-
-          <odd-item-champion 
-            v-for="(ol_item,i) of hp.ol"
-            :key="i"
-            :hs="hp.hs"
-            :data-i="i"
-            :ol_item="ol_item"
-            :style="calc_bgcolor(ol_item)"
-            :class="calc_bgcolor(ol_item) && 'active-item'"
-            :csid="match_of_list.csid"
-            @click.stop="item_click(match_of_list,hp,ol_item)">
-          </odd-item-champion>
-        </div>
-      </div>
-    </template>
-
-  </div>
-</template>
  
-<script setup>
+ 
 import { ref, computed, onUnmounted } from "vue";
 import lodash from 'lodash'
 import { i18n_t} from 'src/core/index.js'
 import store from "src/store-redux/index.js";
-import { MenuData,get_odds_active, compute_img_url, MatchDataWarehouse_H5_List_Common as MatchDataBaseH5 } from "src/core/index.js"
+import { MenuData, compute_img_url, MatchDataWarehouse_H5_List_Common as MatchDataBaseH5 } from "src/core/index.js"
 import { useMittOn, useMittEmit, MITT_TYPES } from "src/core/mitt";
 import oddItemChampion from "./odd-item-champion.vue";
 import GlobalAccessConfig  from  "src/core/access-config/access-config.js"
@@ -93,7 +14,6 @@ import MatchMeta from 'src/core/match-list-h5/match-class/match-meta';
 import { normal_img_not_favorite_white } from 'src/base-h5/core/utils/local-image.js'
 import 'src/base-h5/css/pages/match-container-champion.scss'
 import MatchFold from 'src/core/match-fold'
-import { IconWapper } from 'src/components/icon'
 import MatchCollect from 'src/core/match-collect'
 
 const props = defineProps({
@@ -269,10 +189,9 @@ const get_odds_value = (ol_item,hsw) => {
  */
 const item_click = (match,hp,ol_item) => {
   if (!ol_item.ov || ol_item.ov < 101000) return;   //对应没有赔率值或者欧赔小于101000
-  let flag = get_odds_active(0, hp.hs, ol_item.os);
+  let flag = $common.odds.get_odds_active(0, hp.hs, ol_item.os);
   if (flag == 1 || flag == 4) {   //开盘和锁盘可以点击弹起来
-    // bet_click2(match, hp, ol_item);
-    console.error('ssss',ol_item)
+    bet_click2(match, hp, ol_item);
   }
 }
 /**
@@ -296,8 +215,5 @@ onUnmounted(() => {
   unsubscribe()
 })
 
-</script>
  
-<style scoped lang="scss">
-  @import "../styles/match-container-champion-2.scss";
-</style>
+ 
