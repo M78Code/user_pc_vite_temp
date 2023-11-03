075345629a00be98656d1a8e1fae8367cada99a5
lockie <pmtylockie@itcom888.com>
2023-11-03 16:36:42 +0800

    ouzhou-pc ouzhou-h5

---
+++ project/ouzhou-pc/src/css/server-img/other-img/config/index.js
@@ -0,0 +1,88 @@
+// label/key 对应后台 id/name名字
+// img-bg-menu-live 对应输出的css名称
+
+export default {
+  "pc-img-bg-menu-live": {
+    default: "pc-img-bg-menu-live",
+    // local_dev: "image01",
+  },
+  // 投注记录
+  "pc-img-bet-record": {
+    default: "pc-img-bet-record",
+  },
+  // 热门赛事
+  "pc-img-hot-match": {
+    default: "pc-img-hot-match",
+  },
+  // 滚球赛事
+  "pc-img-play-match": {
+    default: "pc-img-play-match",
+  },
+  // layout-deader
+  "pc-img-layout-header": {
+    default: "pc-img-layout-header",
+  },
+  // 今日早盘背景
+  today_menu_bg_1: {
+    default: "today_menu_bg_1",
+  },
+  // 今日早盘选中
+  today_menu_bg_1_active: {
+    default: "today_menu_bg_1_active",
+  },
+  // 今日早盘选中
+  live_text: {
+    default: "live_text",
+  },
+  // 赛事详情 标题TV图片
+  "pc-img-match-info-unfold-open": {
+    default: "pc-img-match-info-unfold-open",
+  },
+  // 赛事详情 标题比分图片
+  "pc-img-match-info-switch2": {
+    default: "pc-img-match-info-switch2",
+  },
+  // 赛事详情 标题直播图片
+  "pc-img-match-info-video0": {
+    default: "pc-img-match-info-video0",
+  },
+  // 赛事详情 标题动画直播图片
+  "pc-img-match-info-animation0": {
+    default: "pc-img-match-info-animation0",
+  },
+  // 赛事列表 直播源icon
+  "pc-img-match-list-video": {
+    default: "pc-img-match-list-video"
+  },
+  // 赛事列表 动画icon
+  "pc-img-match-list-animation": {
+    default: "pc-img-match-list-animation"
+  },
+  // 赛事列表 比分板icon
+  "pc-img-match-list-switch": {
+    default: "pc-img-match-list-switch"
+  },
+  // 电竞 英雄联盟banner
+  "pc-img-esports-100-banner": {
+    default: "pc-img-esports-100-banner"
+  },
+  // 电竞 csgo banner
+  "pc-img-esports-102-banner": {
+    default: "pc-img-esports-102-banner"
+  },
+  // 电竞 王者荣耀banner
+  "pc-img-esports-103-banner": {
+    default: "pc-img-esports-103-banner"
+  },
+  // 电竞 DOTA2 banner
+  "pc-img-esports-101-banner": {
+    default: "pc-img-esports-101-banner"
+  },
+  "pc-img-loading": {
+    default: "pc-img-loading"
+  },
+  //无盘口图片
+  "pc-lock-img-details": {
+    default: "pc-lock-img-details"
+  },
+};
---
+++ project/ouzhou-pc/src/pages/announce/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <announce-wapper></announce-wapper>
+</template>
+
+<script setup>
+import { AnnounceWapper } from 'src/components/announce'
+
+</script>
