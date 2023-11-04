fba5bc229e683f5cc32d9c648267b4675d2097aa
lane <jstylane@itcom888.com>
2023-11-04 11:45:41 +0800

    欧洲版-top

--- src/base-h5/components/top-menu/full-version-wapper/wapper.js
+++ src/base-h5/components/top-menu/full-version-wapper/wapper.js
@@ -1,11 +1,11 @@
-import FooterOuzhou1 from "../footer-ouzhou-1/index.vue";
+import TopMenuOuzhou1 from "../top-menu-ouzhou-1/index.vue";
 import { compute_component_wapper_config } from "app/job/use-output/index.js";
 const all_components_obj = {
-  FooterOuzhou1,
+  TopMenuOuzhou1,
 };
 const component_config = {
-  registered_component_key: "FooterMenu",
-  default_component_key: "FooterOuzhou1",
+  registered_component_key: "TopMenu",
+  default_component_key: "TopMenuOuzhou1",
   all_components_obj,
 };
 const wapper_config = compute_component_wapper_config(component_config);
