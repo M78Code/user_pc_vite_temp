677d68cef9850d49d5d4dad968bdb1b8e535700e
oba <pmtyoba@itcom888.com>
2023-11-04 21:18:02 +0800

    欧洲版投注 -01

--- src/base-h5/components/bet/full-version-wapper/wapper.js
+++ src/base-h5/components/bet/full-version-wapper/wapper.js
@@ -1,9 +1,11 @@
 import BetBoxH5_1 from "../bet-box-h5-1/bet_mix_box.vue";
 import BetBoxAppH5_1 from "../bet-box-app-h5-1/bet_mix_box.vue";
+import BetOuzhouH5 from "../bet-ouzhou-h5/bet_mix_box.vue";
 import { compute_component_wapper_config } from "app/job/use-output/index.js";
 const all_components_obj = {
   BetBoxH5_1,
   BetBoxAppH5_1,
+  BetOuzhouH5
 };
 const component_config = {
   registered_component_key: "BetBox",
