2769d0200bba1e00f4a12eb13801000763f617eb
jinnian <jinnian@123.cc>
2023-11-04 21:26:53 +0800

    1

--- dev-target-env.js
+++ dev-target-env.js
@@ -30,7 +30,7 @@ let  DEV_TARGET_VERSION = ''
 
 //----------------------------------分割线  复刻版-H5-KYAPP  app-h5  5 -------------------------------------------
 // 测试环境  
-DEV_TARGET_VERSION = "project_5-652e267f1ca49737811219ef-1698460136471-test";
+// DEV_TARGET_VERSION = "project_5-652e267f1ca49737811219ef-1698460136471-test";
 // 试玩环境  
 // DEV_TARGET_VERSION = "project_5-652e267f1ca49737811219ef-1698742048199-shiwan";
 // 生产环境
@@ -49,7 +49,7 @@ DEV_TARGET_VERSION = "project_5-652e267f1ca49737811219ef-1698460136471-test";
 
 //----------------------------------欧洲版-PC  ouzhou-pc 7-------------------------------------------
 // 测试环境  
-//  DEV_TARGET_VERSION = "project_7-6544b0f3fb906b00ad5e8733-1699000611383-test";
+ DEV_TARGET_VERSION = "project_7-6544b0f3fb906b00ad5e8733-1699000611383-test";
 // 试玩环境  
 // DEV_TARGET_VERSION = "project_7-6544b0f3fb906b00ad5e8733-1699003372610-shiwan";
 // 生产环境
