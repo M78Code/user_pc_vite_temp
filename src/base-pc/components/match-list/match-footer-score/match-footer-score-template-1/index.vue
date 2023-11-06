dabd778aec4734cacbff051a3d1b4fe6f77421c7
jiffy <jiffy@it888.com>
2023-11-06 20:25:16 +0800

    次要玩法数据修改

--- src/base-pc/components/match-list/match-footer-score/match-footer-score-template-1/index.vue
+++ src/base-pc/components/match-list/match-footer-score/match-footer-score-template-1/index.vue
@@ -90,7 +90,7 @@ const props = defineProps({
   },
 })
 
-let match_style_obj = MatchListCardDataClass.get_card_obj_bymid(props.match.mid)
+let match_style_obj = MatchListCardDataClass.all_card_obj[props.match.mid+'_']
 // 赛事模板宽度
 const match_list_tpl_size = MATCH_LIST_TEMPLATE_CONFIG[`template_${match_style_obj.data_tpl_id}_config`].width_config
 
