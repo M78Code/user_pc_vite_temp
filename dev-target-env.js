83dc4234b1d5443d9f90798e8177f6441922b7b0
lane <jstylane@itcom888.com>
2023-11-06 12:00:10 +0800

    投注format

--- dev-target-env.js
+++ dev-target-env.js
@@ -30,9 +30,9 @@ let  DEV_TARGET_VERSION = ''
 
 //----------------------------------分割线  复刻版-H5-KYAPP  app-h5  5 -------------------------------------------
 // 测试环境  
-DEV_TARGET_VERSION = "project_5-652e267f1ca49737811219ef-1699175661579-test";
+// DEV_TARGET_VERSION = "project_5-652e267f1ca49737811219ef-1699175661579-test";
 // 试玩环境  
-// DEV_TARGET_VERSION = "project_5-652e267f1ca49737811219ef-1698742048199-shiwan";
+DEV_TARGET_VERSION = "project_5-652e267f1ca49737811219ef-1698742048199-shiwan";
 // 生产环境
 // DEV_TARGET_VERSION = "project_5-652e267f1ca49737811219ef-1699154069801-online";
 
