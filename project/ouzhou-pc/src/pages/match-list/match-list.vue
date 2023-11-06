5b8b354901f101cac29903e8237d87b51863e49b
elrod <tyelrod@itcom888.com>
2023-11-06 15:17:12 +0800

    欧洲版UI调整

--- project/ouzhou-pc/src/pages/match-list/match-list.vue
+++ project/ouzhou-pc/src/pages/match-list/match-list.vue
@@ -60,7 +60,7 @@
       <!-- </div> -->
     <!-- </div> -->
     <!-- 列表容器 -->
-    <load-data :state="'data'"  >
+    <load-data :state="'data'" limit_height="1000" >  <!--此处先写死高度用来调试UI -->
       <!-- 滚球虚拟体育列表 -->
       <scroll-list v-if="menu_config.menu_root_show_shoucang == 300">
         <template v-slot:before>
