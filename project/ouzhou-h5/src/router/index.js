896e213e89abce28be742ed448497460af171e3b
rise <yrise@it888.com>
2023-11-07 18:48:01 +0800

    菜单逻辑修改

--- project/ouzhou-h5/src/router/index.js
+++ project/ouzhou-h5/src/router/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: rise
  * @Date: 2023-11-03 16:37:52
- * @LastEditors: lowen pmtylowen@itcom888.com
- * @LastEditTime: 2023-11-07 17:23:56
+ * @LastEditors: rise
+ * @LastEditTime: 2023-11-07 18:31:50
  * @Description:  
  */
 import { createRouter, createWebHashHistory } from "vue-router";
@@ -17,14 +17,14 @@ const router = createRouter({
       name: "main",
       component: () => import("../layouts/index.vue"),
       children: [
-        // {
-        //   path: "/inPlay",
-        //   name: "inPlay",
-        //   component: () => import("../pages/in-pay/index.vue"),
-        //   meta: {
-        //     keepAlive: true // 需要缓存
-        //   },
-        // },
+          {
+            path: "/inPlay",
+            name: "inPlay",
+            component: () => import("../pages/in-pay/index.vue"),
+            meta: {
+              keepAlive: true // 需要缓存
+            },
+          },
           {
             path: "/match",
             name: "matchList",
