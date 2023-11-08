ede80d96dab335435748f43fc8ab145f17cbe80e
lane <jstylane@itcom888.com>
2023-11-08 14:04:09 +0800

    设置赛果

--- dev-target-env.js
+++ dev-target-env.js
@@ -34,7 +34,7 @@ let  DEV_TARGET_VERSION = ''
 // 试玩环境  
 // DEV_TARGET_VERSION = "project_4-652ccfacfeffd80f0fcda107-1697770590787-shiwan";
 // 生产环境
-DEV_TARGET_VERSION = "project_4-652ccfacfeffd80f0fcda107-1697972059806-online"
+// DEV_TARGET_VERSION = "project_4-652ccfacfeffd80f0fcda107-1697972059806-online"
 
 //----------------------------------分割线  复刻版-H5-KYAPP  app-h5  5 -------------------------------------------
 // 测试环境  
@@ -68,7 +68,7 @@ DEV_TARGET_VERSION = "project_5-652e267f1ca49737811219ef-1699239723632-test";
  
 //----------------------------------欧洲版-H5  ouzhou-h5 8-------------------------------------------
 // 测试环境  
-DEV_TARGET_VERSION = "project_8-6544b116720143009a2f282d-1699000607498-test";
+// DEV_TARGET_VERSION = "project_8-6544b116720143009a2f282d-1699000607498-test";
 // 试玩环境  
 // DEV_TARGET_VERSION = "project_8-6544b116720143009a2f282d-1699003362037-shiwan";
 // 生产环境
