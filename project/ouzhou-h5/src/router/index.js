da6f757347f31bea37de2f924ed1b004cf1701d5
land <land@itcom888.com>
2023-11-10 16:47:17 +0800

    feat:欧洲版h5新增赛事列表页-新ui

--- project/ouzhou-h5/src/router/index.js
+++ project/ouzhou-h5/src/router/index.js
@@ -5,55 +5,63 @@ const router = createRouter({
     {
       path: "/",
       redirect: {
-        name: 'home'
+        name: "home",
       },
       name: "main",
       component: () => import("../layouts/index.vue"),
       children: [
-          {
-            path: "/inPlay",
-            name: "inPlay",
-            component: () => import("../pages/in-play/index.vue"),
-            meta: {
-              keepAlive: true // 需要缓存
-            },
-          },
-          {
-            path: "/match",
-            name: "matchList",
-            component: () => import("../pages/match-page/index.vue"),
-            meta: {
-              keepAlive: true // 需要缓存
-            },
-          },
-          {
-            path: "/handicap",
-            name: "handicapTutorial",
-            component: () => import("../pages/handicap-tutorial/index.vue"),
-            meta: {
-              keepAlive: true // 需要缓存
-            },
-          },
-          {
-            path: "/matchResults",
-            name: "matchResults",
-            component: () => import("../pages/match-results/index.vue"),
-            meta: {
-              keepAlive: true // 需要缓存
-            },
-          },
-          {
-            path: "/home",
-            name: "home",
-            component: () => import("../pages/home/index.vue"),
-          },
-          // {
-          //   path: "/activity_task",
-          //   name: "activity_task",
-          //   component: () => import("../activity-page/activity-task/index.vue")
-          // },
         {
-          path: "/virtual",  // vr体育
+          path: "/inPlay",
+          name: "inPlay",
+          component: () => import("../pages/in-play/index.vue"),
+          meta: {
+            keepAlive: true, // 需要缓存
+          },
+        },
+        {
+          path: "/match",
+          name: "matchList",
+          component: () => import("../pages/match-page/index.vue"),
+          meta: {
+            keepAlive: true, // 需要缓存
+          },
+        },
+        {
+          path: "/match2",
+          name: "matchList",
+          component: () => import("../pages/match/index.vue"),
+          meta: {
+            keepAlive: true, // 需要缓存
+          },
+        },
+        {
+          path: "/handicap",
+          name: "handicapTutorial",
+          component: () => import("../pages/handicap-tutorial/index.vue"),
+          meta: {
+            keepAlive: true, // 需要缓存
+          },
+        },
+        {
+          path: "/matchResults",
+          name: "matchResults",
+          component: () => import("../pages/match-results/index.vue"),
+          meta: {
+            keepAlive: true, // 需要缓存
+          },
+        },
+        {
+          path: "/home",
+          name: "home",
+          component: () => import("../pages/home/index.vue"),
+        },
+        // {
+        //   path: "/activity_task",
+        //   name: "activity_task",
+        //   component: () => import("../activity-page/activity-task/index.vue")
+        // },
+        {
+          path: "/virtual", // vr体育
           name: "virtual_sports",
           component: () => import("../pages/virtual/index.vue"),
         },
@@ -77,21 +85,19 @@ const router = createRouter({
           name: "notice",
           component: () => import("../pages/notice/index.vue"),
         },
-       
+
         {
           path: "/rule_description/:name",
           name: "rule_description",
           component: () =>
-            import(
-              "../pages/rule-description/description-main.vue"
-            ),
+            import("../pages/rule-description/description-main.vue"),
         },
-          // {
-          //   path: '/category_loading/:mid',
-          //   name: 'category_loading',
-          //   component: () => import("。。/pages/details/category-loading.vue")
-          // },
-          // 常规赛事和电竞赛事详情页,赛事id必传，玩法集id可选
+        // {
+        //   path: '/category_loading/:mid',
+        //   name: 'category_loading',
+        //   component: () => import("。。/pages/details/category-loading.vue")
+        // },
+        // 常规赛事和电竞赛事详情页,赛事id必传，玩法集id可选
         {
           path: "/details/:mid/:csid?/:mcid?",
           name: "category",
@@ -105,81 +111,83 @@ const router = createRouter({
           //   },
           // ],
         },
-          // 赛果详情页
-          {
-            path: "/result_details",
-            name: "result_details",
-            component: () => import("../pages/details/result-details.vue"),
-            children: [
-              {
-                path: "/result_details/:index",
-                name: "match_result",
-                component: () => import("../pages/details/components/result-fat-tab.vue")
-              }
-            ]
-          },
+        // 赛果详情页
+        {
+          path: "/result_details",
+          name: "result_details",
+          component: () => import("../pages/details/result-details.vue"),
+          children: [
+            {
+              path: "/result_details/:index",
+              name: "match_result",
+              component: () =>
+                import("../pages/details/components/result-fat-tab.vue"),
+            },
+          ],
+        },
         //   // 虚拟赛事详情页
-          {
-            path: "/virtual_sports_details",
-            name: "virtual_sports_details",
-            component: () => import("../pages/details/virtual-sports-details.vue"),
-            children: [
-              {
-                path: "virtual_sports_category/:id?",
-                name: "virtual_sports_category",
-                component: () => import("../pages/details/children/virtual-sports-category.vue")
-              },
-            ]
-          },
-          // 投注列表
-          {
-            path: "/betting_history",
-            name: "betting_history",
-            component: () => import("../pages/cathectic/index.vue"),
-          },
-          // 个人中心
-          {
-            path: "/personal",
-            name: "personal",
-            component: () => import("../pages/personal/index.vue"),
-            meta: {
-              keepAlive: true // 需要缓存
+        {
+          path: "/virtual_sports_details",
+          name: "virtual_sports_details",
+          component: () =>
+            import("../pages/details/virtual-sports-details.vue"),
+          children: [
+            {
+              path: "virtual_sports_category/:id?",
+              name: "virtual_sports_category",
+              component: () =>
+                import("../pages/details/children/virtual-sports-category.vue"),
             },
+          ],
+        },
+        // 投注列表
+        {
+          path: "/betting_history",
+          name: "betting_history",
+          component: () => import("../pages/cathectic/index.vue"),
+        },
+        // 个人中心
+        {
+          path: "/personal",
+          name: "personal",
+          component: () => import("../pages/personal/index.vue"),
+          meta: {
+            keepAlive: true, // 需要缓存
           },
-          //规则
-          {
-            path: "/rules",
-            name: "rules",
-            component: () => import("../pages/rules/rules.vue"),
-            meta: {
-              keepAlive: true // 需要缓存
-            },
+        },
+        //规则
+        {
+          path: "/rules",
+          name: "rules",
+          component: () => import("../pages/rules/rules.vue"),
+          meta: {
+            keepAlive: true, // 需要缓存
           },
-          //公告
-          // {
-          //   path: "/announcement",
-          //   name: "/announcement",
-          //   component: () => import("../pages/personal/announcement.vue"),
-          //   meta: {
-          //     keepAlive: true // 需要缓存
-          //   },
-          // }
-          {
-              path: "/announcement",
-              name: "announcement",
-              component: () => import("../pages/personal/announcement.vue"),
-              meta: {
-                keepAlive: true // 需要缓存
-              },
-            }
+        },
+        //公告
+        // {
+        //   path: "/announcement",
+        //   name: "/announcement",
+        //   component: () => import("../pages/personal/announcement.vue"),
+        //   meta: {
+        //     keepAlive: true // 需要缓存
+        //   },
+        // }
+        {
+          path: "/announcement",
+          name: "announcement",
+          component: () => import("../pages/personal/announcement.vue"),
+          meta: {
+            keepAlive: true, // 需要缓存
+          },
+        },
       ],
     },
     {
-      path: '/:catchAll(.*)*',
+      path: "/:catchAll(.*)*",
       redirect: {
-        name: 'matchList'
+        name: "matchList",
       },
-      
     },
     //搜索
     {
@@ -187,9 +195,9 @@ const router = createRouter({
       name: "search",
       component: () => import("../pages/search/index.vue"),
       meta: {
-        keepAlive: true // 需要缓存
-      }
-    }
+        keepAlive: true, // 需要缓存
+      },
+    },
   ],
 });
 
