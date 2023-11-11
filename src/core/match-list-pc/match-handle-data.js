2f082e9265de68836e5613b0b2ed29b34076793f
jiffy <jiffy@it888.com>
2023-11-11 20:35:32 +0800

    tpl_id获取错误

--- src/core/match-list-pc/match-handle-data.js
+++ src/core/match-list-pc/match-handle-data.js
@@ -20,10 +20,10 @@ function get_menu_obj_by_menu_id(menu_id) {
    * @param {undefined} undefined
   */
 function get_match_tpl_number(is_hot) {
-    const { left_menu_result = {}, mid_menu_result } = MenuData;
+    const { left_menu_result = {}, mid_menu_result = {} } = MenuData;
     let match_tpl_number = -1
     // 玩法菜单
-    let play_menu = get_menu_obj_by_menu_id(lodash.get(left_menu_result,lv1_mi))
+    let play_menu = get_menu_obj_by_menu_id(lodash.get(left_menu_result, 'lv1_mi'))
     // 详情页热门赛事 或者 搜索 或者列表强力推荐
     if (PageSourceData.route_name == 'details' || PageSourceData.route_name == 'search' || is_hot) {
         match_tpl_number = -1
