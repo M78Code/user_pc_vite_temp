2446c72c699f89b63c44b5524f9f6bab13916150
rise <yrise@it888.com>
2023-11-11 15:24:46 +0800

    赛果数据处理

--- project/ouzhou-h5/src/pages/match-results/index.vue
+++ project/ouzhou-h5/src/pages/match-results/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <scroll-menu menu_type="1" :is_show_badge="false"  v-if="MenuData.menu_list.length" />
+    <scroll-result menu_type="28" :is_show_badge="false" :current_mi="state.current_mi" :menuList="state.slideMenu_sport" @changeMenu="changeMenu"/>
     <div class="match-container">
-        <date-tab :reverse="1"/>
+        <date-tab v-if="state.slideMenu" :defaultVal="state.currentSlideValue"  :dateList="state.slideMenu" @changeDate="changeDate"/>
         <match-container />
     </div>
     <!--  弹窗  -->
@@ -16,16 +16,17 @@
 
 </template>
 <script setup>
-import { watch,onMounted, onBeforeMount, reactive } from "vue";
+import lodash_ from 'lodash'
+import { watch,onMounted, onBeforeMount, reactive,ref,nextTick } from "vue";
 import MatchMeta from "src/core/match-list-h5/match-class/match-meta.js";
 import setectLeague from 'src/base-h5/components/setect-league/index.vue'
 import { scrollMenuEvent } from "src/base-h5/components/menu/app-h5-menu/utils.js"
 import matchContainer from "src/base-h5/components/match-list/index.vue";
 import { i18n_t, compute_css_obj, MenuData } from "src/core/index.js";
 import { is_results, is_kemp } from 'src/base-h5/mixin/menu.js'
-import scrollMenu from 'src/base-h5/components/top-menu/top-menu-ouzhou-1/scroll-menu/scroll-menu.vue';
+import scrollResult from 'src/base-h5/components/top-menu/top-menu-ouzhou-1/scroll-menu/scroll-result.vue';
 import dateTab from 'src/base-h5/components/top-menu/top-menu-ouzhou-1/date-tab/date-tab.vue';
-
+import { api_analysis } from "src/api/"
 // watch(() => MenuData.update_time.value, () => {
 //   console.log("菜单id-球类id-对应euid",`${MenuData.menu_type.value}-${MenuData.menu_mi.value}-${MenuData.get_euid()}`)
 // })
@@ -35,25 +36,103 @@ const inner_height = window.innerHeight;  // 视口高度
 const props = defineProps({})
 const state = reactive({
     select_dialog:false,
+    currentSlideValue:"",
+    slideMenu:[],
+    slideMenu_date: [], // 时间
+    slideMenu_sport: [], // 赛种
 })
-
 const selectFinishHandle = (val) => {
     console.log('选择完成')
     state.select_dialog = false
 }
-
+const changeDate = (item) =>{
+    if (state.currentSlideValue === item.val) return
+    state.currentSlideValue = item.val
+    // set_scroll_data_list(item.sportList)
+     getData(state.slideMenu_sport.filter((item)=>{return item.mi === state.current_mi})[0],lodash_.get(item.subList,'[0].field1', ''))
+}
+const changeMenu = (item) =>{
+    if (state.current_mi === item.mi) return
+    state.current_mi = item.mi;
+    state.slideMenu = item.subList;
+    state.currentSlideValue = lodash_.get(item.subList,'[0].field1', '');
+    getData( item,lodash_.get(item.subList,'[0].field1', ''));
+}
+const switchHandle = async ()=> {
+    const res = await  api_analysis.get_result_menu();
+    //获取 赛果菜单
+    // api_analysis.get_match_result_menu( {menuType:0} ).then( ( res = {} ) => {
+        if(res.code == 200){
+            console.log(222222)
+            console.log(res)
+            let scroll_data = res.data.map( item => {
+                return {
+                    mi: 100+item.sportId*1 + '',
+                    ct: item.count,
+                    md: item.date,
+                    sport: item.sportId,
+                    subList:item.subList.map((n)=>{
+                        return {
+                            val:n.field1,
+                            ...n
+                        }
+                    })
+                }
+            })
+            state.slideMenu_sport = scroll_data
+            state.current_mi = scroll_data[0].mi
+            state.slideMenu = scroll_data[0].subList
+            state.currentSlideValue = lodash_.get(scroll_data[0].subList,'[0].field1', '')
+            // 设置赛种数据
+            // set_scroll_data_list(lodash_.get(res.data,'[0].subList', []))
+            getData( scroll_data[0],lodash_.get(scroll_data[0].subList,'[0].field1', ''))
+        }
+    // })
+}
+// 设置赛种列表
+const set_scroll_data_list = (data_list = []) => {
+    // let scroll_data = data_list.map( item => {
+    //     return {
+    //         mi: 100+item.sportId*1 + '',
+    //         ct: item.count,
+    //         md: item.date,
+    //         sport: item.sportId,
+    //     }
+    // })
+    // state.slideMenu_sport = scroll_data
+    // state.current_mi = scroll_data[0].mi
+    // state.slideMenu = data_list.map((item)=>{
+    //     return {
+    //         val:item.field1,
+    //         ...item
+    //     }
+    // })
+    // // 设置时间默认选中
+    // state.currentSlideValue = lodash_.get(data_list,'[0].field1', '')
+    MenuData.set_result_menu_api_params(scroll_data[0])
+    MatchMeta.get_results_match()
+}
+const getData = (item,date) =>{
+    let params = {
+        mi:item.mi,
+        md:date
+    }
+    MenuData.set_result_menu_api_params(params)
+    MatchMeta.get_results_match()
+}
 onMounted(()=>{
-    set_menu_lv1({mi:28})
+    // set_menu_lv1({mi:28})
+    switchHandle()
 })
 
-const set_menu_lv1 = item => {
-    MenuData.set_current_lv1_menu(item.mi);
-    MenuData.get_menu_lvmi_list(item.mi)
-    // MenuData.get_results_menu();
-    setTimeout(() => {
-        MatchMeta.get_results_match()
-    }, 2000)
-}
+// const set_menu_lv1 = item => {
+//     MenuData.set_current_lv1_menu(item.mi);
+//     MenuData.get_menu_lvmi_list(item.mi)
+//     // MenuData.get_results_menu();
+//     setTimeout(() => {
+//         MatchMeta.get_results_match()
+//     }, 2000)
+// }
 
 // onBeforeMount(() => {
 // })
