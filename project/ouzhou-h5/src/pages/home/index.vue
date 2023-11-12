10b896d5f2fb008da56a4f8f6cc9afee6840e65a
dolphin <pmtydolphin@itcom888.com>
2023-11-12 21:28:43 +0800

    欧洲版  赛事  联调

--- project/ouzhou-h5/src/pages/home/index.vue
+++ project/ouzhou-h5/src/pages/home/index.vue
@@ -199,7 +199,7 @@ const on_update = (val) => {
         background: #fff;
         padding: 0 10px;
         background-repeat: no-repeat;
-        // background-image: url("src/assets/images/featured/mask.png");
+        background-image:url($SCSSPROJECTPATH + "/image/list/mask_group.png");
         background-size: contain;
         background-position: right;
         .q-tab{
