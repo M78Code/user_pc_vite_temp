4179fe6c674860f1a34803dd74feda030060b249
elrod <tyelrod@itcom888.com>
2023-11-12 15:27:55 +0800

    统计分析增加跳转弹窗

--- src/base-pc/components/match-list/match-tpl-new-data/match-tpl-101-after/modules/iconBox/index.vue
+++ src/base-pc/components/match-list/match-tpl-new-data/match-tpl-101-after/modules/iconBox/index.vue
@@ -8,14 +8,27 @@
         <div class="sr-link-icon-w">
             <div :style="compute_css_obj({key: 'pc-home-early-settlement'})"></div>
         </div>
-        <!-- 图标2 -->
+        <!-- 统计分析 -->
         <div class="sr-link-icon-w">
-            <div :style="compute_css_obj({key: 'pc-home-match-analysis'})"></div>
+            <div v-tooltip="{ content: i18n_t('common.analysis') }" 
+            v-if="utils.is_show_sr_flg(match)"
+            @click.stop='details.sr_click_handle(match)'
+            :style="compute_css_obj({key: 'pc-home-match-analysis'})"></div>
         </div>
     </div>
 </template>
 <script setup>
 import { compute_css_obj } from 'src/core/server-img/index.js'
+import { i18n_t } from "src/core/index.js";
+import { utils } from 'src/core/utils/module/utils.js'
+import details from "src/core/match-list-pc/details-class/details.js"
+
+const props = defineProps({
+  match: {
+    type: Object,
+    default: () => { }
+  }
+})
 
 </script>
 <style>
