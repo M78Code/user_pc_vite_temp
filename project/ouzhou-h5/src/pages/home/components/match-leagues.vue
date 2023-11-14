f7c02ff1e21fffa8ed654ca4347938828637bd20
dempsey <dempsey@dempsey.com>
2023-11-14 17:13:34 +0800

    feat:继续完善首页五大赛事相关的工作

--- project/ouzhou-h5/src/pages/home/components/match-leagues.vue
+++ project/ouzhou-h5/src/pages/home/components/match-leagues.vue
@@ -4,18 +4,25 @@
 
 <template>
     <div class="match_leagues_page">
-        <match-container />
+        {{ fiveLeagues_Matches }}
     </div>
 </template>
    
 <script setup>
-import MatchContainer from "src/base-h5/components/match-list/index.vue";
 import MatchMeta from 'src/core/match-list-h5/match-class/match-meta';
 import { onMounted } from "vue"
 
-onMounted(() => {
-    MatchMeta.get_four_leagues_list()
+const props = defineProps({
+    fiveLeagues_Matches: {
+    type: Array,
+    default: () => []
+  }
 })
+
+
+// onMounted(() => {
+//     MatchMeta.get_four_leagues_list()
+// })
 </script>
    
 <style scoped lang="scss"></style>
