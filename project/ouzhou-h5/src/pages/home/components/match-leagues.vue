21f5642cbdbb0193b5c3e0a46050934b5db73d2e
dempsey <dempsey@dempsey.com>
2023-11-14 18:13:36 +0800

    feat:继续完善首页五大联赛

--- project/ouzhou-h5/src/pages/home/components/match-leagues.vue
+++ project/ouzhou-h5/src/pages/home/components/match-leagues.vue
@@ -1,15 +1,25 @@
 <!--
 * @Description: 四大联赛
 -->
-
 <template>
-    <div class="match_leagues_page">
-        {{ fiveLeagues_Matches }}
-    </div>
-</template>
+    <div style="display: none;">{{ MatchDataBaseH5.data_version.version }}</div>
+    <section class="match-play-page">
+      <div class="match-item" v-for="item, index in fiveLeagues_Matches" :key="mid">
+       <template v-if="item">
+         <MatchContainerMainTemplate1
+          :i="index"
+          :match_of_list="item">
+        </MatchContainerMainTemplate1>
+       </template>
+      </div>
+    </section>
+  </template>
    
 <script setup>
 import MatchMeta from 'src/core/match-list-h5/match-class/match-meta';
+import { MenuData,  MatchDataWarehouse_ouzhou_PC_five_league_List_Common as MatchDataBaseFiveLeagueH5} from "src/core/index.js";
+import MatchContainerMainTemplate1 from "src/base-h5/components/match-container/template/ouzhou/match-container-main-template1.vue"; 
+
 import { onMounted } from "vue"
 
 const props = defineProps({
@@ -18,6 +28,10 @@ const props = defineProps({
     default: () => []
   }
 })
+
+const get_match_item = (mid) => {
+  return MatchDataBaseFiveLeagueH5.get_quick_mid_obj(mid)
+}
 
 
 // onMounted(() => {
