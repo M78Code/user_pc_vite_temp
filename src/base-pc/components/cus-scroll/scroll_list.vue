bbe89590506b3470b3f1edc3f089158f1ec36c4a
jiffy <jiffy@it888.com>
2023-11-17 19:07:48 +0800

    切换菜单 滚动到顶部

--- src/base-pc/components/cus-scroll/scroll_list.vue
+++ src/base-pc/components/cus-scroll/scroll_list.vue
@@ -8,7 +8,7 @@
     <!-- 滚动区域 -->
     <div
       class="list-scrollbar router_scroll_layout"
-      ref="area"
+      ref="area_ref"
       @scroll="on_scroll"
     >
       <div class="v-scroll-content relative-position">
@@ -59,7 +59,7 @@ const scrollHeight = ref(0);
 const area_height = ref(0);
 const is_bootom_height = ref(0);
 
-const area_obj = ref('area')
+const area_ref = ref()
 
 
 /**
@@ -70,11 +70,11 @@ const area_obj = ref('area')
  */
  const set_scrollTop = (top, type) => {
   if (type == "inc") {
-    area_obj.value.scrollTop += top;
+    area_ref.value.scrollTop += top;
   } else if (type == "dec") {
-    area_obj.value.scrollTop -= top;
+    area_ref.value.scrollTop -= top;
   } else {
-    area_obj.value.scrollTop = top;
+    area_ref.value.scrollTop = top;
   }
 };
 
@@ -90,7 +90,7 @@ onUnmounted(() => {
   // this.debounce_throttle_cancel(this.update_list_card_offset);
 });
 onMounted(() => {
-  area_height.value = area_obj.value.offsetHeight;
+  area_height.value = area_ref.value.offsetHeight;
   is_mounted.value = true;
   // let { status, height } = this.get_retain_scroll_obj;
   // if (status) {
@@ -157,7 +157,7 @@ const update_list_card_offset = lodash.throttle((e) => {
  * @param {undefined} undefined
  */
 const scroll_height_change = () => {
-  scrollHeight.value = area_obj.value.scrollHeight;
+  scrollHeight.value = area_ref.value.scrollHeight;
   is_bootom_height.value = scrollHeight.value - area_height.value - 500;
   has_thumb.value = scrollHeight.value > area_height.value;
 };
