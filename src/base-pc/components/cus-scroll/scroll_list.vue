056fb73130fecd8fcb5c0b2cae1cbbd2f7c5ace0
lockie <pmtylockie@itcom888.com>
2023-11-17 17:17:06 +0800

    1

--- src/base-pc/components/cus-scroll/scroll_list.vue
+++ src/base-pc/components/cus-scroll/scroll_list.vue
@@ -60,6 +60,7 @@ const area_height = ref(0);
 const is_bootom_height = ref(0);
 
 const area_obj = ref('area')
+let mitt_list = [];
 
 
 /**
@@ -78,11 +79,9 @@ const area_obj = ref('area')
   }
 };
 
-// 设置列表滚动条位置
-useMittOn(MITT_TYPES.EMIT_SET_MATCH_LIST_SCROLL_TOP, set_scrollTop);
+
 onUnmounted(() => {
-  // 设置列表滚动条位置
-  useMittOn(MITT_TYPES.EMIT_SET_MATCH_LIST_SCROLL_TOP, set_scrollTop).off();
+  mitt_list.forEach(i => i());
   // this.debounce_throttle_cancel(this.on_bootom);
   // this.debounce_throttle_cancel(this.emit_on_scroll);
   // this.debounce_throttle_cancel(this.update_list_card_offset);
@@ -90,6 +89,10 @@ onUnmounted(() => {
 onMounted(() => {
   area_height.value = area_obj.value.offsetHeight;
   is_mounted.value = true;
+  // 设置列表滚动条位置
+  mitt_list = [
+    useMittOn(MITT_TYPES.EMIT_SET_MATCH_LIST_SCROLL_TOP, set_scrollTop).off,
+  ]
   // let { status, height } = this.get_retain_scroll_obj;
   // if (status) {
   //   nextTick(() => {
@@ -140,15 +143,10 @@ const on_scroll = (e) => {
   // 设置列表滚动条scrollTop
   MatchListScrollClass.set_scroll_top(scrollTop);
   // 更新列表卡片偏移量
-  update_list_card_offset();
+  lodash.throttle(() => {
+    MatchListCard.set_card_show_level(scrollTop);
+  }, 300)
 };
-/**
- * @Description 更新列表卡片偏移量
- * @param {undefined} undefined
- */
-const update_list_card_offset = lodash.throttle(() => {
-  MatchListCard.set_card_show_level();
-}, 50);
 
 /**
  * @Description 滚动高度改变事件(容器高度变化回调函数)
