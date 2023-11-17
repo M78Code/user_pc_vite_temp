592ea773e40ceddf6eee09932d439f3e963db5a3
jiffy <jiffy@it888.com>
2023-11-17 14:42:35 +0800

    修复 赛事为空

--- src/core/match-list-pc/composables/match-list-processing.js
+++ src/core/match-list-pc/composables/match-list-processing.js
@@ -262,7 +262,7 @@ const mx_use_list_res_when_code_200_and_list_length_gt_0 = ({ match_list, collec
 	all_league_list.push(...lodash.get(match_list, "livedata", []));
 	all_league_list.push(...lodash.get(match_list, "nolivedata", []));
 	deal_with_list_data(all_league_list)
-	// MatchListData.set_list(match_list)
+	MatchListData.set_list(match_list)
 	// 计算赛事卡片
 	MatchListCardClass.compute_match_list_style_obj_and_match_list_mapping_relation_obj(
 		match_list,
@@ -383,6 +383,7 @@ const mx_use_list_res_when_code_error_or_list_length_0 = ({ match_list, collect,
  * @return {undefined} undefined
  */
 const mx_use_list_res = (data, backend_run, cut, collect) => {
+	console.log('mx_use_list_res',data,data.data.map(i=>i.mid))
 	let code = lodash.get(data, "code");
 	clearTimeout(virtual_list_timeout_id);
 	// 赛事列表
