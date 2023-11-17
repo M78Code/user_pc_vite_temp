081dcd556c1fcb5f3cd23babd259c33cb082494d
Jacques <jacques@itcom888.com>
2023-11-17 19:29:32 +0800

    chore(mitt):为useMittOn提供精确的类型定义与代码建议

--- types/mitt.module.ts
+++ types/mitt.module.ts
@@ -3,11 +3,18 @@ import * as MITT_TYPES_DEFAULT from "src/core/mitt/mitt-keys";
 
 declare module 'src/core/mitt/index.js' {
   export type MittType = (typeof MITT_TYPES_DEFAULT & typeof MITT_TYPES_PROJECT);
-  // /** 激发MittEmit事件 */
+  /** 激发MittEmit事件 */
   function useMittEmit(type: keyof MittType): void;
-  // /** 激发RefreshDetails时, 携带params 
-  //  * @param {RefreshDetailsParam} pyload
-  // */
-  function useMittEmit(type: typeof MITT_TYPES_PROJECT.EMIT_REFRESH_DETAILS,param: { mid: string, csid: string, tid: string }): void;
+
+  interface EmitRefreshDetailsParamType { 
+    /** 赛事ID */
+    mid: string, 
+    csid: string,
+    /** 联赛ID */ 
+    tid: string 
+  }
+  /** 激发RefreshDetails时, 携带params */
+  function useMittEmit(type: typeof MITT_TYPES_PROJECT.EMIT_REFRESH_DETAILS, param: { mid: string, csid: string, tid: string }): void;
+  function useMittOn(type: typeof MITT_TYPES_PROJECT.EMIT_REFRESH_DETAILS, callback: (param: EmitRefreshDetailsParamType) => void): MITT.UseMittOnResult;
 
 }
