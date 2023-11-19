b3dada5ced061f06a7cd53a5e6453b861922de3b
elrod <tyelrod@itcom888.com>
2023-11-19 20:59:42 +0800

    修改bug48079

--- dev-target-env.js
+++ dev-target-env.js
@@ -52,7 +52,7 @@ DEV_TARGET_VERSION = "project_4-652ccfacfeffd80f0fcda107-1697972059806-online"
 // 测试环境  
   // DEV_TARGET_VERSION = "project_7-6544b0f3fb906b00ad5e8733-1699598524474-test";
 // 试玩环境  
-DEV_TARGET_VERSION = "project_7-6544b0f3fb906b00ad5e8733-1700392615807-shiwan";
+DEV_TARGET_VERSION = "project_7-6544b0f3fb906b00ad5e8733-1700398288281-shiwan";
 
 // 生产环境
 // DEV_TARGET_VERSION = "project_7-6544b0f3fb906b00ad5e8733-1699003377370-online";
