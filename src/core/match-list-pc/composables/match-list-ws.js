5aaecf24201545ea64685a704704e9e951401e58
jiffy <jiffy@it888.com>
2023-11-20 20:42:44 +0800

    ws推送问题修改

--- src/core/match-list-pc/composables/match-list-ws.js
+++ src/core/match-list-pc/composables/match-list-ws.js
@@ -7,9 +7,12 @@ import { api_bymids } from "./match-list-featch.js";
 // import { fetch_match_list } from '../match-list-composition.js'
 import { useMittEmit, MITT_TYPES, MenuData, MatchDataWarehouse_PC_List_Common } from 'src/core/index.js';
 import { socket_remove_match } from "src/core/match-list-pc/match-list-composition.js";
-
+const bymids = lodash.debounce((mids, MatchListData) => {
+	api_bymids({ mids }, null, MatchListData)
+}, 300);
 function use_match_list_ws(MatchListData = MatchDataWarehouse_PC_List_Common) {
 	let mids = []
+	console.log('use_match_list_ws', MatchListData.name_code, mids)
 	let message_fun = ws_message_listener.ws_add_message_listener((cmd, data) => {
 		// 赛事新增
 		if (["C109"].includes(cmd)) {
@@ -27,7 +30,8 @@ function use_match_list_ws(MatchListData = MatchDataWarehouse_PC_List_Common) {
 		}
 		// 调用 mids  接口 303是盘口赔率变更
 		if (["C303", "C114"].includes(cmd)) {
-			api_bymids({ mids }, null, MatchListData);
+			console.log('bymids', MatchListData.name_code,Date.now())
+			mids.length && bymids(mids, MatchListData)
 		}
 	})
 	return {
@@ -41,6 +45,7 @@ function use_match_list_ws(MatchListData = MatchDataWarehouse_PC_List_Common) {
 		// 将新的可视区域赛事id 设置为活跃
 		ws_destroyed: () => {
 			mids = null;
+			console.log('ws_destroyed')
 			ws_message_listener.ws_remove_message_listener(message_fun)
 			MatchListData.clear()//清除数仓数据
 		}
