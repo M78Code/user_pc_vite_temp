222d698fc060fee8b65bfbd9dc5d33030213b4e0
numbers <tynumbers@itcom888.com>
2023-11-21 19:38:39 +0800

    欧洲pc搜索样式调整

--- src/base-pc/components/top-header/template1/right_head.vue
+++ src/base-pc/components/top-header/template1/right_head.vue
@@ -240,9 +240,7 @@ export default defineComponent({
     function hide_search(e) {
       const target_class_list = ['q-field__native q-placeholder', 'serach-wrap column', 'sports-tab', 'tab', 'tab active', 'q-scrollarea__bar q-scrollarea__bar--v absolute-right', 'q-scrollarea__bar q-scrollarea__bar--v absolute-right q-scrollarea__bar--invisible', 'windows desktop landscape', 'icon-close'];
       if(is_focus.value && SearchPCClass.search_isShow) {
-        console.log('e', e.target.className);
         if(!target_class_list.includes(e.target.className)) {
-          // e.target.className != 'q-field__native q-placeholder' && e.target.className != 'serach-wrap column' && e.target.className != 'sports-tab' && e.target.className != 'tab' && e.target.className != 'tab active' && e.target.className != 'q-scrollarea__bar q-scrollarea__bar--v absolute-right' && e.target.className != 'q-scrollarea__bar q-scrollarea__bar--v absolute-right q-scrollarea__bar--invisible' && e.target.className != 'windows desktop landscape' && e.target.className != 'icon-close'
           SearchPCClass.set_search_isShow(false);
           is_focus.value = false;
           text.value = ''
