1e6ba9c644ae634404464cb4ff3dc8db5e0da8b6
lowen <pmtylowen@itcom888.com>
2023-11-21 18:39:06 +0800

    feat:详情投注与投注仓库关联

--- project/ouzhou-h5/src/pages/detailnew/components/template/tem_other.vue
+++ project/ouzhou-h5/src/pages/detailnew/components/template/tem_other.vue
@@ -4,6 +4,7 @@
  * @Description: 模板id= --用于无盘口&2个/多个投注项玩法
 -->
 <template>
+  <div v-show="false">{{BetData.bet_data_class_version}}</div>
   <div class="temp3 mx-10 box-style">
     <!-- ms: 0开 1封 2关 11锁 -->
     <!-- hs: 0开 1封 2关 11锁 -->
@@ -38,7 +39,7 @@
 
               <div
                 @click="go_betting(ol)"
-                :class="[{ 'is-active': ol?.oid == active }, 'ol_ov']"
+                :class="[{ 'is-active': BetData.bet_oid_list.includes(ol?.oid ) }, 'ol_ov']"
               >
                 <!-- {{ (ol.ov/100000).toFixed(2) }} -->
 
@@ -62,12 +63,12 @@
         <div v-for="ol in item_data.hl[0].ol" :key="ol?.oid" class="ol_on">
           <div
             @click="go_betting(ol)"
-            :class="[{ 'is-active': ol?.oid == active }, 'ol_ov']"
+            :class="[{ 'is-active': BetData.bet_oid_list.includes(ol?.oid ) }, 'ol_ov']"
           >
             <template v-if="ol.os == 1">
               <span class="ol-on-text">{{ ol?.on || ol?.ott }}</span>
               <span class="ol-ov-text">{{ get_oddv(ol?.ov / 100000) }}</span>
-              <olStatus :item_ol_data="ol" :active="ol?.oid == active" />
+              <olStatus :item_ol_data="ol" :active="BetData.bet_oid_list.includes(ol?.oid )" />
             </template>
             <span v-if="ol.os == 2"
               ><img class="lock" :src="odd_lock_ouzhou" alt="lock"
@@ -83,6 +84,7 @@
 import { onMounted, ref, computed } from "vue";
 import { storage_bet_info } from "src/core/bet/module/bet_info.js"; //#TODO core/index.js not export storage_bet_info
 import { odd_lock_ouzhou } from "src/base-h5/core/utils/local-image.js";
+import BetData from "src/core/bet/class/bet-data-class.js";
 // import EMITTER from  "src/global/mitt.js"
 import olStatus from "../ol_status.vue";
 const emit = defineEmits(["bet_click_"]);
@@ -101,7 +103,7 @@ const props = defineProps({
 // }
 
 const go_betting = (data) => {
-  if(data.o2 == 2) return
+  if(data.os == 2) return 
   emit("bet_click_", data);
   // storage_bet_info(payload)
   // EMITTER.emit("show_bet_dialog", true)
