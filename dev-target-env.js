d22c69257f340d2428bc70a5d818a70c1e1899d3
lowen <pmtylowen@itcom888.com>
2023-11-23 13:24:39 +0800

    fix:详情刷新两次

--- dev-target-env.js
+++ dev-target-env.js
@@ -53,7 +53,7 @@ let  DEV_TARGET_VERSION = ''
 
 //----------------------------------欧洲版-PC  ouzhou-pc 7-------------------------------------------
 // 测试环境
-DEV_TARGET_VERSION = "project_7-6544b0f3fb906b00ad5e8733-1700645253734-test";
+// DEV_TARGET_VERSION = "project_7-6544b0f3fb906b00ad5e8733-1700645253734-test";
    
 // 试玩环境
 // DEV_TARGET_VERSION = "project_7-6544b0f3fb906b00ad5e8733-1700646374913-shiwan";
@@ -66,6 +66,8 @@ DEV_TARGET_VERSION = "project_7-6544b0f3fb906b00ad5e8733-1700645253734-test";
 // DEV_TARGET_VERSION = "project_8-6544b116720143009a2f282d-1700645526474-test";
 // 试玩环境  
 // DEV_TARGET_VERSION = "project_8-6544b116720143009a2f282d-1700306375010-shiwan";
+// 试玩环境  
+DEV_TARGET_VERSION = "project_8-6544b116720143009a2f282d-1700716624534-geli";
 // 生产环境
 // DEV_TARGET_VERSION = "project_8-6544b116720143009a2f282d-1700470476243-online";
 
