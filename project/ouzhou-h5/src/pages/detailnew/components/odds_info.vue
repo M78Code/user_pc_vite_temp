b2546cb4a6fc8d0a789d801616bac2634352a46c
tybevis <tybevis@itcom888.com>
2023-11-24 10:39:17 +0800

    no message

--- project/ouzhou-h5/src/pages/detailnew/components/odds_info.vue
+++ project/ouzhou-h5/src/pages/detailnew/components/odds_info.vue
@@ -10,10 +10,8 @@
             :class="topKey_active[item.topKey] || props.allCloseState?'up':'down'" ></span>
         </div>
         
-        <div
-          :class="[{ 'is-expend': topKey_active[item.topKey] || props.allCloseState }, 'odds-expend']"
-        >
-         {{ `tem${[0, 1, 5, 10].includes(item.hpt) ? tem_choice(item.hpt) : '_other'}   ${ index }` }}
+        <div :class="[{ 'is-expend': topKey_active[item.topKey] || props.allCloseState }, 'odds-expend']">
+<!--         {{ `tem${[0, 1, 5, 10].includes(item.hpt) ? tem_choice(item.hpt) : '_other'}   ${ index }` }}-->
           <component
               :is="componentArr[`tem${[0, 1, 5, 10].includes(item.hpt) ? tem_choice(item.hpt) : '_other'}`]"
               :item_data="item"
