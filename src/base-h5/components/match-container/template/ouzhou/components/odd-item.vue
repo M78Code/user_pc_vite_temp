69bc1a48ca47cd582b3503d9377134d14fa26f7a
Y6C-DUNN-3CF0-T\admin <tydunn@itcom888.com>
2023-11-24 12:06:19 +0800

    BUG 48492 48489 修复

--- src/base-h5/components/match-container/template/ouzhou/components/odd-item.vue
+++ src/base-h5/components/match-container/template/ouzhou/components/odd-item.vue
@@ -65,7 +65,7 @@ const old_ov = ref(0)
 
 const is_show_title = computed(() => {
   const hpid = lodash.get(MatchResponsive.match_hpid_info.value, `csid_${props.csid}`, '1')
-  return hpid != 1
+  return hpid != 1 && !props.show_hpn
 })
 
 const is_active = computed(() => {
