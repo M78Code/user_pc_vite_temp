6fcfbd5c83581b8bc23b11155a80bc927a00c287
lockie <pmtylockie@itcom888.com>
2023-11-25 11:02:56 +0800

    layout 问题修复

--- project/ouzhou-pc/src/pages/match-list/index.js
+++ project/ouzhou-pc/src/pages/match-list/index.js
@@ -177,7 +177,7 @@ let match_count = ref(0);
 export const init_home_matches = async () => {
   const params = {
     type: 1,
-    sort: 1,
+    sort: 2,
     // hasFlag: 0
   };
   await axios_loop({
