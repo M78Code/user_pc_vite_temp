1c2c594d01e59c5152c560e17f7f95c31ddcd688
tybevis <tybevis@itcom888.com>
2023-11-26 21:08:58 +0800

    限制锁盘

--- project/ouzhou-h5/src/pages/detailnew/components/bevis/play-template-0.vue
+++ project/ouzhou-h5/src/pages/detailnew/components/bevis/play-template-0.vue
@@ -46,7 +46,7 @@ const go_betting = (data) => {
             <template v-for="hl_item in play.hl" :key="hl_item.hid">
                 <li v-for="ol_item in hl_item.ol.filter(i=>i.os != 3)" :key="ol_item?.oid" @click="go_betting(ol_item)"
                     :class="[{ 'is-active': BetData.bet_oid_list.includes(ol_item?.oid ) }, 'list-item']">
-                    <template v-if="ol_item?.os == 1">
+                    <template v-if="ol_item?.os == 1 && ol_item._hs != 11">
                         <span class="on-text textOverflow2">
                             {{ ol_item?.on || ol_item?.ott }}
                         </span>
@@ -55,7 +55,7 @@ const go_betting = (data) => {
                         </span>
                         <olStatus :item_ol_data="ol_item" :active="BetData.bet_oid_list.includes(ol_item?.oid )"/>
                     </template>
-                    <span v-if="ol_item?.os == 2">
+                    <span v-if="ol_item?.os == 2 || ol_item._hs == 11">
                         <img class="lock" :src="odd_lock_ouzhou" alt="lock"/>
                     </span>
                 </li>
