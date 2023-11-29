61e330f694c8d7d73a5cc22bdccdb84cf32114a1
jamison <pmtyjamison@itcom888.com>
2023-11-29 20:41:50 +0800

    赛果冠军玩法模版逻辑修改

--- src/base-pc/components/match-results/result-header/result-header-1/index.vue
+++ src/base-pc/components/match-results/result-header/result-header-1/index.vue
@@ -18,13 +18,13 @@
     <div class="wrap-select">
       
       <!-- 冠军球种才展示这个下拉选择框 -->
-      <div class="r-select ball-games" v-if="current_sport_id == '0'">
+      <div class="r-select ball-games" v-if="false">
         <!-- 球种 -->
         <span class="label ball-games-label">{{
           i18n_t("results.ball_games")
         }}</span>
         <Select-Wrapper
-          :value="champion_sport"
+          :sportType="champion_sport"
           :options="champion_sport_type"
           :isChampion="1"
           :showInput="true"
@@ -34,7 +34,7 @@
       <!-- 日期 -->
       <div class="r-select">
         <div class="label time-search-label">
-          {{ i18n_t("results.date") }}{{current_sport_id}}
+          {{ i18n_t("results.date") }}
         </div>
         <div class="search-date-wrapper">
           <div class="date-wrap" @click.stop="startTimeShowFunc">
@@ -181,6 +181,13 @@ const props = defineProps({
   sport_type: {
     type: Array,
     default: () => [],
+  },
+  champion_sport_type: {
+    type: Array,
+    default: () => [],
+  },
+  champion_sport: {
+    type: null,
   },
   sport:{
     type:String
