8fcfeabb05347557c22317ff58d932a608560c13
jinnian <jinnian@123.cc>
2023-12-01 15:21:08 +0800

    SDK 自测项目 架构调整

---
+++ project/check-sdk-esm/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+     <h1>SDK 本地 调试  check-sdk-esm  ```</h1>
+
+  </div>
+</template>
+
+<script  >
+
+
+export default {
+  data() {
+    return {
+      // key: value
+    }
+  },
+  created () {
+    console.log('TY_JSSDK------222----',window);  ;
+  },
+}
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
