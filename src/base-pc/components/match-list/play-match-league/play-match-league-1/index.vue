745f35574ddf51fb355b5544497fba030b583482
jiffy <jiffy@it888.com>
2023-12-04 11:07:42 +0800

    49792 【UAT】【欧洲版二期】【PC】收藏列表，冠军，收藏后的赛事，列表错误展示主胜/客胜/平局 1

--- src/base-pc/components/match-list/play-match-league/play-match-league-1/index.vue
+++ src/base-pc/components/match-list/play-match-league/play-match-league-1/index.vue
@@ -7,16 +7,16 @@
       <div class="leagues-wrap" :style="`width:${match_list_tpl_size.process_team_width}px !important;`">
         <div class="yb-flex-center" :style="`width:${match_list_tpl_size.media_width - 3}px !important;`">
           <!-- 联赛是否收藏 -->
-          <div @click.stop="collect"
-            class="icon-wrap m-star-wrap-league" v-if="!menu_config.is_esports() && GlobalAccessConfig.get_collectSwitch">
-            <div class="collect-start" :style="compute_css_obj({key: is_collect ? 'pc-home-star-fill' : 'pc-home-star-empty'})"></div>
+          <div @click.stop="collect" class="icon-wrap m-star-wrap-league"
+            v-if="!menu_config.is_esports() && GlobalAccessConfig.get_collectSwitch">
+            <div class="collect-start"
+              :style="compute_css_obj({ key: is_collect ? 'pc-home-star-fill' : 'pc-home-star-empty' })"></div>
           </div>
         </div>
-          <!-- 联赛图标 -->
-          <div class="league-icon-wrap">
-            <span class="soprts_id_icon"
-            v-if="menu_config.is_esports()"
-            :style="compute_css_obj({key:'pc-left-menu-bg-image', position: `item_${BaseData.compute_sport_id(card_style_obj.league_obj.csid)}` })"></span>
+        <!-- 联赛图标 -->
+        <div class="league-icon-wrap">
+          <span class="soprts_id_icon" v-if="menu_config.is_esports()"
+            :style="compute_css_obj({ key: 'pc-left-menu-bg-image', position: `item_${BaseData.compute_sport_id(card_style_obj.league_obj.csid)}` })"></span>
           <img v-else v-img="[lodash.get(card_style_obj, 'league_obj.lurl')]" />
         </div>
         <!-- 联赛名称 -->
@@ -33,7 +33,7 @@
       </div>
       <div :style="`width:${match_list_tpl_size.play_icon_width}px !important;`"></div>
       <!-- 玩法名称 -->
-      <div class="play-name-ouzhou" v-if="csid&&!card_style_obj.is_league_fold">
+      <div class="play-name-ouzhou" v-if="!is_kemp && csid && !card_style_obj.is_league_fold">
         <div class="play-name-title-box"
           v-for="(item, col_index) in match_tpl_info.get_current_odds_list(MatchListCardDataClass.get_csid_current_hpids(csid))"
           :key="col_index" :style="{ 'width': match_list_tpl_size.bet_width + 'px' }">
@@ -46,8 +46,8 @@
       </div>
       <!-- 赛事数量 -->
       <div v-else class="league-match-count">
-          <span>{{ card_style_obj.match_count}}</span>
-        </div>
+        <span>{{ card_style_obj.match_count }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -56,10 +56,8 @@
 // import sportIcon from "src/public/components/sport_icon/sport_icon.vue"
 import lodash from 'lodash';
 import { ref, computed, onUnmounted, watch } from 'vue';
-import sprite_img from "src/core/server-img/sprite-img/index.js"
 import BaseData from "src/core/base-data/base-data.js"
-import { t, compute_css_obj } from "src/core/index.js";
-import { get_match_tpl_title } from 'src/core/format/index.js'
+import { MenuData, compute_css_obj } from "src/core/index.js";
 import GlobalAccessConfig from "src/core/access-config/access-config.js"
 import { useMittEmit, MITT_TYPES } from 'src/core/mitt/index.js'
 import { utils_info } from 'src/core/utils/module/match-list-utils.js';
@@ -83,6 +81,9 @@ const props = defineProps({
     default: () => ''
   }
 })
+const is_kemp = computed(() => {
+  return (MenuData.is_kemp() || MenuData.is_common_kemp() || MenuData.is_collect_kemp() || MenuData.is_esports_champion()) && MenuData.menu_data_version.value
+})
 const csid = lodash.get(props.card_style_obj, 'league_obj.csid')
 let data_tpl_id = get_ouzhou_data_tpl_id(csid)
 const match_tpl_info = MATCH_LIST_TEMPLATE_CONFIG[`template_${data_tpl_id}_config`]
@@ -102,7 +103,7 @@ if (!csid && ['1', '500'].includes(menu_config.menu_root)) {
 */
 function set_fold() {
   // 如果当前联赛是折叠的 并且是今日、早盘  调用bymids接口拉数据
-  if (props.card_style_obj.is_league_fold ) {
+  if (props.card_style_obj.is_league_fold) {
     // 设置赛事基础数据
     MatchListCardData.set_match_basic_data(props.card_style_obj)
     let params = {
@@ -121,13 +122,12 @@ const collect = lodash.throttle(() => {
   is_collect.value = !is_collect.value;
 }, 1000)
 
-onUnmounted(()=>{
+onUnmounted(() => {
   clearTimeout(timer)
 })
 
 </script>
 <style lang="scss">
-
 .ouzhou-match-league {
   display: flex;
   width: 100%;
@@ -136,17 +136,19 @@ onUnmounted(()=>{
   border-bottom: 1px solid var(--q-gb-bd-c-2);
   font-weight: 500;
   cursor: pointer;
-  .league-icon-wrap {
-      width: 18px;
-      height: 18px;
-      margin-right: 10px;
-      line-height: 18px;
 
-      img {
-        width: 100%;
-        height: 100%;
-      }
+  .league-icon-wrap {
+    width: 18px;
+    height: 18px;
+    margin-right: 10px;
+    line-height: 18px;
+
+    img {
+      width: 100%;
+      height: 100%;
     }
+  }
+
   .leagues-wrap {
     padding-left: 5px;
     display: flex;
@@ -160,24 +162,25 @@ onUnmounted(()=>{
   }
 
   .league-match-count {
+    height: 100%;
+    position: absolute;
+    right: 13px;
+    font-weight: 600;
+    color: var(--q-gb-t-c-5);
+
+    span {
+      display: flex;
       height: 100%;
-      position: absolute;
-      right: 13px;
-      font-weight: 600;
-      color: var(--q-gb-t-c-5);
-      span {
-        display: flex;
-        height: 100%;
-        align-items: center;
-      }
+      align-items: center;
     }
-    
-    .collect-start {
-      width: 14px;
-      height: 14px;
-      cursor: pointer;
-      background-size: 100%;
-    }
+  }
+
+  .collect-start {
+    width: 14px;
+    height: 14px;
+    cursor: pointer;
+    background-size: 100%;
+  }
 
 }
 </style>
