d60d514488109faa46a8491c78272c21ba64f493
Haku <tyhaku@123.com>
2023-12-04 16:46:21 +0800

    保存右侧资讯选中mid 赛事移除匹配mid是否存在

--- project/ouzhou-pc/src/pages/scroll-ball/index.vue
+++ project/ouzhou-pc/src/pages/scroll-ball/index.vue
@@ -102,7 +102,8 @@ export default {
                 }
             })
             if (mids_arr.length) {
-                useMittEmit(MITT_TYPES.EMIT_SHOW_DETAILS, mids_arr[0]);
+                const mid_index = mids_arr.findIndex(item =>item == MatchListCardDataClass.select_mid)
+                useMittEmit(MITT_TYPES.EMIT_SHOW_DETAILS, mids_arr[mid_index]);
             }
         }
         onMounted(() => {
