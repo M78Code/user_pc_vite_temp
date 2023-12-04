c6bb8daca1d93c4e20245dd7e8f39f363d41e73a
oba <pmtyoba@itcom888.com>
2023-12-04 16:24:06 +0800

    Revert "复刻版夜间颜色修改"
    This reverts commit e47ccf2cac65ad24b0e06722c3aa5dffeb5b901e.

--- src/base-h5/components/menu/app-h5-menu/scroll-menu/scroll-menu.vue
+++ src/base-h5/components/menu/app-h5-menu/scroll-menu/scroll-menu.vue
@@ -132,7 +132,6 @@ onUnmounted(()=>{
     max-height: 1.35rem;
     padding: 0 0.05rem;
     transition: transform 0.6s, max-height 0.3s;
-    background-color: var(--q-gb-bg-c-1);
 
     // 二级菜单
     .sport-m-container {
