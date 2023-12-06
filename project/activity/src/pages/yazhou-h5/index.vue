69abfabfa15e7fefcbd43583c81a2dd022e7fbe7
jinnian <jinnian@123.cc>
2023-12-06 21:17:50 +0800

    活动 引入问题

--- project/activity/src/pages/yazhou-h5/index.vue
+++ project/activity/src/pages/yazhou-h5/index.vue
@@ -94,7 +94,7 @@ import acticity_mixin from "./mixin/acticity_mixin.js";
  
 import _ from 'lodash';
 import utils from 'project/activity/src/utils/utils.js';
-import { get_file_path } from 'project/activity/src/utils/get_file_path.js'
+ 
 
 
 export default {
@@ -205,7 +205,7 @@ export default {
     set_user(args) {
         return userStore.mutations.set_user(args);
     },
-    get_file_path: get_file_path,
+    get_file_path:  ()=>'',
     // 去到维护页面
     to_maintenance() {
       this.$toast('活动现已维护，感谢您的支持', 2000)
