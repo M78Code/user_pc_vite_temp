c4f347089cde49f8a1f94652756333000d4bab72
Jacques <jacques@itcom888.com>
2023-12-06 22:36:24 +0800

    docs(types): 修正api 类型限定

--- types/api.ts
+++ types/api.ts
@@ -13,6 +13,6 @@ namespace AXIOS {
 }
 
 namespace API {
-  export type Result<T extends AXIOS.Resource<Object|Array<Object>>=AXIOS.Resource<Object|Array<Object>>> = Promise<AXIOS.Resource<T>>
-  export interface MatchDetails extends AXIOS.Resource<Array<TYPES.MatchDetail>> { }
+  export type Result<T = Object> = Promise<AXIOS.Resource<T>>
+  export interface MatchDetails extends API.Result<Array<TYPES.MatchDetail>> { }
 }
