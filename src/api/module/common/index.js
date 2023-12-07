83bcdb3ae08929fa2258391d9be9a1a133736de3
lockie <pmtylockie@itcom888.com>
2023-12-07 17:14:41 +0800

    新增菜单收藏数量pb接口
    (cherry picked from commit 291c10b826b41d853635abf0fab8360db0b68e47)

--- src/api/module/common/index.js
+++ src/api/module/common/index.js
@@ -103,6 +103,14 @@ export const get_menu_of_favorite = (params, config, url="/v1/m/menu/countCollec
 export const get_menu_of_favorite_count = (params, config, url="/v1/m/menu/countCollectPB") => {
   return http.post(`${prefix}${url}`, params, config);
 };
+// 更新收藏列表菜单赛事数量 H5
+export const get_collect_menu_count = (params, config, url="/v1/m/menu/countCollectPB") => {
+  return http.post(`${prefix}${url}`, params, config);
+};
+// 更新收藏列表菜单赛事数量 PC
+export const get_collect_menu_count_pc = (params, config, url="/v1/w/menu/countCollectPB") => {
+  return http.post(`${prefix}${url}`, params, config);
+};
 //
 export const collectNewH5ListBottom = (params,config,url="/v1/m/collectNewH5ListBottom") => {
   params.sys = 7; //系统（1.panda-H5菜单  2.panda老PC-菜单 3.188菜单  4-新版PC菜单   7-H5 v2.0）
