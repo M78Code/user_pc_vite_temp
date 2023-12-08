fc9437cfdbb31ddc579c7077d67daebc459c08f3
dempsey <dempsey@dempsey.com>
2023-12-08 18:24:49 +0800

    fix:复刻版恢复一些注释

--- src/base-h5/components/menu/app-h5-menu/scroll-menu/scroll-menu.vue
+++ src/base-h5/components/menu/app-h5-menu/scroll-menu/scroll-menu.vue
@@ -74,10 +74,10 @@ function set_menu_lv2(item = {},event) {
   // if (item.mi == MenuData.current_lv_2_menu_i) return;
   scrollMenuEvent(event,".s-menu-container",".current");
   emits('changeMenu',item)
-  // nextTick(()=>{
-  // 设置菜单点击事件
-  // useMittEmit(MITT_TYPES.EMIT_SCROLL_TOP_NAV_CHANGE,item)
-  // })
+  nextTick(()=>{
+  设置菜单点击事件
+  useMittEmit(MITT_TYPES.EMIT_SCROLL_TOP_NAV_CHANGE,item)
+  })
 }
 /**
  * 初始化滚动条
