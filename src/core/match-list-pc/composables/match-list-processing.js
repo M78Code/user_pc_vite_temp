e085d86817e974d2f7ee0753c36f78767634a867
lockie <pmtylockie@itcom888.com>
2023-12-09 10:43:23 +0800

    fix

--- src/core/match-list-pc/composables/match-list-processing.js
+++ src/core/match-list-pc/composables/match-list-processing.js
@@ -92,7 +92,6 @@ const deal_with_list_data = (data) => {
  * @return {undefined} undefined
  */
 const mx_list_res = (data, backend_run, cut, collect) => {
-	console.log('dataasdasfaf', data);
 	let code = lodash.get(data, "code");
 	let res_data = lodash.get(data, "data");
 	// 将全量数据接口 切割成含有mid元素的对象数组
@@ -130,10 +129,10 @@ const mx_list_res = (data, backend_run, cut, collect) => {
 				count_mf = lodash.get(data, 'data.collectCount', 0)
 				console.error(error);
 			}
-			set_collect_count({
-				type: "set",
-				count: lodash.get(data, "data.collectCount", 0),
-			});
+			// set_collect_count({
+			// 	type: "set",
+			// 	count: lodash.get(data, "data.collectCount", 0),
+			// });
 		}
 		// 如果是专业版 && 今日、早盘、串关之间的切换 && 之前有筛选 && 并且当前没有筛选
 		if (
