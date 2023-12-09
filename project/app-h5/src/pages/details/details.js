5641c913d2cd71ce282b91346acb2ca28a177750
lowen <pmtylowen@itcom888.com>
2023-12-09 11:58:34 +0800

    fix:路径报错

--- project/app-h5/src/pages/details/details.js
+++ project/app-h5/src/pages/details/details.js
@@ -1,15 +1,19 @@
-import lodash from "lodash";
-import GlobalAccessConfig from "src/core/access-config/access-config.js"
 import { api_common, api_analysis } from "src/api/index.js";  // API 公共入口
-import { useMittOn, useMittEmit, useMittEmitterGenerator, MITT_TYPES } from "src/core/mitt"
 import { useRouter, useRoute } from "vue-router";
-import store from "src/store-redux/index.js";
 import axios_debounce_cache from "src/core/http/debounce-module/axios-debounce-cache.js";
 // import { Level_one_category_list, Level_one_detail_data } from "./category-list.js";
-import { defineComponent, reactive, computed, onMounted, onUnmounted, toRefs, watch, nextTick, ref, onBeforeMount } from "vue";
-import UserCtr from "src/core/user-config/user-ctr.js";
-import { MatchDataWarehouse_H5_Detail_Common, MatchDetailCalss, LOCAL_PROJECT_FILE_PREFIX, MenuData,MatchDataWarehouse_H5_List_Common } from "src/output/index";
-import { SessionStorage } from "src/core/utils/common/index.js"
+import { reactive, computed, onMounted, onUnmounted, toRefs, watch, nextTick, ref, onBeforeMount } from "vue";
+import {SessionStorage,
+   MatchDataWarehouse_H5_Detail_Common,
+   MatchDetailCalss, LOCAL_PROJECT_FILE_PREFIX,
+   MenuData,
+   MatchDataWarehouse_H5_List_Common,
+   GlobalAccessConfig,
+   useMittOn, useMittEmit,
+   useMittEmitterGenerator,
+   MITT_TYPES,
+   UserCtr
+  } from "src/output/index.js";
 
 export const details_main = () => {
   const router = useRouter();
