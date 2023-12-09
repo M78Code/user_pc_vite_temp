18df46cb343ce4bbfe6d845f7ae6173d3a0e8c35
hanmar <jstyhanmar@itcom888.com>
2023-12-09 22:22:33 +0800

    vr的lodash引入-02

--- project/app-h5/main.js
+++ project/app-h5/main.js
@@ -25,6 +25,7 @@ import lodash from "lodash";
 import { i18n_t } from "src/boot/i18n.js";
 const app = createApp(App);
 app.config.globalProperties.lodash = lodash;
+app.config.globalProperties._ = lodash;
 app.config.globalProperties.i18n_t = i18n_t;
 window.lodash = lodash;
 window._ = lodash;
