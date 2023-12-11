7927426b60cea41c4a5e43e6c72f026b74090904
jamescarr <jamescarr@gmail.com>
2023-12-11 13:31:47 +0800

    fix: 【欧洲版二期】【H5】比赛详情页，播放视频源时切换到其他比赛，视频会继续播放

--- src/base-h5/components/details/components/videos2.vue
+++ src/base-h5/components/details/components/videos2.vue
@@ -74,8 +74,9 @@
         </div>
         <!-- 视频单页项目-->
         <!-- {{ iframe_src+'&rdm='+iframe_rdm }} -->
+        <!-- {{iframe_show  }} {{ is_show_no_handle}} {{iframe_src  }} iframe_show && !is_show_no_handle && -->
         <iframe 
-        v-if="iframe_show && !is_show_no_handle && iframe_src" 
+        v-if=" iframe_src" 
         v-show="!is_playing_replay" 
         ref="iframe" id="bdIframe" 
         style="width:100%;height:100%;" 
