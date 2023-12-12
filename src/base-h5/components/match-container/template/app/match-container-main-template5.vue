318e5bf79379cd759a8e2ae54b6e459e36996f69
dempsey <dempsey@dempsey.com>
2023-12-12 13:16:20 +0800

    fix: 复刻版新手版样式修改

--- src/base-h5/components/match-container/template/app/match-container-main-template5.vue
+++ src/base-h5/components/match-container/template/app/match-container-main-template5.vue
@@ -863,7 +863,7 @@ export default {
         justify-content: center;
         /* justify-content: space-between; */
         /* padding-right: 46px; */
-        margin-top: 8px;
+        margin: .08rem 0;
 
         .bet_btn {
           display: flex;
