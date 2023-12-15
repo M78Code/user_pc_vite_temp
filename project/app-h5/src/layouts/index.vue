789b047bb702a6e261958548baa61823463c263f
jamison <pmtyjamison@itcom888.com>
2023-12-15 14:30:02 +0800

    50645 【SIT】【H5新版复刻】【H5】设置菜单、投注模式，切换专业版后，刷新页面，变回新手版

--- project/app-h5/src/layouts/index.vue
+++ project/app-h5/src/layouts/index.vue
@@ -162,6 +162,13 @@ const touchend_event_fun = (event) => {
 const gesturestart_event_fun = (event) => {
   event.preventDefault();
 };
+/**
+ * @description: 设置当前版本 
+ */
+const set_standard_edition_fun = () => {
+  const status =  sessionStorage.getItem('standard_edition') || 2
+  UserCtr.set_standard_edition(status);
+};
 
 //计算投注记录框的样式
 const calc_bottom = () => {
@@ -215,6 +222,7 @@ const show_chain_bet = () => {
 }
 
 onMounted(() => {
+  //获取当前版本默认值
   window.onresize = debounce((e) => {
     console.log(e)
     inner_height.value = window.innerHeight
@@ -236,6 +244,8 @@ onMounted(() => {
   });
   // 设置设备类型
   BetData.set_device_type(1)
+  //设置当前版本默认
+  set_standard_edition_fun()
 });
 const mitt_list = [
   // 监听设置框状态
