75935569df45711925810c029e5e6268f9637b24
dolphin <pmtydolphin@itcom888.com>
2023-12-16 14:25:44 +0800

    1

--- project/app-h5/src/layouts/top.vue
+++ project/app-h5/src/layouts/top.vue
@@ -278,7 +278,7 @@ useMittOn(MITT_TYPES.EMIT_CHANGE_SEARCH_FILTER_SHOW, function (value) {
     const mi_tid_mids_res = lodash_.get(BaseData, 'mi_tid_mids_res')
     if (lodash_.isEmpty(mi_tid_mids_res)) return
     // 设置菜单对应源数据
-    MatchMeta.set_origin_match_data()
+    // MatchMeta.set_origin_match_data()
 
     // 今日 下 得足球  提前设置 热门联赛
     if (MenuData.current_lv_2_menu_i === '1012') MatchMeta.set_tid_map_mids()
