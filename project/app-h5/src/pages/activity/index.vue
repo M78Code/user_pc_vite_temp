41c4f1558287b40cc022510a3df79cb067bbaa35
nico <pmtynico@itam8888>
2023-12-17 00:00:31 +0800

    复刻活动页面各个环境对应

--- project/app-h5/src/pages/activity/index.vue
+++ project/app-h5/src/pages/activity/index.vue
@@ -13,16 +13,33 @@
 <script setup>
 import { ref,nextTick } from "vue";
 import UserCtr from "src/core/user-config/user-ctr.js";
+/** 环境变量 */ 
+const current_env = window.BUILDIN_CONFIG.CURRENT_ENV
 // 活动访问地址暂时写死
 const  acticity_src = ref('')
+const  url = ref('')
 //用户token
 const  token = UserCtr.get_user_token()
 //用户分组
 const  gr = window.SEARCH_PARAMS.init_param.get('gr')?.toLocaleUpperCase()
+if (current_env == 'idc_online' || current_env == 'idc_ylcs') {
+            // 生产环境
+          url.value =  'http://front-test-100.dbsportxxx2li.com/activity'
+        } else if (current_env == 'idc_sandbox') {
+            // 试玩环境
+          url.value = 'http://front-test-99.dbsportxxx2li.com/activity'
+        }else if(current_env == 'idc_lspre'){
+          //隔离环境
+          url.value = 'http://front-test-98.dbsportxxx2li.com/activity'
+        } else {
+            // 其他环境，测试和开发 等
+          url.value =  'http://test-topic.sportxxxifbdxm2.com/activity'
+        }
     
 // 访问路由拼接 
-const  url  = 'http://test-topic.sportxxxifbdxm2.com/activity' +'?gr='+gr+'&token='+token
-acticity_src.value = url
+const  acticity_src_url  = url.value  +'?gr='+gr+'&token='+token
+acticity_src.value = acticity_src_url
+// console.error(token,'token',acticity_src);
 </script>
 <style lang="scss" scoped>
 
