6892de6f79cac8b1e601172656d7d6919da03873
jamescarr <jamescarr@gmail.com>
2023-12-17 14:59:58 +0800

    fix: 【H5新版复刻】【H5】荧幕旋转，没显示 不支援横屏

---
+++ src/base-h5/components/common/orientation.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="orientation">
+        <img :src="imgUrl" alt="" class="img">
+        {{ i18n_t('common.hengping_tip') }}
+    </div>
+</template>
+
+<script setup>
+import { LOCAL_PROJECT_FILE_PREFIX } from 'src/output';
+
+const imgUrl = `${LOCAL_PROJECT_FILE_PREFIX}/image/app-h5/hengpin.svg`
+</script>
+
+<style scoped lang="scss">
+
+.orientation {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #666;
+    color: #fff;
+    font-size:14px;
+    .img{
+        width: 90px;
+    height: 108px;
+    margin-bottom: 10px;
+    }
+}
+</style>
