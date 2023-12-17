e97686ab14418ec9d7aeba856efe7f390741681c
dempsey <dempsey@dempsey.com>
2023-12-17 12:14:03 +0800

    fix: 恢复一些修改的逻辑，配合多芬去修改

--- src/base-h5/components/observer-wrapper/observer-item2.vue
+++ src/base-h5/components/observer-wrapper/observer-item2.vue
@@ -5,13 +5,19 @@
 <template>
   <template v-if="is_show">
     <!-- 赛果玩法 -->
-      <template v-if="is_results">
+      <template v-if="is_results && !is_kemp">
         <MatchContainerMainTemplate3
           :i="index"
           :match_of_list="item">
         </MatchContainerMainTemplate3>
       </template>
-      <template>
+      <template v-else-if="is_results && is_kemp">
+        <MatchContainerMainTemplate6
+          :i="index"
+          :match_of_list="item">
+        </MatchContainerMainTemplate6>
+      </template>
+      <template v-else>
         <MatchContainerMainTemplate1
           :i="index"
           :match_of_list="item">
@@ -22,10 +28,11 @@
 
 <script setup>
 import { computed } from 'vue';
-import {is_results } from 'src/base-h5/mixin/menu.js'
+import {is_results, is_kemp } from 'src/base-h5/mixin/menu.js'
 import MatchResponsive from 'src/core/match-list-h5/match-class/match-responsive';
 import MatchContainerMainTemplate1 from "src/base-h5/components/match-container/template/app/match-container-main-template1.vue"; 
 import MatchContainerMainTemplate3 from "src/base-h5/components/match-container/template/app/match-container-main-template3.vue"; 
+import MatchContainerMainTemplate6 from "src/base-h5/components/match-container/template/app/match-container-main-template6.vue"; 
 
 const props = defineProps({
  item: {
