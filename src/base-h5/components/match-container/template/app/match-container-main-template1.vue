248345ed0e6cb587bf2a8b7867fd645434e24735
jamison <pmtyjamison@itcom888.com>
2023-12-18 21:24:56 +0800

    51276 【SIT】【H5新版复刻】【H5】夜间版赛事列表部分字体无法查看

--- src/base-h5/components/match-container/template/app/match-container-main-template1.vue
+++ src/base-h5/components/match-container/template/app/match-container-main-template1.vue
@@ -1038,6 +1038,7 @@ export default {
       display: flex;
       align-items: center;
       font-size: 0.1rem;
+      
     }
 
     .team-wrapper {
@@ -1178,6 +1179,7 @@ export default {
           line-height: 0.14rem;
           display: flex;
           align-items: center;
+          color: var(--q-gb-t-c-18);
           .yb-flex-center{
             padding-left: 2px;
             .yb-goal-gif{
