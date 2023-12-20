cd328ba328f13942ae6759ec1d5e00da99f357b1
jiffy <jiffy@it888.com>
2023-12-20 14:20:59 +0800

    51474 【SIT】【H5新版复刻】【H5】电竞模块，从新手版切换回专业版时，页面展示异常 2

--- src/base-h5/components/menu/app-h5-menu/switch/switch-wap.vue
+++ src/base-h5/components/menu/app-h5-menu/switch/switch-wap.vue
@@ -7,8 +7,8 @@
 -->
 <template>
     <div class="switch-wap">
-        <div class="switch-content" v-for="(item , index) in switchData" :key="index">
-            <SwitchNav :list = "item.list" :defaultVal="item.defaultVal"/>
+        <div class="switch-content" v-for="(item, index) in switchData" :key="index">
+            <SwitchNav :list="item.list" :defaultVal="item.defaultVal" />
         </div>
     </div>
 </template>
@@ -18,62 +18,70 @@ import { ref, nextTick, onUnmounted, watch } from "vue";
 import { theme_list, theme_map } from "src/core/theme/"
 import UserCtr from "src/core/user-config/user-ctr.js"
 import { lang } from "src/base-h5/mixin/userctr";
-
+import MatchMeta from 'src/core/match-list-h5/match-class/match-meta';
+import { MenuData,useMittEmit,MITT_TYPES} from "src/output/index.js";
+import VirtualList from 'src/core/match-list-h5/match-class/virtual-list'
 /**
  * 首页switch wap
  */
 const switchData = [
     {
-        defaultVal:0,
-        list:[
+        defaultVal: 0,
+        list: [
             {
-                name:"专业版",
-                val:2,
-                changeFun:(val)=>{
-                    // 1 新手版 2 专业版
-                   nextTick(()=>{
+                name: "专业版",
+                val: 2,
+                changeFun: (val) => {
                     UserCtr.set_standard_edition(2)
-                   })
+                    useMittEmit(MITT_TYPES.EMIT_GOT_TO_TOP);
+                    // 1 新手版 2 专业版
+                    nextTick(() => {
+                        VirtualList.set_is_show_ball(true)
+                        MatchMeta.compute_page_render_list({ scrollTop: 0, type: 2, is_scroll: false })
+                    })
                 }
             },
             {
-                name:"新手版",
-                val:1,
-                changeFun:(val)=>{
-                    nextTick(()=>{
-                        UserCtr.set_standard_edition(1)
+                name: "新手版",
+                val: 1,
+                changeFun: (val) => {
+                    UserCtr.set_standard_edition(1)
+                    useMittEmit(MITT_TYPES.EMIT_GOT_TO_TOP);
+
+                    nextTick(() => {
+                        !MenuData.is_collect() && MatchMeta.handler_match_list_data({ list: MatchMeta.complete_matchs, scroll_top: 0 })
                     })
                 }
             }
         ]
     },
     {
-        defaultVal:0,
-        list:[
+        defaultVal: 0,
+        list: [
             {
-                name:"热门",
-                val:0,
-                isSort:1,
-                changeFun:(val,sort)=>{
+                name: "热门",
+                val: 0,
+                isSort: 1,
+                changeFun: (val, sort) => {
                     return console.log(`执行热门-${val}-${sort}`)
                 }
             },
             {
-                name:"时间",
-                val:1,
-                isSort:1,
-                changeFun:(val,sort)=>{
+                name: "时间",
+                val: 1,
+                isSort: 1,
+                changeFun: (val, sort) => {
                     return console.log(`执行时间-${val}-${sort}`)
                 }
             }
         ]
     },
     {
-        defaultVal:theme_list.findIndex((item)=>{return item.is_default === 1;}),
-        list:theme_list.map((item)=>{
+        defaultVal: theme_list.findIndex((item) => { return item.is_default === 1; }),
+        list: theme_list.map((item) => {
             item.name = item.i18n[lang.value];
             item.val = item.key;
-            item.changeFun = (val)=>{
+            item.changeFun = (val) => {
                 console.log(val)
                 return UserCtr.set_theme(val)
             }
@@ -83,14 +91,15 @@ const switchData = [
 ]
 </script>
 <style scoped lang="scss">
-    .switch-wap{
-        width: 100%;
-        height: 0.24rem;
-        display: flex;
-        margin: 0.05rem 0;
-        .switch-content{
-            flex: 1;
-            margin: 0 0.1rem;
-        }
+.switch-wap {
+    width: 100%;
+    height: 0.24rem;
+    display: flex;
+    margin: 0.05rem 0;
+
+    .switch-content {
+        flex: 1;
+        margin: 0 0.1rem;
     }
+}
 </style>
