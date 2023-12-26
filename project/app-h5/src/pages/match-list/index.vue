79e2967b213c8e3ed17dd0f935c5533c79c28f3c
dolphin <pmtydolphin@itcom888.com>
2023-12-26 15:57:38 +0800

    52323 【SIT】【H5新版复刻】【H5】普通赛果，电子足球数据展示不足

--- project/app-h5/src/pages/match-list/index.vue
+++ project/app-h5/src/pages/match-list/index.vue
@@ -32,7 +32,7 @@ import { useMittOn, useMittEmit, MITT_TYPES } from  "src/core/mitt/index.js"
 import lodash from "lodash";
 import store from "src/store-redux/index.js";
 import tiaozhuanPanel from "src/base-h5/components/match-list/components/tiaozhuan-panel.vue";    //  跳转banner图和猜你喜欢
-import matchContainer from "src/base-h5/components/match-list/index.vue";
+import MatchContainer from "src/base-h5/components/match-list/index.vue";
 // import SList from "src/base-h5/components/skeleton/skeleton-list.vue"   // 赛事列表骨架屏
 import scrollTop from "src/base-h5/components/common/record-scroll/scroll-top.vue";
 import BaseData from 'src/core/base-data/base-data.js'
