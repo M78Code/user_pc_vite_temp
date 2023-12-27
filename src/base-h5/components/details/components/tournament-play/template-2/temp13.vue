12714898e537be75c3b0f2ef11acf9a9c9204edb
lowen <pmtylowen@itcom888.com>
2023-12-27 15:44:27 +0800

    52352 【SIT】【H5新版复刻】【H5】赛事详情、投注项名称，部份玩法FoxFire浏览器会看到预期外的文字

--- src/base-h5/components/details/components/tournament-play/template-2/temp13.vue
+++ src/base-h5/components/details/components/tournament-play/template-2/temp13.vue
@@ -123,7 +123,7 @@
           <!-- 小 -->
           <div class="play-name " v-show="!get_is_hengping">
               <div class="play-name-card ellipsis">
-                {{lodash.get(item_data, 'title[1].osn')}}+++{{ rem(1)+'px' }}
+                {{lodash.get(item_data, 'title[1].osn')}}
               </div>
           </div>
           <div class="row slide-con" ref="bet_slide" style="flex:1;" v-touch-pan.horizontal.prevent.mouse="touch_pan">
