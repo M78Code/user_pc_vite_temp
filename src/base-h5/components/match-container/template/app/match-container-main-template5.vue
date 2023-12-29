5cc2d2e798421548b8f457c47db0efd37c5f8832
jamison <pmtyjamison@itcom888.com>
2023-12-29 19:04:53 +0800

    52585 【SIT】【H5新版复刻】【H5】赛果详情页，切换到精选赛事时，会先展示一个缺省图

--- src/base-h5/components/match-container/template/app/match-container-main-template5.vue
+++ src/base-h5/components/match-container/template/app/match-container-main-template5.vue
@@ -583,7 +583,7 @@ export default {
     .match-content{
       border-radius: 0 0 8px 8px;
       background: var(--q-gb-bg-c-18);
-      border: 1px solid var(--q-gb-bd-c-15);
+      border: 1px solid var(--q-gb-bd-c-18);
       &.collapsed{
         border-top: none;
       }
@@ -596,7 +596,7 @@ export default {
       }
     }
     > .match-indent{
-      border: 1px solid var(--q-gb-bd-c-15);
+      border: 1px solid var(--q-gb-bd-c-18);
       border-radius: 8px 8px 0 0 !important;
       border-bottom: 1px solid var(--q-gb-bd-c-4) !important;
       background: var(--q-gb-bg-c-18);
