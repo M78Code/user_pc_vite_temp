25b0fa50b750ca7456f51b799c90f4133d8479e5
dolphin <pmtydolphin@itcom888.com>
2023-12-30 13:10:16 +0800

    问题修复

--- project/app-h5/src/router/index.js
+++ project/app-h5/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import { MenuData } from "src/output";
-import UserCtr from 'src/core/user-config/user-ctr.js'
+import PageSourceData  from  "src/core/page-source/page-source.js";
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
@@ -147,7 +147,7 @@ const router = createRouter({
  * 路由切换清除默认球种
  */
 router.afterEach((to, from) => {
-  UserCtr && typeof UserCtr.set_from_page === 'function' && UserCtr.set_from_page(to.name)
+  PageSourceData && PageSourceData.set_from_page(to.name)
   if(from.name=="virtual_sports" && to.name == "matchList"){
     MenuData.set_top_menu_title({});//设置菜单
  }
