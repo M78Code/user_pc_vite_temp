8fd97aba02529570e89c7d314f9a2ef3700abed8
Jacques <jacques@itcom888.com>
2023-12-30 16:40:48 +0800

    Revert "fix: 修改bug - 52740"
    This reverts commit b6b52d89aae46eb1da3250fed184a1abe3c9f3eb.

--- src/base-h5/components/match-filter/index.vue
+++ src/base-h5/components/match-filter/index.vue
@@ -16,7 +16,7 @@
         </div>
     <!-- 中间滚动选择项 -->
     <q-scroll-area class="scroll-area" v-if="!no_find_content && !list_data_loading" ref="scrollArea">
-      <div v-if="list.length" v-scroll="scrolled" class="yb_mb18 overflowx">
+      <div v-if="list.length" v-scroll="scrolled" class="yb_mb18">
         <!-- 循环整个后台返回数据 -->
         <div class="scroll-area1" v-for="(item, index) in list" :key="index" ref="scroll_area1">
           <div class="bg-f6f7f8 scroll-title" ref="bg_f6f7f8" v-if="item.title">
@@ -1136,10 +1136,4 @@ if (type.value == 30) {
 .icon_keep:before {
   color: var(--q-color-fs-color-50);
 }
-
-.overflowx {
-  overflow-x: hidden;
-}
-
-
 </style>
