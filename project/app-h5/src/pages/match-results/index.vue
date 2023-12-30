6784350c979370d17abdc3b2740852febd5fff99
rise <yrise@it888.com>
2023-12-30 11:08:27 +0800

    vr赛果增加联赛筛选

--- project/app-h5/src/pages/match-results/index.vue
+++ project/app-h5/src/pages/match-results/index.vue
@@ -28,7 +28,13 @@
         <DateTab @changeDate="setDate" ref="RsDateTabMenu" :dataList="dataList"  />
         <!-- <ScrollMenu v-if="state.slideMenu_sport.length" :scrollDataList="menu_list" :is_show_badge="false" :current_mi="state.current_mi" @changeMenu="set_scroll_current"/> -->
         <ScrollMenu v-if="menu_list.length" :scrollDataList="menu_list" :is_show_badge="false" :current_mi="current_mi" @changeMenu="set_scroll_current"/>
-
+        <div class="tab-wrapper"  v-if="state.currentSwitchValue == 2">
+        <!-- <div class="tab-wrapper" v-if="state.currentSwitchValue == 2"> -->
+            <div class="tab-item" :class="{active:i == state.tab_item_i}" v-for="(tab_item,i) of state.tab_items.find(n=>{return +n.menuId == MenuData.current_lv_2_menu?.sport_id})?.subList"
+            :key="i" @click="changeLeagueVr(tab_item.menuId,i)">
+            <div>{{tab_item.name}}</div>
+            </div>
+        </div>
         <!-- <ObserverWrapper class="match-result-contant" :match_list="state.matchs_data" com_type="app-h5"></ObserverWrapper> -->
         <MatchContainer/>
     </template>
@@ -42,7 +48,7 @@
 </template>
 <script setup>
 // import lodash_ from 'lodash'
-import { onMounted, ref, reactive,computed, nextTick } from "vue";
+import { ref, reactive,computed, nextTick } from "vue";
 import { ScrollMenu,DateTab } from 'src/base-h5/components/menu/app-h5-menu/index'
 import navigationBar from 'src/base-h5/components/tutorial/navigation-bar/index.vue'
 // import VirtualList from 'src/core/match-list-h5/match-class/virtual-list'
@@ -53,10 +59,11 @@ import MatchFold from 'src/core/match-fold/index.js'
 import { MenuData,compute_local_project_file_path } from "src/output/index.js"
 import { useMittEmit, MITT_TYPES } from "src/core/mitt";
 import { dateTabList } from "src/base-h5/components/menu/app-h5-menu/utils";
-import ObserverWrapper from 'src/base-h5/components/observer-wrapper/index.vue';
+// import ObserverWrapper from 'src/base-h5/components/observer-wrapper/index.vue';
 import BaseData from "src/core/base-data/base-data.js";
 import setectLeague from 'src/base-h5/components/tutorial/navigation-bar/setect-league.vue'
 import MatchContainer from "src/base-h5/components/match-list/index.vue";
+import {api_common } from "src/api/index.js";
 
 const inner_height = window.innerHeight;  // 视口高度
 // const switchMenu = [i18n_t('app_h5.match.normal_results'), i18n_t('app_h5.match.game_results'), i18n_t('app_h5.match.vr_results'), i18n_t('app_h5.match.championship_results')]
@@ -92,7 +99,10 @@ const switchMenu = [
     setting_dialog: false,
     slideMenu_date: [], // 时间
     slideMenu_sport: [], // 赛种
-    matchs_data: []
+    matchs_data: [],
+    tab_items:[],
+    tid:"",
+    tab_item_i:0
 })
  /**
  * 球种列表
@@ -107,7 +117,7 @@ const menu_list = computed(()=>{
             arr = BaseData.dianjing_sublist.map(item=>{return {...item,sport_id:+item.mi-2000,result_mi:+item.mi}});
             break;
         case 2:
-            arr = MenuData.get_menu_lvmi_special_list(300).map(item=>{return {...item,sport_id:+item.mi-30000,result_mi:+item.mi}});;
+            arr = MenuData.get_menu_lvmi_special_list(300).map(item=>{return {...item,sport_id:+item.mi-30000,result_mi:+item.mi}});
             break;
         case 3:
             arr = [];
@@ -124,7 +134,7 @@ const current_mi = ref(MenuData.current_lv_2_menu?.mi||menu_list.value[0]?.mi)
 const setDate = () =>{
     state.currentSlideValue = MenuData.data_time;
     //调用接口
-    get_date_matches_list(MenuData.current_lv_2_menu?.mi?MenuData.current_lv_2_menu:menu_list.value[0]);
+    get_date_matches_list();
 }
 /**
  * 筛选点击
@@ -132,12 +142,53 @@ const setDate = () =>{
 const filterChange = () =>{
     if(state.currentSwitchValue !==3)state.select_dialog = true;
 }
-const switchHandle = (val,type) => {
+/**
+ * 联赛点击
+ * @param {*} tid 
+ * @param {*} i 
+ */
+const changeLeagueVr = (tid,i) =>{
+    if(state.tab_item_i == i)return;
+    state.tab_item_i = i;
+    state.tid = tid
+    MenuData.set_results_vr_type(i);
+    get_date_matches_list();
+}
+/**
+ * 暂时获取联赛接口  后期国际化从init取 目前init没有国际化
+ */
+const get_vr_menu_list = async () =>{
+    const res = await api_common.get_virtual_menu({device: 'V1_H5'});
+    if(res && res.code =='200'){
+        return res.data
+    }
+    return [];
+}
+/**
+ * 设置vr联赛id
+ * @param {*} menu_mi 
+ * @param {*} index 
+ */
+const setTid = (menu_mi,index) =>{
+    const tid = state.tab_items.find(n=>{return +n.menuId == menu_mi.sport_id})?.subList?.[index]?.menuId;
+    state.tid = tid;
+    state.tab_item_i = index;
+}
+/**
+ * 头部切换
+ * @param {*} val 
+ * @param {*} type 
+ */
+const switchHandle = async(val,type) => {
     state.currentSwitchValue = val
     MenuData.set_result_menu_lv1_mi(val)
     state.matchs_data = []
     MenuData.set_results_type(val)
-    set_scroll_current(MenuData.current_lv_2_menu?.mi && type?MenuData.current_lv_2_menu:menu_list.value[0])
+    const menu_mi = MenuData.current_lv_2_menu?.mi && type?MenuData.current_lv_2_menu:menu_list.value[0];
+    if(val !== 2)return set_scroll_current(menu_mi);
+    state.tab_items =  await get_vr_menu_list();
+    const index = MenuData.is_results_vr_type && type?MenuData.is_results_vr_type:0;
+    return set_scroll_current(menu_mi,index)
     // //获取 赛果菜单
     // switch (+val) {
     //     case 0:
@@ -202,6 +253,7 @@ const switchHandle = (val,type) => {
  * 获取数据
  */
 const get_date_matches_list = async (item)=>{
+    item = item || MenuData.current_lv_2_menu?.mi?MenuData.current_lv_2_menu:menu_list.value[0];
     MatchFold.clear_fold_info()
     MatchMeta.clear_match_info()
     useMittEmit(MITT_TYPES.EMIT_SHOW_SKELETON_DIAGRAM, true);
@@ -223,7 +275,7 @@ const get_date_matches_list = async (item)=>{
             break;
         case 2:
             nextTick(async () => {
-                state.matchs_data = await MatchMeta.get_virtual_results_match();
+                state.matchs_data = await MatchMeta.get_virtual_results_match(state.tid);
                 useMittEmit(MITT_TYPES.EMIT_SHOW_SKELETON_DIAGRAM, false);
             })
             break;
@@ -243,11 +295,15 @@ const get_date_matches_list = async (item)=>{
 }
 
 // 设置滑动菜单的选中id
-const set_scroll_current = item => {
+const set_scroll_current = (item,index) => {
+    index = index ||0;
     state.matchs_data = []
     if(item){
         current_mi.value = item.mif ||item.mi;
         MenuData.set_current_lv_2_menu_i(item);
+    }
+    if(state.currentSwitchValue == 2){
+        setTid(item,index)
     }
     get_date_matches_list(item)
     // if (MenuData.get_results_type() == 3) {
