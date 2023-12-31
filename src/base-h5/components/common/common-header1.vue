3c631296422ff31916ef17c2de7901fd685cfdf5
tide <pmtytide@itcom888.com>
2023-12-31 16:58:07 +0800

    详情页修复联赛标题居中问题

--- src/base-h5/components/common/common-header1.vue
+++ src/base-h5/components/common/common-header1.vue
@@ -301,13 +301,14 @@ export default {
   letter-spacing: 0;
   text-align: center;
   font-weight: 400;
-   padding-left: 0.6rem;
+   //padding-left: 0.6rem;
   // 修复名称居中问题
+  // width不能用100%，会盖住返回等问题
   background-color: transparent !important;
-  //position: absolute;
-  //left: 0;
-  //width: 100%;
-  //padding: 0 0.4rem;
+  position: absolute;
+  left: 50%;
+  width: 76%;
+  margin-left: -38%;
 }
 
 .details-c {
