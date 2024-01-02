261f24891e923caf473a56e63b7c04be43fb3ca7
lowen <pmtylowen@itcom888.com>
2024-01-02 11:37:33 +0800

    去除t国际化告警

--- src/base-h5/components/details/components/chatroom/common-bet-item.vue
+++ src/base-h5/components/details/components/chatroom/common-bet-item.vue
@@ -121,7 +121,7 @@
 // import { mapGetters } from "vuex";
 import { reactive, computed, onMounted, onUnmounted, toRefs, watch, defineComponent } from "vue";
 import { format_time_zone_time, format_money2,format_odds, format_score } from "src/output/index.js"
-import { t } from "src/boot/i18n.js";;
+;
 import {UserCtr,compute_css_obj} from "src/output/index.js";
 
 //国际化
