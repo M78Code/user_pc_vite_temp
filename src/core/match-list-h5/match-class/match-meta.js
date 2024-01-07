419f9cbbe64a8f8c1cae125ebd71653f5ffec89b
dolphin <pmtydolphin@itcom888.com>
2024-01-07 16:01:04 +0800

    删除打印 测试

--- src/core/match-list-h5/match-class/match-meta.js
+++ src/core/match-list-h5/match-class/match-meta.js
@@ -1472,7 +1472,7 @@ class MatchMeta {
     switch (obj.text) {
       // 活动
       case "activities":
-        console.log('每日活动')
+        // console.log('每日活动')
         break;
       // 排序
       case "sortRules":
