8d0c9edda2fbd3b7fd7229af9953bfd024cc267b
crafter <tycrafter@itcom888.com>
2024-01-08 16:45:51 +0800

    ouzhou-h5 vr 顶部返回

--- src/base-h5/components/top-menu/top-menu-ouzhou-1/components/vr-top/vr-top.vue
+++ src/base-h5/components/top-menu/top-menu-ouzhou-1/components/vr-top/vr-top.vue
@@ -61,7 +61,7 @@ const go_back = () => {
   if(props.is_vr_page){
     // console.log("history===", history);
     // router.go(hisLen.value - history.length - 1)
-    router.back(-1)
+    router.push('/virtual')
     return ;
   }
 }
