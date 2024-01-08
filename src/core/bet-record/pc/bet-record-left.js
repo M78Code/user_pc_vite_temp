423435fb07d27f514bf4b450df23a7d950f73459
orich <liontxkj@gmail.com>
2024-01-08 19:11:51 +0800

    欧洲新版PC开发

--- src/core/bet-record/pc/bet-record-left.js
+++ src/core/bet-record/pc/bet-record-left.js
@@ -1,5 +1,5 @@
 import { ref } from "vue"
-import { columns } from "./util.js"
+import { o_params } from "./util.js"
 import { useMittEmit, MITT_TYPES } from "src/core/mitt/index.js"
 import UserCtr from "src/core/user-config/user-ctr.js";
 import { api_betting } from "src/api/index.js";
@@ -14,16 +14,9 @@ class BetRecord {
     // 0 未结算 1 已结算  2 预约
     this.selected = 0
     // 列表
-    this.params = {
-      page: 1,
-      size: 50,
-      userId: UserCtr.user_info.userId
-    }
     this.api_url = api_betting.post_getOrderList
     this.table_data = []
     this.records = {}
-    // table列表columns
-    this.columns = columns
     //是否在加载中
     this.loading = true
     // 投注记录版本变更
@@ -34,14 +27,10 @@ class BetRecord {
   set_selected(number) {
     this.selected = number
     this.reset()
-    // 切换提示语
-    this.set_tip_msg(number)
-    // 更改columns
-    this.set_columns(number)
     // 更改api
     this.set_api_url(number)
     // 通知 重新获取数据 
-    useMittEmit(MITT_TYPES.EMIT_BET_RECORD_SELECTED_CHANGE, this.selected)
+    useMittEmit(MITT_TYPES.EMIT_GET_RECORD_LIST, o_params[number])
     this.set_bet_record_version()
   }
 
@@ -49,25 +38,6 @@ class BetRecord {
   set_table_data(value) {
     this.table_data = value
     this.set_bet_record_version()
-  }
-
-  // 更改columns
-  set_columns(number) {
-    if (number == 1) {
-      this.columns[5] = {
-        name: 'return',
-        label: i18n_t("common.donate_win"),
-        align: 'center',
-        field: 'return'
-      }
-    } else {
-      this.columns[5] = {
-        name: 'highestWin',
-        label: i18n_t("common.maxn_amount_val"),
-        align: 'center',
-        field: 'highestWin'
-      }
-    }
   }
 
   // 更改api
@@ -85,60 +55,8 @@ class BetRecord {
   }
 
 
-  // 根据 今天、昨天、7天、30天提示
-  set_date_tip_msg(val) {
-    let msg = ''
-    switch (val) {
-      case 1:
-        msg = 'bet_record.msg_2'
-        break;
-      case 2:
-        msg = 'bet_record.msg_3'
-        break;
-      case 3:
-        msg = 'bet_record.msg_4'
-        break;
-      case 4:
-        msg = 'bet_record.msg_5'
-        break;
-    }
-    this.tipMsg = msg
-    this.set_bet_record_version()
-  }
-
-  /**
-   * 获取数据
-   */
-  handle_fetch_order_list = async () => {
-    try {
-      this.loading = true
-      let res = await this.api_url({ ...this.params })
-      if (res.code !== '200') {
-        if (res.code === '0401038') {
-          GlobalSwitchClass.set_tip_show_state(true, {
-            // 当前访问人数过多，请稍后再试
-            text: i18n_t('common.limited')
-          })
-          return;
-        }
-        return;
-      }
-      this.records = res.data || {}
-      this.set_table_data(res.data?.records || [])
-    } catch (error) {
-      console.error(error)
-    } finally {
-      this.loading = false
-    }
-  }
-
   // 初始化数据
   reset() {
-    this.params = {
-      page: 1,
-      size: 50,
-      userId: UserCtr.user_info.userId
-    }
     this.table_data = []
     this.records = {}
     this.loading = true
