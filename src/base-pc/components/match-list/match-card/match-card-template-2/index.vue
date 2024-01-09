b1f8e82cd4b38577bd858a245d7ec0dad13adfeb
jiffy <jiffy@it888.com>
2024-01-09 20:03:15 +0800

    欧洲代码优化

--- src/base-pc/components/match-list/match-card/match-card-template-2/index.vue
+++ src/base-pc/components/match-list/match-card/match-card-template-2/index.vue
@@ -4,23 +4,24 @@
   <div v-show="false">{{ MatchListCardDataClass.list_version }}</div>
   <div class="c-match-card relative-position" :id="`list-mid-${mid}`" v-if="match_style_obj.is_show_card"
     :style="`height:${lodash.get(match_style_obj, `total_height`)}px !important;
-            width:${LayOutMain_pc.oz_layout_content - LayOutMain_pc.oz_right_width - LayOutMain_pc.oz_left_width}px  !important;`">
+                    width:${LayOutMain_pc.oz_layout_content - LayOutMain_pc.oz_right_width - LayOutMain_pc.oz_left_width}px  !important;`">
     <!--改成101用来打包调试-->
     <!-- <div class="test">{{ match_style_obj.offset_top }}----{{ match_style_obj.show_level }}</div> -->
-    <component :is="`MatchTpl${get_current_template_number()}After`"  :mid="mid" />
+    <component :is="`MatchTpl${get_current_template_number()}After`" :mid="mid" />
   </div>
 </template>
 
 <script>
-import { ref, onMounted, onUnmounted, provide,inject } from 'vue';
+import { ref, onMounted, computed, onUnmounted, provide, inject } from 'vue';
 import MatchListCardData from 'src/core/match-list-pc/match-card/match-list-card-class.js'
 import MatchListCardDataClass from "src/core/match-list-pc/match-card/module/match-list-card-data-class.js";
-import {LayOutMain_pc} from "src/output/project/common/pc-common.js";
+import { LayOutMain_pc } from "src/output/project/common/pc-common.js";
 import { get_match_template_id } from 'src/core/match-list-pc/match-handle-data.js';
 // 玩法模板 101 欧洲版 常规赛事
 import { MatchTpl101AfterFullVersionWapper as MatchTpl101After } from "src/base-pc/components/match-list/match-tpl-new-data/match-tpl-101-after/index.js";
 // 欧洲版 冠军模板
 import { MatchTpl118AfterFullVersionWapper as MatchTpl118After } from "src/base-pc/components/match-list/match-tpl-new-data/match-tpl-118-after/index.js";
+import { MATCH_LIST_TEMPLATE_CONFIG } from 'src/core/match-list-pc/list-template/index.js'
 export default {
   props: {
     mid: {
@@ -37,10 +38,21 @@ export default {
     const get_match_item = (mid) => {
       return MatchListData.get_quick_mid_obj(mid)
     }
-    provide("match",MatchListData.get_quick_mid_obj_ref(props.mid))
-    // 赛事样式对象
-    let match_style_obj = MatchListCardDataClass.get_card_obj_bymid(props.mid)
-    const match = get_match_item(props.mid)
+    const match = MatchListData.get_quick_mid_obj_ref(props.mid)
+    let match_style_obj = computed(() => {
+      return MatchListCardDataClass.get_card_obj_bymid(props.mid, MatchListCardDataClass.list_version.value)
+    });
+    const match_list_tpl_size = computed(() => {
+      return MATCH_LIST_TEMPLATE_CONFIG[`template_101_config`].width_config
+    })
+    const match_tpl_info = computed(() => {
+      return MATCH_LIST_TEMPLATE_CONFIG[`template_${match_style_obj.value?.data_tpl_id || match.value.tpl_id}_config`]
+    })
+    provide("match", match)
+    provide("match_list_tpl_size", match_list_tpl_size)
+    provide("match_style_obj", match_style_obj)// 赛事样式对象
+    provide("match_tpl_info", match_tpl_info)// 赛事样式对象
+    //101号模板 默认就是 101的宽高配置 不会改变
     const get_current_template_number = () => {
       let tpl_id = get_match_template_id(match);
       if (tpl_id == 118) {
