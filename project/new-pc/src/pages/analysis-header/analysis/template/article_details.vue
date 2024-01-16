98c51d0bca5403c383ef301bcfaacfb78f8446e7
tide <pmtytide@itcom888.com>
2024-01-16 15:40:58 +0800

    new pc 赛事分析 样式调整

--- project/new-pc/src/pages/analysis-header/analysis/template/article_details.vue
+++ project/new-pc/src/pages/analysis-header/analysis/template/article_details.vue
@@ -176,4 +176,75 @@ export default {
 .rule-scroll-area {
   flex: 1;
 }
+
+.event-item {
+  color: var(--qq--analysis-text-color-2);
+  display: flex;
+  align-items: center;
+  height: 40px;
+  border-bottom: 1px solid var(--qq--analysis-bd-color-2);
+  border-left: 1px solid var(--qq--analysis-bd-color-2);
+  border-right: 1px solid var(--qq--analysis-bd-color-2);
+  &:last-child {
+    border-bottom: 1px solid var(--qq--analysis-bd-color-2);
+    border-radius: 0 0 8px 8px;
+  }
+  .time {
+    width: 58px;
+    text-align: center;
+  }
+  .sign {
+    width: 7px;
+    height: 7px;
+    border-radius: 7px;
+    background: var(--qq--analysis-bg-color-16);
+    &.team-1 {
+      background: var(--qq--analysis-bg-color-13);
+    }
+    &.team-2 {
+      background: var(--qq--analysis-bg-color-12);
+    }
+    &:before {
+      content: "";
+      position: absolute;
+      bottom: 7px;
+      left: 3px;
+      height: 17px;
+      width: 1px;
+      background: var(--qq--analysis-bg-color-16);
+    }
+    &:after {
+      content: "";
+      position: absolute;
+      top: 7px;
+      left: 3px;
+      height: 17px;
+      width: 1px;
+      background: var(--qq--analysis-bg-color-16);
+    }
+  }
+  .info {
+    margin-left: 8px;
+    & > span {
+      margin-right: 8px;
+    }
+    .sign-icon {
+      margin-right: 8px;
+    }
+    .status-default {
+      color: var(--qq--analysis-text-color-4);
+    }
+  }
+  &:first-child {
+    .sign:before {
+      display: none;
+    }
+  }
+  &:last-child {
+    .sign:after {
+      display: none;
+    }
+  }
+}
+
 </style>
