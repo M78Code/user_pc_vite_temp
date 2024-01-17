a4537cc7f28653c68a73bfa7ac0ca84bd039adbc
hanmar <jstyhanmar@itcom888.com>
2024-01-17 12:05:48 +0800

    51245 【大数据项目】【产品】【生产】新增埋点版本统计-欧洲版对应-05

--- project/ouzhou-h5/src/pages/home/index.vue
+++ project/ouzhou-h5/src/pages/home/index.vue
@@ -120,11 +120,6 @@ const set_init_sport = (val) =>{
 }
 let timer = 0;
 onMounted(async () => {
-  // 发送进入首页埋点消息
-  clearTimeout(timer)
-  timer = setTimeout(() => {
-    lodash.get(UserCtr,'user_info.userId') && into_home_event();
-  }, 2000);
   if (tabValue.value === 'top_events') {
     // 设置 元数据计算 流程
     state.current_mi = MenuData.top_events_list?.[0]?.mi;
