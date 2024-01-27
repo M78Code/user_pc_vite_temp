f2966a90418a6ba974d0f2556224bfc334e6335c
crafter <tycrafter@itcom888.com>
2024-01-27 16:29:40 +0800

    fixed: 54791 【UAT】【欧洲版四期】【PC】电竞、已结注单、注单明细，地图玩法，没有显示赔率种类与赛前

--- project/ouzhou-pc/src/pages/bet-record/compoments/record_table.vue
+++ project/ouzhou-pc/src/pages/bet-record/compoments/record_table.vue
@@ -496,22 +496,22 @@ const matchType = (type, langCode=UserCtr.lang) => {
   // if(type && langCode) {
   //   res = match_type[type]
   // }
-
+  // console.log('type===langCode===', type,  langCode)
+  // console.log('type===langCode=ccc==', `odds.${langCode}.morning_session`)
   if(type && langCode) {
         switch (parseInt(type)) {
           case 1:
-            res = i18n_t(`odds.${langCode}.morning_session`); //"赛前";
+            res = i18n_t(`odds.${UserCtr.lang}.morning_session`); //"赛前";
             break;
           case 2:
-            res = i18n_t(`odds.${langCode}.list_today_play_title`);//"滚球";
+            res = i18n_t(`odds.${UserCtr.lang}.list_today_play_title`);//"滚球";
             break;
           case 3:
-            res =i18n_t(`odds.${langCode}.match_winner`); //"冠军";
+            res =i18n_t(`odds.${UserCtr.lang}.match_winner`); //"冠军";
             break;
         }
       }
       return res;
-
 }
 /**
  * 投注状态
