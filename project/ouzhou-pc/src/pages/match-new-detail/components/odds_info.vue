668174964ce570ead32fd674729fe23f5be5b306
cooper <cooper@123.com>
2024-01-27 14:28:32 +0800

    54877 【UAT】【欧洲版三期】【PC】足球，详情页面，340、359、383玩法，不展示投注项1

--- project/ouzhou-pc/src/pages/match-new-detail/components/odds_info.vue
+++ project/ouzhou-pc/src/pages/match-new-detail/components/odds_info.vue
@@ -23,7 +23,7 @@
         <!-- 赛事玩法名称  hs: 0开 1封 2关 11锁  -->
         <template v-slot:header>
           <div class="odds-item">
-            {{ item.hpn }} +++{{ item.hpt }} +++{{ item.hpid }}
+            {{ item.hpn }} 
             <span v-if="item.hps && get_match_status(detail_info.ms) == 1">
               ({{ item.hps.split("|")[1] }})
             </span>
