463925874e35462206228a61cf166f57219f68e2
dempsey <dempsey@dempsey.com>
2024-01-31 17:30:13 +0800

    fix: 55219 【SIT】【欧洲版四期】【PC】合并投注，且其中一个预约投注，投注后，提示语错误

--- src/core/bet/class/bet-data-class.js
+++ src/core/bet/class/bet-data-class.js
@@ -11,6 +11,7 @@ import { nextTick, ref } from "vue"
 import lodash_ from "lodash"
 import UserCtr from "src/core/user-config/user-ctr.js";
 import { SessionStorage } from "src/core/utils/common/module/web-storage.js";
+import betViewDataClass from "./bet-view-data-class.js";
 
 
 class BetData {
@@ -1426,6 +1427,12 @@ this.bet_appoint_ball_head= null */
 
   // 设置投注项状态
   set_options_state() {
+    console.log('这里！', betViewDataClass.bet_order_status)
+    // 需要筛选出 某些状态不需要再去做投注项状态的改版
+    // 目前 7 8 为预约投注后
+    if ([7, 8].includes(betViewDataClass.bet_order_status)) {
+      return
+    }
     let single_name = ''
     if(this.is_bet_single){
       single_name = 'bet_single_list'
