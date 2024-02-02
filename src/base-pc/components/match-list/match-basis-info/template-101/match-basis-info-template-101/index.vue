7fd0ca1eb2d53b090c45863632d95a075c564e5a
jiffy <jiffy@it888.com>
2024-02-02 11:16:07 +0800

    串关判断修改

--- src/base-pc/components/match-list/match-basis-info/template-101/match-basis-info-template-101/index.vue
+++ src/base-pc/components/match-list/match-basis-info/template-101/match-basis-info-template-101/index.vue
@@ -9,7 +9,7 @@
             :style="compute_css_obj({ key: is_collect ? 'pc-home-star-fill' : 'pc-home-star-empty' })"></div>
         </div>
         <div class="esports-mfo" v-if="MenuData.is_esports() && match.mfo">
-          <em v-if="!match.ispo">{{ i18n_t('match_info.match_parlay')}}</em>
+          <em v-if="match.ispo">{{ i18n_t('match_info.match_parlay')}}</em>
           <span>{{ match.mfo }}</span>
         </div>
         <!-- 比赛进程 -->
