65ce987cb9cb2cdb07d5b8e1839eac2e8e8f74ed
quentin <pmtyquentin@itcom888.com>
2024-02-03 10:59:06 +0800

    55322 【SIT】【欧洲版四期】【PC】电竞串关投注、点击投注项投注会错误默认展示金额0  误触代码回撤

--- src/base-pc/components/bet/bet-box-ouzhou-pc-1/components/bet-input.vue
+++ src/base-pc/components/bet/bet-box-ouzhou-pc-1/components/bet-input.vue
@@ -17,7 +17,7 @@
                     </div>
                 </div>
         
-                <div>21312312111111111  
+                <div>  
                     <input class="bet-input" :v-model="ref_data.money==0?'':ref_data.money" type="text" @input="set_win_money" @click="show_quick_amount(true)" @focus="stop_drap_fn(false)" @blur="stop_drap_fn(true)" @keydown.enter="keydown($event)"
                     :placeholder="placeholder" maxLength="11"  />
                 </div>
