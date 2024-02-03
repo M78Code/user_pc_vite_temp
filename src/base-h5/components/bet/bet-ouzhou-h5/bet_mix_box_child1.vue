ae59695382e93ceb1a8a1dd96f73c2292b72c85b
dempsey <dempsey@dempsey.com>
2024-02-03 16:41:25 +0800

    fix: 优化投注输入相关代码

--- src/base-h5/components/bet/bet-ouzhou-h5/bet_mix_box_child1.vue
+++ src/base-h5/components/bet/bet-ouzhou-h5/bet_mix_box_child1.vue
@@ -61,7 +61,7 @@
                           <div class="bet-odds-name">{{i18n_t('pre_record.handicap')}}</div>
                           <div class="bet-odds-edit">
                             <span class="bet-odds-reduce" :class="{begray:ref_pre_book.appoint_ball_value <= 0}" v-touch-repeat:0:300.mouse.enter.space="() => {sub_handle(items)}">-</span>
-                            <bet-input-info3 :items="items" :valueModel="ref_pre_book.appoint_ball_value" :index="'pre_handicap' + index"></bet-input-info3>
+                            <bet-input-info3 :items="items" :readonly="items.sportId == 1" :valueModel="ref_pre_book.appoint_ball_value" :index="'pre_handicap' + index"></bet-input-info3>
                             <span class="bet-odds-add" :class="{begray:ref_pre_book.appoint_ball_value >= check_ball_maxmin(items)}" v-touch-repeat:0:300.mouse.enter.space="() => {add_handle(items)}">+</span> 
                           </div>
                         </div>                    
