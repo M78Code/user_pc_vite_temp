6cb934042f77d084df6ced2d11d188e951f4763c
jamescarr <jamescarr@gmail.com>
2024-02-09 13:07:14 +0800

    feat: 3288 全屏投注需求开发

--- src/base-h5/components/screen-bet/components/date-icon.vue
+++ src/base-h5/components/screen-bet/components/date-icon.vue
@@ -1,10 +1,13 @@
 <template>
-    <svg class="date-icon" width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class="date-icon" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect y="0.5" width="32" height="32" rx="16" fill="black" fill-opacity="0.5" />
-        <rect x="5.6" y="9.6" width="20.8" height="14.8" rx="4.4" stroke="white" stroke-width="1.2" />
-        <path
-            d="M11.8594 18.9014V20H7.75928V13.6587H9.08643V18.9014H11.8594ZM13.9731 20H12.646V13.6587H13.9731V20ZM18.3545 20H16.7549L14.6235 13.6587H16.1089L17.5591 18.6333H17.5898L19.04 13.6587H20.4858L18.3545 20ZM25.3374 18.9014V20H21.1362V13.6587H25.3374V14.7529H22.4634V16.2822H25.1748V17.3018H22.4634V18.9014H25.3374Z"
-            fill="white" />
+        <rect x="5.6" y="7.6" width="20.8" height="16.8" rx="4.4" stroke="white" stroke-width="1.2" />
+        <rect x="10" y="17.7002" width="1.5" height="4" rx="0.75" fill="white" />
+        <rect x="15.25" y="14.2002" width="1.5" height="7.5" rx="0.75" fill="white" />
+        <rect x="20.5" y="15.2002" width="1.5" height="6.5" rx="0.75" fill="white" />
+        <path d="M16 11L19 13" stroke="white" stroke-width="1.5" stroke-linecap="round" />
+        <path d="M19 13L22 11" stroke="white" stroke-width="1.5" stroke-linecap="round" />
+        <path d="M10 15L16 11" stroke="white" stroke-width="1.5" stroke-linecap="round" />
     </svg>
 </template>
 
--- src/base-h5/components/screen-bet/screen-bet.vue
+++ src/base-h5/components/screen-bet/screen-bet.vue
@@ -1,35 +1,52 @@
 <template>
-    <div class="screen-bet">
-        <div class="header">
-            <div class="leading">
-                <IconBack />
+    <div class="screen-bet-container">
+        <div class="screen-bet" v-show="!show_line">
+            <div class="header">
+                <div class="leading">
+                    <IconBack />
+                </div>
+                <div class="title">
+                    <span>法国</span>
+                    <span class="score">0-0</span>
+                    <span>葡萄牙</span>
+                </div>
+                <div class="actions" @click.stop="show_line_callback">
+                    高清
+                </div>
             </div>
-            <div class="title">
-                <span>法国</span>
-                <span class="score">0-0</span>
-                <span>葡萄牙</span>
+            <div class="body">
+                <ul class="actions-list">
+                    <li><IconDetail /></li>
+                    <li><IconAni /></li>
+                    <li><IconDate /></li>
+                </ul>
             </div>
-            <div class="actions">
-                高清
-            </div>
-        </div>
-        <div class="body">
 
-        </div>
-
-        <div class="footer">
-            <IconInfo />
-            <div class="actions">
-                <IconExitScreen />
+            <div class="footer">
+                <IconInfo />
+                <div class="actions">
+                    <IconExitScreen />
+                </div>
+            </div>
+            <!-- 定位在右侧组件 -->
+            <div class="right">
+                <!-- 详情插槽 -->
+                <slot name="details"></slot>
+                <div class="bet-container" v-if="false">
+                    <!-- 投注插槽 -->
+                    <slot name="bet"></slot>
+                </div>
             </div>
         </div>
-        <!-- 定位在右侧组件 -->
-        <div class="right">
-            <!-- 详情插槽 -->
-            <slot name="details"></slot>
-            <div class="bet-container" v-if="false">
-                <!-- 投注插槽 -->
-                <slot name="bet"></slot>
+        <!-- 高清 -->
+        <div class="line-container" @click.stop="show_line_callback">
+            <div :class="['change-line', show_line ? 'line-ani' : '']" @click.stop>
+                <ul >
+                    <li :class="['line-item', select_line == 0 ? 'active' : '']" @click.stop="change_line(0)">高清一</li>
+                    <li :class="['line-item', select_line == 1 ? 'active' : '']" @click.stop="change_line(1)">高清一</li>
+                    <li :class="['line-item', select_line == 2 ? 'active' : '']" @click.stop="change_line(2)">高清一</li>
+                </ul>
+                <p class="desc">*视频卡顿或不清晰可自行调整线路</p>
             </div>
         </div>
     </div>
@@ -39,7 +56,11 @@
 import { ref, computed } from "vue";
 import IconBack from "./components/back-icon.vue";
 import IconInfo from "./components/info-icon.vue";
-import IconExitScreen from "./components/exit-screen-icon.vue"
+import IconExitScreen from "./components/exit-screen-icon.vue";
+import IconDetail from "./components/detail-icon.vue";
+import IconDate from "./components/date-icon.vue";
+import IconLive from "./components/live-icon.vue";
+import IconAni from "./components/ani.icon.vue"
 /** @type {{type: 'animation' | 'video'}} */
 const props = defineProps({
     /** 全屏的是动画还是视频 */
@@ -54,22 +75,94 @@ const is_video = computed(() => props.type == "video");
 const is_animation = computed(() => props.type == "animation");
 /** 控制是否显示投注弹窗 */
 const status = ref(false);
+/** 是否展示高清切换 */
+const show_line = ref(false);
+/** 选择的清晰度 */
+const select_line = ref(0);
+/**
+ * 清晰度弹窗
+ */
+const show_line_callback = () => {
+    
+    show_line.value = !show_line.value;
+    console.log(show_line.value, "show_line.value");
+}
+
+/**
+ * 修改清晰度
+ * @param {*} index 
+ */
+const change_line = (index) => {
+    select_line.value = index;
+}
 </script>
 
 <style lang="scss" scoped>
+.screen-bet-container {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    .line-container {
+        position: relative;
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+    .change-line {
+        background: rgba($color: #000000, $alpha: .88);
+        position: absolute;
+        height: 100%;
+        right: -270px;
+        top: 0;
+        width: 270px;
+        transition: transform .3s linear;
+        transform: translateX(0);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        .line-item {
+            width: 88px;
+            height: 36px;
+            border-radius: 8px;
+            border: 1px solid var(--border-color-border-primary, rgba(255, 255, 255, 0.04));
+            margin-bottom: 16px;
+            color: var(--text-color-text-subtle, rgba(255, 255, 255, 0.50));
+            text-align: center;
+            line-height: 36px; /* 125% */
+            transition:  .3s linear;
+            
+        }
+
+        .active {
+            border-color: #127DCC;
+            color: #127DCC;
+        }
+        .desc {
+            color: rgba(255, 255, 255, 0.60);
+            margin-top: 74px;
+        }
+    }
+    .line-ani {
+        transform: translateX(-100%);
+    }
+
+    
+}
 .screen-bet {
     width: 100vw;
     height: 100vh;
-    background: #000;
-    position: fixed;
+    background: gray;
     top: 0;
     left: 0;
-    z-index: 1000;
+    z-index: 10;
     padding: 20px 36px 16px 36px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-
+    position: absolute;
     .header {
         height: 44px;
         display: flex;
@@ -92,7 +185,7 @@ const status = ref(false);
                 padding: 0 16px;
             }
         }
-
+       
         .actions {
             display: flex;
             align-items: center;
@@ -104,6 +197,18 @@ const status = ref(false);
             color: #fff;
         }
 
+    }
+
+    .body {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        align-items: flex-end;
+        .actions-list {
+            li {
+                margin-bottom: 16px;
+            }
+        }
     }
 
     .footer {
@@ -120,5 +225,4 @@ const status = ref(false);
         top: 0;
     }
 }
-
 </style>
