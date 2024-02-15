98427d9a77106c6bf980af5d7095c484373c9c8a
lane <jstylane@itcom888.com>
2024-02-15 17:27:28 +0800

    Revert "1"
    This reverts commit a692aa3e2a6b7fa4ae2084e3956637381d3801b3.

--- src/base-h5/components/common/counting-down.vue
+++ src/base-h5/components/common/counting-down.vue
@@ -13,7 +13,7 @@
     </span>
     <span v-if="show_time_counting()" ref="counting" class="counting" v-html="counting_time_ctr_show_format_ouzhou(match,counting_time)"></span>
     <span ref="special-match" class="special" :class="{'mar-l5': u_like}" :data-d="`${match.csid}-${match.mmp}`"
-      v-show="[2,4,6,15,16].includes(+match.csid) && [301,302,303].includes(+match.mmp)">
+      v-show="[2,4,6,15,16].includes(+match.csid) && [301,302,303].includes(+match.mmp)"><!--csid:16水球-->
       {{match.mlet}}
     </span>
   </div>
